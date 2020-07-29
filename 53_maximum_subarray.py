07e1641b5eab9cf349562286c0ed3287755e90ba
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2020-07-28 21:17:13 -0700

    Dynamic Programming

---
+++ 53_maximum_subarray.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def max_sub_array(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        max_val = dp[0]
+        for i in range(1, len(nums)):
+            if dp[i - 1] > 0:
+                dp[i] = nums[i] + dp[i - 1]
+            else:
+                dp[i] = nums[i]
+            max_val = max(max_val, dp[i])
+        return max_val
