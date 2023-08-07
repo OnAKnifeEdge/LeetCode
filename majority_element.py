7e3fab543fd1ba75733d1a3ab90d5e716b0016d0
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2023-08-07 16:57:29 -0700

    169 majority element

---
+++ majority_element.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/majority-element/
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count = count + 1
+            else:
+                count = count - 1
+        return candidate
