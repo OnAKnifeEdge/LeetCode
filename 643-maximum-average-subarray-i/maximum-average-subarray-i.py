ad5895eb7500291d4aa6300f3f37f70fa580adf2
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-04 14:07:37 -0800

    Time: 873 ms (84.81%) | Memory: 28.7 MB (40.14%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -6,7 +6,6 @@ class Solution:
         for i in range(k, len(nums)):
             running_sum += nums[i]
             running_sum -= nums[i - k]
-
             max_sum = max(max_sum, running_sum)
 
         return max_sum / k
