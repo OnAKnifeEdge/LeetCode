6f42f7d1b32e7c6abad57a81f92940cc8b88dbe8
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-06 21:23:41 -0800

    Time: 234 ms (42.93%) | Memory: 19.4 MB (95.17%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        # https://leetcode.com/problems/subarray-sum-equals-k/solutions/3777004/why-sum-k-read-this-to-understand/
+        # d[sum] -> OccuranceCount
+        d = {0: 1}
+        s, answer = 0, 0
+        for i, e in enumerate(nums):
+            s += e
+            if s - k in d:
+                answer += d[s - k]
+            if s in d:
+                d[s] = d[s] + 1
+            else:
+                d[s] = 1
+        return answer
+
+        
