f976085a357f1852709042e1499e95366a48121a
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-08 15:28:50 -0800

    Time: 634 ms (88.00%) | Memory: 17.2 MB (80.51%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
@@ -15,6 +15,7 @@ class Solution:
                     # if out of bounds or reach a wall
                     if r + 1 == 0 or r == ROWS or c == COLS or c + 1 == 0 or maze[r][c] == '+':
                         continue
+                    # if reach an empty cell on the border
                     if r == 0 or r == ROWS - 1 or c == 0 or c == COLS - 1:
                         return d + 1
                     maze[r][c] = '+'
