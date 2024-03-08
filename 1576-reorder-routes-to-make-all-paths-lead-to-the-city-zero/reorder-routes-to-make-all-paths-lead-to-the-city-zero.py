77a7da95996970976d8818a08d5bc77a54965c50
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-08 13:30:32 -0800

    Time: 965 ms (38.61%) | Memory: 51.8 MB (24.95%) - LeetSync

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,34 @@
+class Solution:
+
+
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        # start at city 0
+        # recursively check its neighbors
+        # count outgoing edges
+
+        edges = {(a, b) for a, b in connections}
+        neighbors = {city: [] for city in range(n)}
+        visited = set()
+        changes = 0
+
+        for a, b in connections:
+            neighbors[a].append(b)
+            neighbors[b].append(a)
+
+        def dfs(city):
+            nonlocal edges, neighbors, visited, changes
+
+            for neighbor in neighbors[city]:
+                if neighbor in visited:
+                    continue
+                # check if the neighor can reach city 0
+                if (neighbor, city) not in edges:
+                    changes += 1
+                visited.add(neighbor)
+                dfs(neighbor)
+        visited.add(0)
+        dfs(0)
+
+        return changes
+
+        
