188e51dc321ae0e86db42f93267122f5851631ca
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-08 21:10:24 -0800

    Time: 749 ms (77.01%) | Memory: 37.2 MB (71.75%) - LeetSync

---
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        pairs = [(a, b) for a, b in zip(nums1, nums2)]
+        pairs = sorted(pairs, key=lambda p:p[1],reverse=True)
+        max_score = 0
+        min_heap = []
+        prefix_sum = 0
+        for a, b in pairs:
+            prefix_sum += a
+            heapq.heappush(min_heap, a)
+            if len(min_heap) == k:
+                max_score = max(max_score, b * prefix_sum)
+                prefix_sum -= heapq.heappop(min_heap)
+        return max_score
