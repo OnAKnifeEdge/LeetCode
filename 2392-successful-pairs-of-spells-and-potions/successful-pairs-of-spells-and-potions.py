4f16b01f04b08e66ea83412d1911d77caec3ca20
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-11 12:34:44 -0700

    Time: 823 ms (93.30%) | Memory: 39.6 MB (57.45%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
@@ -1,10 +1,11 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        # return (len(potions) - bisect_left(potions, success, key=lambda p: p * s) for s in spells)
+
         potions.sort()
         answer = []
 
         max_potion = potions[- 1]
-
         for spell in spells:
             min_potion = math.ceil(success/spell)
             i = bisect_left(potions, min_potion)
