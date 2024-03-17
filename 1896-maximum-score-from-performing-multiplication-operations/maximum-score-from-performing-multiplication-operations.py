4e1c8ace558c38e75a5ac57d63f19aac84192be1
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-17 15:13:03 -0700

    Time: 782 ms (77.76%) | Memory: 28.3 MB (70.70%) - LeetSync

---
+++ 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
+        n, m = len(nums), len(multipliers)
+        dp = [[0] * (m + 1) for _ in range(0, m + 1)]
+
+        for i in range(m - 1, -1, -1):
+            for left in range(i, -1, -1):
+                right = n - 1 - (i - left)
+                select_left = multipliers[i] * nums[left] + dp[i + 1][left + 1]
+                select_right = multipliers[i] * nums[right] + dp[i + 1][left]
+                dp[i][left] = max(select_left, select_right)
+                
+        return dp[0][0]
+        
