606b72ad6dab6a8462fe149690d9710979729027
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-16 17:45:05 -0700

    Time: 80 ms (40.11%) | Memory: 18.7 MB (63.34%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.py
+++ 740-delete-and-earn/delete-and-earn.py
@@ -6,17 +6,25 @@ class Solution:
             points[num] += num
             max_number = max(max_number, num)
         
-        @cache
-        def dp(num): 
-            # dp(i) is the max points when reach to num i
-            if num == 0:
-                return 0
-            if num == 1:
-                return points[1]
+        # top down recurrence
+        # @cache
+        # def dp(num): 
+        #     # dp(i) is the max points when reach to num i
+        #     if num == 0:
+        #         return 0
+        #     if num == 1:
+        #         return points[1]
             
-            return max(dp(num - 1), dp(num - 2) + points[num])
+        #     return max(dp(num - 1), dp(num - 2) + points[num])
         
-        return dp(max_number)
+        # return dp(max_number)
+        # bottom up case
+        dp = [0] * (max_number + 1)
+        dp[1] = points[1]
+
+        for i in range(2, max_number + 1):
+            dp[i] = max(dp[i - 1], dp[i - 2] + points[i])
+        return dp[max_number]
 
 
 
