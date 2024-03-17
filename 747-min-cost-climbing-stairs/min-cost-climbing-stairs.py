c38f5c4fc6023d9cdea01e439f149ab0b23a0a04
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-16 17:02:21 -0700

    Time: 43 ms (98.12%) | Memory: 16.7 MB (42.28%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -9,11 +9,16 @@ class Solution:
         
         # return dp[-1]
 
-        @cache
-        def dp(i):
-            if i <= 1:
-                return 0
-            one = dp(i - 1) + cost[i - 1]
-            two = dp(i - 2) + cost[i - 2]  
-            return min(one, two)
-        return dp(len(cost))
+        # @cache
+        # def dp(i):
+        #     if i <= 1:
+        #         return 0
+        #     one = dp(i - 1) + cost[i - 1]
+        #     two = dp(i - 2) + cost[i - 2]  
+        #     return min(one, two)
+        # return dp(len(cost))
+
+        one = two = 0
+        for i in range(2, len(cost) + 1):
+            one, two =  min(one + cost[i - 1], two + cost[i - 2]), one
+        return one
