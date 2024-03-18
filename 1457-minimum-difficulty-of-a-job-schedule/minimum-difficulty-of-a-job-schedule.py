8daa3811049c893746d7d6ee20b28cb921b41077
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-18 12:11:53 -0700

    Time: 523 ms (59.94%) | Memory: 17.9 MB (32.64%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
@@ -14,7 +14,7 @@ class Solution:
             best = float('inf')
             # cur is the max difficulty we've seen so far on day `k`
             # init current max with 0
-            cur = 0
+            hardest = 0
             # for jobDifficulty like 6 5 4 3 2 1, 
             # we can have following ways to distribute them into two days
             # 6 | 5 4 3 2 1
@@ -26,13 +26,13 @@ class Solution:
             # given the starting index `i`, 
             # we can only at most choose the jobs till the position `n - d + k - 1`
             for j in range(i, n - d + k):
-                cur = max(cur, jobDifficulty[j])
+                hardest = max(hardest, jobDifficulty[j])
                 # if j-th job is the last job on day `k`, 
                 # the max difficulty for day `k` is `cur`
                 # and we need to start (j + 1)-th job on the next day
                 # the result would be `cur + dp(j + 1, k + 1)`
                 # then we take the min
-                best = min(best, cur + dp(j + 1, k + 1))
+                best = min(best, hardest + dp(j + 1, k + 1))
             return best
         # n < d : you will have free days. hence you cannot find a schedule for the given jobs
         # e.g. Example 2
