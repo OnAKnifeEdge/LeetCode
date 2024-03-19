80a043541b69650adde0c8f3bdb9f563fbde87f4
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-19 11:49:56 -0700

    Time: 192 ms (57.43%) | Memory: 26.1 MB (55.26%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -7,21 +7,36 @@ class Solution:
 
         # base case: 1. if amount == 0, n = 1; 2. if i == len(coins), n = 0
 
-        # #top down apprpoach
+        # # TOP DOWN Apprpoach
 
-        memo = {}
-        def dp(i, amount):
-            if amount == 0:
-                return 1
-            if i == len(coins):
-                return 0
-            if (i, amount) in memo:
-                return memo[(i, amount)]
-            if coins[i] > amount:
-                memo[(i, amount)] = dp(i + 1, amount)
-            else:
-                memo[(i, amount)] = dp(i, amount - coins[i]) + dp(i + 1, amount)
-            return memo[(i, amount)]
+        # memo = {}
+        # def dp(i, amount):
+        #     if amount == 0:
+        #         return 1
+        #     if i == len(coins):
+        #         return 0
+        #     if (i, amount) in memo:
+        #         return memo[(i, amount)]
+        #     if coins[i] > amount:
+        #         memo[(i, amount)] = dp(i + 1, amount)
+        #     else:
+        #         memo[(i, amount)] = dp(i, amount - coins[i]) + dp(i + 1, amount)
+        #     return memo[(i, amount)]
 
-        return dp(0, amount)
-        
+        # return dp(0, amount)
+
+        # # BOTTOM UP Approach
+        n = len(coins)
+
+        dp = [[0] * (amount + 1) for _ in range(n + 1)]
+        for i in range(n):
+            dp[i][0] = 1
+
+        for i in reversed(range(n)):
+            for a in range(1, amount + 1):
+                if coins[i] > a:
+                    dp[i][a] = dp[i + 1][a]
+                else:
+                    dp[i][a] = dp[i][a - coins[i]] + dp[i + 1][a]
+
+        return dp[0][amount]
