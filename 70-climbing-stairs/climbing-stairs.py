1973d2966f57593b829754f1e5f99f1b8fc01644
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-18 21:29:47 -0700

    Time: 19 ms (99.79%) | Memory: 16.5 MB (49.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,15 +1,6 @@
 class Solution:
-
-    def climbStairs(self, n: int) -> int:   
-        if n == 1:
-            return 1
-            
-        # An array that represents the answer to the problem for a given state
-        dp = [0] * (n + 1)
-        dp[1] = 1 # Base cases
-        dp[2] = 2 # Base cases
-        
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2] # Recurrence relation
-
-        return dp[n]
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1,
+        for i in range(n - 1):
+            one, two = one + two, one
+        return one
