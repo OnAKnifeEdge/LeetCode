0d92b6e7754dee0a7a342ce4b2fb5672455493ca
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-18 21:59:19 -0700

    Time: 52 ms (27.26%) | Memory: 16.8 MB (44.72%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -1,19 +1,33 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        cache = {}
+        dp = {}
 
-        def dp(i):
-            if i == len(days):
-                return 0
-            if i in cache:
-                return cache[i]
-            
-            cache[i] = float("inf")
+        for i in reversed(range(len(days))):
+            dp[i] = float('inf')
             for d, c in zip([1, 7, 30], costs):
                 j = i
                 while j < len(days) and days[j] < days[i] + d:
                     j += 1
-                cache[i] = min(cache[i], c + dp(j))
-            return cache[i]
+                dp[i] = min(dp[i], c + dp.get(j, 0))
+        return dp[0]
 
-        return dp(0)
+
+
+
+        # cache = {}
+
+        # def dp(i):
+        #     if i == len(days):
+        #         return 0
+        #     if i in cache:
+        #         return cache[i]
+            
+        #     cache[i] = float("inf")
+        #     for d, c in zip([1, 7, 30], costs):
+        #         j = i
+        #         while j < len(days) and days[j] < days[i] + d:
+        #             j += 1
+        #         cache[i] = min(cache[i], c + dp(j))
+        #     return cache[i]
+
+        # return dp(0)
