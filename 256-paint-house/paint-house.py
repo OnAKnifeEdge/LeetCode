b4c792d6129c5cdf55ca14a8691b345cecd769ac
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-20 12:45:32 -0700

    Time: 52 ms (87.86%) | Memory: 16.6 MB (84.17%) - LeetSync

--- 256-paint-house/paint-house.py
+++ 256-paint-house/paint-house.py
@@ -1,18 +1,21 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
         # costs[i][j] i is house, j is color
-        # a = b = c = 0
+        a = b = c = 0
 
-        # for i in range(len(costs)):
-        #     a = costs[i][0] + min(b, c)
-        #     b = costs[i][1] + min(a, c)
-        #     c = costs[i][2] + min(a, b)
-        
-        # return min(a, b, c)
-        dp = [0, 0, 0]
         for i in range(len(costs)):
-            a = costs[i][0] + min(dp[1], dp[2])
-            b = costs[i][1] + min(dp[0], dp[2])
-            c = costs[i][2] + min(dp[0], dp[1])
-            dp = [a, b, c]
-        return min(dp)
+            aa = costs[i][0] + min(b, c)
+            bb = costs[i][1] + min(a, c)
+            cc = costs[i][2] + min(a, b)
+            a = aa
+            b = bb
+            c = cc
+        return min(a, b, c)
+
+        # dp = [0, 0, 0]
+        # for i in range(len(costs)):
+        #     a = costs[i][0] + min(dp[1], dp[2])
+        #     b = costs[i][1] + min(dp[0], dp[2])
+        #     c = costs[i][2] + min(dp[0], dp[1])
+        #     dp = [a, b, c]
+        # return min(dp)
