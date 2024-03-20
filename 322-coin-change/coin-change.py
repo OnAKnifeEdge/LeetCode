8fdfa5968b51ca24922cade92fe84e7c284a0a24
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-20 15:20:55 -0700

    Time: 847 ms (40.72%) | Memory: 16.9 MB (91.02%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,6 +1,6 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1) # a max value or inf
+        dp = [float('inf')] * (amount + 1) 
         dp[0] = 0
         
         for a in range(1, amount + 1):
@@ -8,5 +8,9 @@ class Solution:
                 if a - c >= 0:
                     dp[a] = min(dp[a], 1 + dp[a -  c])
 
-        return dp[amount] if dp[amount] != amount + 1 else -1
+        if dp[amount] != float('inf'):
+            return dp[amount]
+        else:
+            return -1
+
         
