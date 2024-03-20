ce0fe0cefb92c0d2358cbcd19e5632288e915d6e
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-20 13:47:25 -0700

    Time: 345 ms (97.48%) | Memory: 16.6 MB (92.34%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -6,10 +6,10 @@ class Solution:
             m, n = n, m
             text1, text2 = text2, text1
 
-        previous = [0] * (n + 1)
+        previous = [0] * (m + 1)
 
         for c in reversed(range(n)): # loop long text2
-            current = [0] * (n + 1)
+            current = [0] * (m + 1)
             for r in reversed(range(m)):
                 if text2[c] == text1[r]:
                     current[r] = 1 + previous[r + 1]
