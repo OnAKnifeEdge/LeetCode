75acbf48a962f401332e371a92cf932686cdaa65
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-20 13:47:13 -0700

    Time: 337 ms (97.84%) | Memory: 16.7 MB (89.34%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,59 +1,24 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
 
-          # m, n = len(text1), len(text2)
-        # dp = [[0] * (n + 1) for _ in range(m + 1)]
-        # # 竖着的是 text1, 横着的是 text2t
-        # for r in reversed(range(m)):
-        #     for c in reversed(range(n)):
-        #         if text1[r] == text2[c]:
-        #             dp[r][c] = 1 + dp[r + 1][c + 1]
-        #         else:
-        #             dp[r][c] = max(dp[r + 1][c], dp[r][c + 1])
-
-        # return dp[0][0]
-        
-        # space optimaztion
-        
-        # text1 短， text2 长
-
         m, n = len(text1), len(text2)
-
-        if n < m:
-            text1, text2 = text2, text1
+        if m > n:
             m, n = n, m
+            text1, text2 = text2, text1
 
-        previous = [0] * (n + 1) # 给 text2 用长的
+        previous = [0] * (n + 1)
 
-        for c in reversed(range(n)):
+        for c in reversed(range(n)): # loop long text2
             current = [0] * (n + 1)
             for r in reversed(range(m)):
-                if text2[c] == text1[r]: # 要。之前最长的 + 1
+                if text2[c] == text1[r]:
                     current[r] = 1 + previous[r + 1]
-                else: # 不要。之前的，或者下一个。
+                else:
                     current[r] = max(previous[r], current[r + 1])
+
             previous = current
 
         return current[0]
-
-        # m, n = len(text1), len(text2)
-        # if m > n:
-        #     m, n = n, m
-        #     text1, text2 = text2, text1
-
-        # previous = [0] * (n + 1)
-
-        # for i in reversed(range(n)): # loop long text2
-        #     current = [0] * (n + 1)
-        #     for j in reversed(range(m)):
-        #         if text2[i] == text1[j]:
-        #             current[j] = 1 + previous[j]
-        #         else:
-        #             current[j] = max(previous[j], current[j + 1])
-
-        #     previous = current
-
-        # return current[0]
 
 
         
