f4e8c7f57ca11f1f06499fff439d059a0b176e65
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-22 13:48:20 -0700

    Time: 291 ms (60.92%) | Memory: 73 MB (55.23%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        M, N = len(s), len(t)  # row and col
+
+        dp = [[0] * (N + 1) for _ in range(M + 1)]
+
+        for c in range(N + 1):
+            # 最后一行 s = "", t = "a"
+            dp[M][c] = 0
+
+        for r in range(M + 1):
+            # 最后一列 s = "a", t = ""
+            dp[r][N] = 1
+
+        for i in reversed(range(M)):
+            for j in reversed(range(N)):
+                dp[i][j] = dp[i + 1][j]
+
+                if s[i] == t[j]:
+                    dp[i][j] += dp[i + 1][j + 1]
+        return dp[0][0]  #
