d76a2ab6d4e48b6e1fc9f9751e7ee2cc4007f06c
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-22 21:49:24 -0700

    Time: 865 ms (64.22%) | Memory: 31.1 MB (57.56%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.py
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.py
@@ -2,7 +2,6 @@ class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
         sum_even, sum_odd = 0, 0
         for i in reversed(range(len(nums))):
-            tmp_even = max(sum_odd + nums[i], sum_even)
-            tmp_odd = max(sum_even - nums[i], sum_odd)
-            sum_even, sum_odd = tmp_even, tmp_odd
+            sum_even = max(sum_odd + nums[i], sum_even)
+            sum_odd = max(sum_even - nums[i], sum_odd)
         return sum_even
