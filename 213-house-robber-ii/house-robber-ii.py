11ccef55831bdf68d13726a615467f0d434569b0
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-26 12:31:13 -0700

    Time: 34 ms (74.32%) | Memory: 16.5 MB (34.38%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,19 +1,14 @@
 class Solution:
-
     def rob(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        
         if len(nums) == 1:
             return nums[0]
+        return max(self.helper(nums[:-1]), self.helper(nums[1:]))
+        
 
-        return max(self.rob_simple(nums[:-1]), self.rob_simple(nums[1:]))
-
-
-
-    def rob_simple(self, nums: List[int]) -> int:
-        two_back = 0
-        one_back = 0
+    def helper(self, nums: List[int]) -> int:
+        two_back, one_back = 0, 0 
         for num in nums:
-            two_back, one_back = one_back, max(two_back + num, one_back)
+            one_back, two_back = max(two_back + num, one_back), one_back
         return one_back
