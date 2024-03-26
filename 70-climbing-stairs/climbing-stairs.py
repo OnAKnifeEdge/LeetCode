0601f22109dcc701bdae6e0d18eeb5c7e4da419f
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-26 11:51:31 -0700

    Time: 42 ms (9.04%) | Memory: 16.6 MB (31.50%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,9 +2,7 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         one_back, two_back = 1, 1
         for i in reversed(range(n - 1)):
-            temp = one_back + two_back
-            one_back = two_back
-            two_back = temp
+            one_back, two_back = two_back, one_back + two_back
         return two_back
 
         
