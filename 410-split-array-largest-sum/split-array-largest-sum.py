8f5bf89bf30d421f7ba99d0d86fef735eb242565
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-25 19:12:59 -0700

    Time: 33 ms (95.33%) | Memory: 16.7 MB (53.01%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.py
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -1,27 +1,25 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        n = len(nums)
 
-        prefix_sum = [0] + list(itertools.accumulate(nums))
+        def split_nums(nums: List[int], limit: int) -> int:
+            partitions, total = 1, 0
+            for num in nums:
+                if total + num > limit:
+                    total = 0
+                    partitions += 1
+                total += num
+            return partitions
 
-        @functools.lru_cache(None)
-        def dp(i, cnt):
-            if cnt == 1:
-                return prefix_sum[n] - prefix_sum[i]
-            
-            min_max_sum = prefix_sum[n]
+        low, high = max(nums), sum(nums)
 
-            for j in range(i, n - cnt + 1):
-                first = prefix_sum[j + 1] - prefix_sum[i]
+        while low < high:
+            mid = (low + high) // 2
+            partitions = split_nums(nums, mid)
+            if partitions <= k:
+                high = mid
+            else:
+                low = mid + 1
+        return high
 
-                largest = max(first, dp(j + 1, cnt - 1))
 
-                min_max_sum = min(min_max_sum, largest)
-
-                if first >= min_max_sum:
-                    break
-
-            return min_max_sum
-
-        return dp(0, k)
         
