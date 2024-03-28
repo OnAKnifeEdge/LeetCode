756a4eed0678ef6b790570c76a70b1fd6eb74ef8
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-28 14:38:34 -0700

    Time: 75 ms (99.41%) | Memory: 16.6 MB (95.07%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -3,6 +3,6 @@ class Solution:
         dp = [0] * (amount + 1)
         dp[0] = 1
         for coin in coins:
-            for a in range(coin, amount + 1):
-                dp[a] += dp[a - coin]
+            for amt in range(coin, amount + 1):
+                dp[amt] += dp[amt - coin]
         return dp[amount]
