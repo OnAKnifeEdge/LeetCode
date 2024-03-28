5f9892e4e40d4f44f24bff61d23335118cf8cb80
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-28 16:00:28 -0700

    Time: 640 ms (5.02%) | Memory: 96.1 MB (5.03%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -3,11 +3,10 @@ class Solution:
         ROWS, COLS = len(matrix), len(matrix[0])
         dp = {} # (r, c) -> LIP
 
+        @cache
         def dfs(r, c, pre):
             if (r < 0 or r == ROWS or c < 0 or c == COLS or matrix[r][c] <= pre):
                 return 0
-            if (r, c) in dp:
-                return dp[(r, c)]
 
             result = 1
             result = max(1 + dfs(r + 1, c, matrix[r][c]), result)
@@ -19,7 +18,7 @@ class Solution:
 
         for r in range(ROWS):
             for c in range(COLS):
-                dfs(r, c, -1)
+                dfs(r, c, float('-inf'))
         return max(dp.values())
 
 
