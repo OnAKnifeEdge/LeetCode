1fcce21f68b6bc0cf56a75aa0f5d1a863792b715
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-28 16:36:01 -0700

    Time: 335 ms (45.74%) | Memory: 72.9 MB (56.26%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -5,11 +5,11 @@ class Solution:
         dp = [[0] * (N + 1) for _ in range(M + 1)]
 
         for c in range(N + 1):
-            # 最后一行 s = "", t = "a"
+            # 最后一行 s[] = "", t[]= t[-1]
             dp[M][c] = 0
 
         for r in range(M + 1):
-            # 最后一列 s = "a", t = ""
+            # 最后一列 s[] = s[-1], t = ""
             dp[r][N] = 1
 
         for i in reversed(range(M)):
@@ -18,4 +18,4 @@ class Solution:
 
                 if s[i] == t[j]:
                     dp[i][j] += dp[i + 1][j + 1]
-        return dp[0][0]  #
+        return dp[0][0]  
