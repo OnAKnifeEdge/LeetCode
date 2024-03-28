518644c6f386532b0dc2041661fac25fbfb07498
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-28 14:36:55 -0700

    Time: 94 ms (85.84%) | Memory: 16.7 MB (95.07%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,32 +1,7 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # If we choose to take a coin with value, 
-        # we are now searching for a combination of coins that sum up to amount - value. 
-        # If we choose to skip the coin, 
-        # we are still looking for a combination of coins that sum up to amount, but with fewer coins.
-
-        # base case: 1. if amount == 0, n = 1; 2. if i == len(coins), n = 0
-
-        # # TOP DOWN Apprpoach
-
-        # memo = {}
-        # def dp(i, amount):
-        #     if amount == 0:
-        #         return 1
-        #     if i == len(coins):
-        #         return 0
-        #     if (i, amount) in memo:
-        #         return memo[(i, amount)]
-        #     if coins[i] > amount:
-        #         memo[(i, amount)] = dp(i + 1, amount)
-        #     else:
-        #         memo[(i, amount)] = dp(i, amount - coins[i]) + dp(i + 1, amount)
-        #     return memo[(i, amount)]
-
-        # return dp(0, amount)
-
-        # # BOTTOM UP Approach
-        dp = [1] + [0 for _ in range(amount)]
+        dp = [0] * (amount + 1)
+        dp[0] = 1
         for coin in coins:
             for a in range(coin, amount + 1):
                 dp[a] += dp[a - coin]
