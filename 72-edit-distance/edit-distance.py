cd0ff9f6afe8efa93c512bdae5ffcb23b45168b5
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-28 16:49:54 -0700

    Time: 105 ms (51.47%) | Memory: 20.2 MB (39.95%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,21 +1,22 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp = [[0] * (len(word2) + 1) for _ in range(len(word1) + 1)]
+        l1, l2 = len(word1), len(word2)
+        dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+        # dp[i][j] means min # of operations to convert word1[0..i) to word2[0..j)
+        
+        for i in range(1, l1 + 1):
+            dp[i][0] = i # 第一列
 
-        for c in range(len(word2) + 1):
-            # 最后一行
-            dp[len(word1)][c] = len(word2) - c
-        for r in range(len(word1) + 1):
-            # 最后一列
-            dp[r][len(word2)] = len(word1) - r
+        for j in range(1, l2 + 1):
+            dp[0][j] = j # 第一行
 
-        for r in reversed(range(len(word1))):
-            for c in reversed(range(len(word2))):
-                if word1[r] == word2[c]:
-                    dp[r][c] = dp[r + 1][c + 1]
+        for r in range(1, l1 + 1):
+            for c in range(1, l2 + 1):
+                if word1[r - 1] == word2[c - 1]:
+                    dp[r][c] = dp[r - 1][c - 1]
                 else:
-                    insert_case = dp[r + 1][c]
-                    delete_case = dp[r][c + 1]
-                    replace_case = dp[r + 1][c + 1]
+                    insert_case = dp[r - 1][c]
+                    delete_case = dp[r][c - 1]
+                    replace_case = dp[r - 1][c - 1]
                     dp[r][c] = 1 + min(insert_case, delete_case, replace_case)
-        return dp[0][0]
+        return dp[l1][l2]
