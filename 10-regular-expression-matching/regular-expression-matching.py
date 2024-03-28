6be1efc4365bafca1bcf49f18b75b5eeef64bbf7
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-28 16:59:23 -0700

    Time: 49 ms (37.83%) | Memory: 16.9 MB (22.24%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -2,17 +2,18 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         memo = {}
 
+        @cache
         def dp(i, j):
-            if (i, j) not in memo:
-                if j == len(p):
-                    result = i == len(s)
+
+            if j == len(p):
+                result = i == len(s)
+            else:
+                first_match = i < len(s) and p[j] in {s[i], "."}
+                if j + 1 < len(p) and p[j + 1] == "*":
+                    result = dp(i, j + 2) or first_match and dp(i + 1, j)
                 else:
-                    first_match = i < len(s) and p[j] in {s[i], "."}
-                    if j + 1 < len(p) and p[j + 1] == "*":
-                        result = dp(i, j + 2) or first_match and dp(i + 1, j)
-                    else:
-                        result = first_match and dp(i + 1, j + 1)
-                memo[i, j] = result
+                    result = first_match and dp(i + 1, j + 1)
+            memo[i, j] = result
             return memo[i, j]
 
         return dp(0, 0)
