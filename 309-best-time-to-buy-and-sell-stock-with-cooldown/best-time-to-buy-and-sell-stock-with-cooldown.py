7a47c21ed9cfc6e33813139b42be31679cf069b0
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-03-28 14:31:13 -0700

    Time: 43 ms (67.39%) | Memory: 16.9 MB (82.96%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,14 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        cooldown, sell, hold = 0, 0, float('-inf') # 股票不可能無中生有，免費獲得利潤，所以 hold 要記得初始化成負無窮大。
+        cooldown, sold, hold = 0, 0, float('-inf') # 股票不可能無中生有，免費獲得利潤，所以 hold 要記得初始化成負無窮大。
 
         for price in prices:
-            prev_cooldown, prev_sell, prev_hold = cooldown, sell, hold
+            prev_sold = sold
 
             # cooldown on day i: cooldown of day (i - 1), or sell out of day (i - 1). day i is a cooling day 
-            cooldown = max(prev_cooldown, prev_sell, prev_hold)
-            sell = prev_hold + price
-            hold = max(prev_hold, prev_cooldown - price)
+            sold = hold + price
+            hold = max(hold, cooldown - price)
+            cooldown = max(cooldown, prev_sold)
 
-        return max(sell, cooldown)
+        return max(sold, cooldown)
         
