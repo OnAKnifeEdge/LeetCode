e2852599d9ac6765b95d385b3538d254c73a69f1
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-04 15:38:43 -0700

    Time: 118 ms (91.20%) | Memory: 18.1 MB (31.32%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,7 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         slow = 0
-        for fast, num in enumerate(nums):
-            if num != 0:
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
                 nums[slow], nums[fast] = nums[fast], nums[slow]
                 slow += 1
