3729853f75de29a1beac2536957bd9249173336c
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-04 15:31:39 -0700

    Time: 233 ms (99.58%) | Memory: 27.3 MB (97.31%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,30 +5,45 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        left = None
         slow, fast = head, head
-
         while fast and fast.next:
             fast = fast.next.next
+            left, left.next, slow = slow, left, slow.next
+        if fast:
             slow = slow.next
-
-        left, right = head, self.reverse(slow)
-
-
-        while right:
-            if left.val != right.val:
-                return False
+        while left and left.val == slow.val:
+            slow = slow.next
             left = left.next
-            right = right.next
+        
+        return not left
 
-        return True
 
 
-    def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pre, curr = None, head
-        while curr:
-            nxt = curr.next
-            curr.next = pre
-            pre = curr
-            curr = nxt
-        return pre
+    #     slow, fast = head, head
+
+    #     while fast and fast.next:
+    #         fast = fast.next.next
+    #         slow = slow.next
+
+    #     left, right = head, self.reverse(slow)
+
+
+    #     while right:
+    #         if left.val != right.val:
+    #             return False
+    #         left = left.next
+    #         right = right.next
+
+    #     return True
+
+
+    # def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     pre, curr = None, head
+    #     while curr:
+    #         nxt = curr.next
+    #         curr.next = pre
+    #         pre = curr
+    #         curr = nxt
+    #     return pre
         
