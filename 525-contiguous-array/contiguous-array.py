16c73990b5d8e6429d2d3badce9240f7dd7fb63e
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-05 13:29:38 -0700

    Time: 618 ms (61.56%) | Memory: 22 MB (89.42%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,41 +1,37 @@
 class Solution:
-    # def findMaxLength(self, nums: List[int]) -> int:
-        # zero, one = 0, 0
-        # diff_map = {}
-        # result = 0
+    def findMaxLength(self, nums: List[int]) -> int:     
+        d = {}
+        result = 0
+        diff = 0
 
-        # for i, num in enumerate(nums):
-        #     if num == 0:
-        #         zero += 1
-        #     elif num == 1:
-        #         one += 1
-            
-        #     if one - zero not in diff_map:
-        #         diff_map[one - zero] = i
+        for idx, num in enumerate(nums):
+            diff += 1 if num else -1 # when num is 1 increment when num is 0 decrement
 
-        #     if one == zero:
-        #         result = one + zero
-        #     else:
-        #         idx = diff_map.get(one - zero)
-        #         result = max(result, i - idx)
+            if diff == 0:
+                result = max(result, idx + 1)
+            elif diff in d:
+                result = max(result, idx - d[diff])
+            else:
+                d[diff] = idx
 
-        # return result
+        return result
 
-        # generator https://leetcode.com/problems/contiguous-array/solutions/4883501/97-90-short-and-clear-with-generator-of-cumulated-differences
-        def findMaxLength(self, nums: List[int]) -> int:
-            def cumulated_diffs():
-                diff = 0
-                for num in nums:
-                    yield diff
-                    diff += 1 if num else -1
-                yield diff
 
-            d = {}
-            result = 0
-            for idx, diff in enumerate(cumulated_diffs()):
-                if diff in d:
-                    result = max(result, idx - d[diff])
-                else:
-                    d[diff] = idx
+        # # generator https://leetcode.com/problems/contiguous-array/solutions/4883501/97-90-short-and-clear-with-generator-of-cumulated-differences
+        # def findMaxLength(self, nums: List[int]) -> int:
+        #     def cumulated_diffs():
+        #         diff = 0
+        #         for num in nums:
+        #             yield diff
+        #             diff += 1 if num else -1
+        #         yield diff
 
-            return result
+        #     d = {}
+        #     result = 0
+        #     for idx, diff in enumerate(cumulated_diffs()):
+        #         if diff in d:
+        #             result = max(result, idx - d[diff])
+        #         else:
+        #             d[diff] = idx
+
+        #     return result
