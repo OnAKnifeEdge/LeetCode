feede9411614aae158bd84e66d965148420d5e84
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-04 17:27:41 -0700

    Time: 28 ms (97.98%) | Memory: 16.6 MB (65.46%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,7 +4,20 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         # matrix[:] = map(list, zip(*matrix[::-1]))
-        matrix.reverse()
-        for i in range(len(matrix)):
+
+        # matrix.reverse()
+        # for i in range(len(matrix)):
+        #     for j in range(i):
+        #         matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        n = len(matrix)
+
+        # transpose
+        for i in range(n):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # flip
+        for row in matrix:
+            for j in range(n // 2):
+                row[j], row[~j] = row[~j], row[j]
