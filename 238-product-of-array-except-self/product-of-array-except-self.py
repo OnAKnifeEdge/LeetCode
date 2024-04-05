6f86a278957d233a52dea9a4de710b656f3ad73c
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-04 23:38:53 -0700

    Time: 291 ms (5.07%) | Memory: 26.3 MB (5.20%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,30 +1,30 @@
 class Solution:
-    # def productExceptSelf1(self, nums: List[int]) -> List[int]:
-    #     left = [1] * len(nums)
-    #     for i in range(1, len(nums)):
-    #         left[i] = nums[i - 1] * left[i - 1]
-    #         # 1， 1， 2， 6
-    #     right = [1] * len(nums)
-    #     for j in range(len(nums) - 2, -1, -1):
-    #         right[j] = nums[j + 1] * right[j + 1]
-    #         # 24，12，4，1
-    #     return [a * b for a, b in zip(left, right)]
-
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        answer = [1] * n
-        prefix = suffix = 1
+        left = [1] * len(nums)
+        for i in range(1, len(nums)):
+            left[i] = nums[i - 1] * left[i - 1]
+            # 1， 1， 2， 6
+        right = [1] * len(nums)
+        for j in reversed(range(len(nums) - 1)):
+            right[j] = nums[j + 1] * right[j + 1]
+            # 24，12，4，1
+        return [a * b for a, b in zip(left, right)]
 
-        # [1, 2, 3, 4]
+    # def productExceptSelf(self, nums: List[int]) -> List[int]:
+    #     n = len(nums)
+    #     answer = [1] * n
+    #     prefix = suffix = 1
 
-        for i in range(n):
-            answer[i] = prefix
-            prefix *= nums[i]
-            # [1, 1, 2, 6]
+    #     # [1, 2, 3, 4]
+
+    #     for i in range(n):
+    #         answer[i] = prefix
+    #         prefix *= nums[i]
+    #         # [1, 1, 2, 6]
         
-        for i in reversed(range(n)):
-            answer[i] *= suffix
-            #postfix= [1, 4, 12, 24]
-            suffix *= nums[i]
+    #     for i in reversed(range(n)):
+    #         answer[i] *= suffix
+    #         #postfix= [1, 4, 12, 24]
+    #         suffix *= nums[i]
         
-        return answer
+    #     return answer
