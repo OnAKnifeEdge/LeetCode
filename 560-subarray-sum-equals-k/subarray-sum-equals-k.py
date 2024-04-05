7bd5a53fe4dd98c55ed691717891676c5d69ebaf
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-05 14:36:16 -0700

    Time: 232 ms (48.30%) | Memory: 19.5 MB (38.71%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,29 +1,24 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # https://leetcode.com/problems/subarray-sum-equals-k/solutions/3777004/why-sum-k-read-this-to-understand/
-        # # d[sum] -> OccuranceCount
-        # d = {0: 1}
-        # s, count = 0, 0
-        # for num in nums:
-        #     s += num
-        #     if s - k in d:
-        #         count += d[s - k]
-        #     d[s] = d.get(s, 0) + 1
-        # return count
-
-        count, prefix_sum = 0, 0
         d = {}
-        
-        for num in nums:
+        prefix_sum = 0
+        cnt = 0
+
+        for i, num in enumerate(nums):
             prefix_sum += num
 
             if prefix_sum == k:
-                count += 1
+                cnt += 1
             
-            count += d.get(prefix_sum - k, 0)
-            
-            d[prefix_sum] = d.get(prefix_sum, 0) + 1
-                
-        return count
+            # If there is a previous prefix sum such that current_sum - previous_sum = k,
+            # add the number of occurrences of that previous_sum to count.
+            if (prefix_sum - k) in d:
+                cnt += d[prefix_sum - k]
 
-        
+            # Update the occurrences of the prefix sum.
+            if prefix_sum in d:
+                d[prefix_sum] += 1
+            else:
+                d[prefix_sum] = 1
+
+        return cnt
