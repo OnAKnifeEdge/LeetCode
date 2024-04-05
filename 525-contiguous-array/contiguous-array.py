a54567b940116e10eae3fed37a4cd7371ef6998e
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-05 13:16:55 -0700

    Time: 638 ms (22.14%) | Memory: 22.1 MB (89.42%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,51 +1,41 @@
 class Solution:
-    def findMaxLength(self, nums: List[int]) -> int:
-        zero, one = 0, 0
-        diff_map = {}
-        result = 0
+    # def findMaxLength(self, nums: List[int]) -> int:
+        # zero, one = 0, 0
+        # diff_map = {}
+        # result = 0
 
-        for i, num in enumerate(nums):
-            if num == 0:
-                zero += 1
-            elif num == 1:
-                one += 1
+        # for i, num in enumerate(nums):
+        #     if num == 0:
+        #         zero += 1
+        #     elif num == 1:
+        #         one += 1
             
-            if one - zero not in diff_map:
-                diff_map[one - zero] = i
+        #     if one - zero not in diff_map:
+        #         diff_map[one - zero] = i
 
-            if one == zero:
-                result = one + zero
-            else:
-                idx = diff_map.get(one - zero)
-                result = max(result, i - idx)
+        #     if one == zero:
+        #         result = one + zero
+        #     else:
+        #         idx = diff_map.get(one - zero)
+        #         result = max(result, i - idx)
 
-        return result
-
-
-        
-
-
-
-
-
-
+        # return result
 
         # generator https://leetcode.com/problems/contiguous-array/solutions/4883501/97-90-short-and-clear-with-generator-of-cumulated-differences
-        # def findMaxLength(self, nums: List[int]) -> int:
-        #     def cumulated_diffs():
-        #         prev = 0
-        #         for num in nums:
-        #             yield prev
-        #             prev += 1 if num else -1
-        #         yield prev
+        def findMaxLength(self, nums: List[int]) -> int:
+            def cumulated_diffs():
+                diff = 0
+                for num in nums:
+                    yield diff
+                    diff += 1 if num else -1
+                yield diff
 
-        #     dct = {}
-        #     ans = 0
-        #     for ind, dif in enumerate(cumulated_diffs()):
-        #         if dif in dct:
-        #             if ans < ind - dct[dif]:
-        #                 ans = ind - dct[dif]
-        #         else:
-        #             dct[dif] = ind
+            d = {}
+            result = 0
+            for idx, diff in enumerate(cumulated_diffs()):
+                if diff in d:
+                    result = max(result, idx - d[diff])
+                else:
+                    d[diff] = idx
 
-        #     return ans
+            return result
