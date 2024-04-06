3232bc9fa0527d9cfa274abffad76d24322bf034
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-05 23:04:06 -0700

    Time: 223 ms (85.18%) | Memory: 21.4 MB (66.61%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -3,15 +3,15 @@ class Solution:
         d = {}
         prefix_sum = 0
         cnt = 0
-
-
         for i, num in enumerate(nums):
             prefix_sum += num
+            # Calculate the mod, adjusting immediately for negative values
             mod = (prefix_sum % k + k) % k # handle the negative cases
 
             if mod == 0:
                 cnt += 1
 
+            # If we've seen this mod before, it contributes to additional subarrays
             if mod in d:
                 cnt += d[mod]
 
