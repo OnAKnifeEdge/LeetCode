9a52820e1bc2d490d406996785fb677715948572
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-05 22:49:40 -0700

    Time: 797 ms (93.85%) | Memory: 30.7 MB (29.54%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        # https://leetcode.com/problems/subarray-product-less-than-k/
+        # two-pointer: find the maximum length subarray which has the sum equal to sum(nums) - x. 
+        total = sum(nums)
+        target = total - x
+
+        n = len(nums)
+
+        if target == 0:
+            return n
+        
+        left = 0
+        prefix_sum = 0
+        ops = float('inf')
+
+        for right in range(n):
+            prefix_sum += nums[right]
+
+            while left <= right and prefix_sum > target:
+                prefix_sum -= nums[left]
+                left += 1
+
+            if prefix_sum == target:
+                l = n - (right - left + 1)
+                ops = min(ops, l)
+
+        return ops if ops != float('inf') else -1
+
