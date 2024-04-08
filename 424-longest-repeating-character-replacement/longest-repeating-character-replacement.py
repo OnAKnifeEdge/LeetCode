feed3b2269075d58be6766a0174d97dc8a6abcb8
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-07 19:28:59 -0700

    Time: 71 ms (92.31%) | Memory: 16.6 MB (49.43%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,26 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = 0
+        d = collections.defaultdict(int)
+        max_frequency = 0
+        result = 0
+        for right in range(len(s)):
+            c = s[right]
+            d[c] += 1
+
+            max_frequency = max(max_frequency, d[c])
+
+
+            is_valid = (right - left + 1 - max_frequency) <= k
+
+            if not is_valid:
+                c = s[left]
+                d[c] -= 1
+                left += 1
+            
+            result = right - left + 1
+
+        return result
+
+        
+        
