b52604eada8963bb3683556c03608f5f5b3964d5
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-15 21:18:13 -0700

    Time: 69 ms (87.71%) | Memory: 17.3 MB (93.26%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.py
+++ 566-reshape-the-matrix/reshape-the-matrix.py
@@ -1,15 +1,31 @@
 class Solution:
     def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
+        # if len(nums) == 0 or r * c != len(nums) * len(nums[0]):
+        #     return nums
+
+        # reshaped = []  # Initialize the reshaped matrix
+        # temp = []
+
+        # for row in nums:
+        #     for val in row:
+        #         temp.append(val)
+        #         if len(temp) == c:
+        #             reshaped.append(temp)
+        #             temp = []
+        # return reshaped
+
         if len(nums) == 0 or r * c != len(nums) * len(nums[0]):
             return nums
 
-        reshaped = []  # Initialize the reshaped matrix
-        temp = []
+        rows, cols = len(nums), len(nums[0])  # The dimensions of the original matrix
 
-        for row in nums:
-            for val in row:
-                temp.append(val)
-                if len(temp) == c:
-                    reshaped.append(temp)
-                    temp = []
+        reshaped = [[0] * c for _ in range(r)]
+        cnt = 0  
+        
+        for i in range(rows):
+            for j in range(cols):
+                ii, jj = divmod(cnt, c)
+                reshaped[ii][jj] = nums[i][j]  
+                cnt += 1  
+                
         return reshaped
