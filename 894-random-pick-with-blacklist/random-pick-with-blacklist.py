e186fd2b5ecfeeb7dce22171e9e34e6b64669f78
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-16 11:42:06 -0700

    Time: 200 ms (86.64%) | Memory: 27.7 MB (50.61%) - LeetSync

--- 894-random-pick-with-blacklist/random-pick-with-blacklist.py
+++ 894-random-pick-with-blacklist/random-pick-with-blacklist.py
@@ -7,17 +7,17 @@ class Solution:
 
         last = n - 1
         for b in blacklist:
-            if b >= self.l:
+            if b >= self.l: # If 'b' is not affecting the range [0, self.l), ignore it.
                 continue
             
             while last in blacklist_set:
-                last -= 1
-            self.d[b] = last
-            last -= 1
+                last -= 1  # Decrease 'last' until it's not in the blacklist.
+            self.d[b] = last # Map 'b' to the 'last' non-blacklisted value.
+            last -= 1 # Prepare 'last' for the potential next mapping.
 
     def pick(self) -> int:
-        k = random.randint(0, self.l - 1)
-        return self.d.get(k, k)
+        k = random.randint(0, self.l - 1) # Generate a random index in the allowed range.
+        return self.d.get(k, k) # Return the remapped value if 'k' is blacklisted, otherwise 'k' itself.
         
 
 
