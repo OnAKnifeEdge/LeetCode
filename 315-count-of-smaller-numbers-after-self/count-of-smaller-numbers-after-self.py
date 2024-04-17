144fbbbf409ece2aa1c6745cd6efc8bae2ce408f
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-16 23:06:20 -0700

    Time: 1709 ms (63.87%) | Memory: 45.9 MB (18.10%) - LeetSync

---
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
@@ -0,0 +1,45 @@
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        count = [0] * len(nums)
+        arr = [(v, i) for i, v in enumerate(nums)]
+
+        def merge_sort(arr):
+            if len(arr) <= 1:
+                return arr
+            pivot = len(arr) // 2
+            left = merge_sort(arr[:pivot])
+            right = merge_sort(arr[pivot:])
+            return merge(left, right)
+
+        def merge(left, right):
+            merged = []
+            i, j = 0, 0
+            while i < len(left) and j < len(right):
+
+                left_value, left_idx = left[i]
+                right_value, _ = right[j]
+
+                if left_value <= right_value:
+                    merged.append(left[i])
+                    count[left_idx] += j
+                    i += 1
+                else:
+                    merged.append(right[j])
+                    j += 1
+
+            while i < len(left):
+                left_value, left_idx = left[i]
+                merged.append(left[i])
+                count[left_idx] += j
+                i += 1
+            
+            merged.extend(right[j:])
+
+            return merged
+        
+        merge_sort(arr)
+        return count
+    
+
+
+        
