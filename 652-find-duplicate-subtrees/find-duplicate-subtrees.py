a4a5cb77835615957010dcdb1e36362784c55efe
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-16 18:35:05 -0700

    Time: 42 ms (88.54%) | Memory: 17.2 MB (90.95%) - LeetSync

--- 652-find-duplicate-subtrees/find-duplicate-subtrees.py
+++ 652-find-duplicate-subtrees/find-duplicate-subtrees.py
@@ -6,20 +6,27 @@
 #         self.right = right
 class Solution:
     def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
-        count = {} # key triplet: val count
+        count = {} # key triplet tuple: val count
         duplicates = []
+        tree_id = 1
+        trees = {} # key triplet tuple: val id
 
         def serialize(node: Optional[TreeNode]) -> str:
             if not node:
-                return "#"
-            left = serialize(node.left)
-            right = serialize(node.right)
-            subtree = f'{left},{right},{node.val}'
+                return "0"
+            left_id = serialize(node.left)
+            right_id = serialize(node.right)
+            subtree = (node.val, left_id, right_id)
             count[subtree] = count.get(subtree, 0) + 1
+
+            if subtree not in trees:
+                nonlocal tree_id
+                trees[subtree] = tree_id
+                tree_id += 1
 
             if count[subtree] == 2:
                 duplicates.append(node)
-            return subtree
+            return trees[subtree]
 
         serialize(root)
         return duplicates
