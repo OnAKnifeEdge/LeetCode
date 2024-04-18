b808c45ab19df031524ad2fec06647239bf19e37
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-18 14:38:04 -0700

    Time: 63 ms (15.47%) | Memory: 19.8 MB (66.19%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -5,43 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # right tree min value
-    def successor(self, root: TreeNode) -> int:
-        # 下一个大的数：右的最左
-        root = root.right
-        while root.left:
-            root = root.left
-        return root.val
-    
-    # left tree max value
-    def predecessor(self, root: TreeNode) -> int:
-        # 上一个小的数：左的最右
-        root = root.left
-        while root.right:
-            root = root.right
-        return root.val
-        
-
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
             return None
-
-        if key > root.val:
+        if root.val == key:
+            if root.left == None:
+                return root.right
+            if root.right == None:
+                return root.left
+            # has left and right: replace with successor or replace with predecessor
+            successor = self.get_successor(root)
+            root.right = self.deleteNode(root.right, successor.val)
+            successor.left = root.left
+            successor.right = root.right
+            root = successor
+        elif root.val < key:
             root.right = self.deleteNode(root.right, key)
-        
-        elif key < root.val:
-            root.left = self.deleteNode(root.left, key)
-
         else:
-            if not root.left and not root.right:
-                root = None
-            elif root.right:
-                # 下一个大的数：右的最左
-                root.val = self.successor(root)
-                root.right = self.deleteNode(root.right, root.val)
-            else:
-                # 上一个小的数：左的最右
-                root.val = self.predecessor(root)
-                root.left = self.deleteNode(root.left, root.val)
+            root.left = self.deleteNode(root.left, key)
         return root
+
+    def get_successor(self, node: Optional[TreeNode]) ->  Optional[TreeNode]:
+        node = node.right
+        while node.left:
+            node = node.left
+        return node
+
+        
         
