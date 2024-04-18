8d681d3bd6bfa3560dba7384ff58e17ce6466f58
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-18 14:52:53 -0700

    Time: 60 ms (30.27%) | Memory: 19.9 MB (66.19%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -14,11 +14,16 @@ class Solution:
             if root.right == None:
                 return root.left
             # has left and right: replace with successor or replace with predecessor
+
+            # Node with two children: Get the inorder successor
+            # (smallest in the right subtree)
             successor = self.get_successor(root)
+            root.val = successor.val
             root.right = self.deleteNode(root.right, successor.val)
-            successor.left = root.left
-            successor.right = root.right
-            root = successor
+            # root.right = self.deleteNode(root.right, successor.val)
+            # successor.left = root.left
+            # successor.right = root.right
+            # return successor
         elif root.val < key:
             root.right = self.deleteNode(root.right, key)
         else:
@@ -26,6 +31,8 @@ class Solution:
         return root
 
     def get_successor(self, node: Optional[TreeNode]) ->  Optional[TreeNode]:
+        # The successor is the smallest node in the right subtree,
+        # which is the leftmost child of the node's right subtree
         node = node.right
         while node.left:
             node = node.left
