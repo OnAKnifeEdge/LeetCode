0e6000940608d71451f287b6d54f5107f1276370
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-19 11:49:07 -0700

    Time: 54 ms (29.40%) | Memory: 19.9 MB (59.58%) - LeetSync

---
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        a, b = p, q
+        while a != b:
+            if a:
+                a = a.parent
+            else:
+                a = q
+            if b:
+                b = b.parent
+            else:
+                b = p
+            
+        return a
+        
