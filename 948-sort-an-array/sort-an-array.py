89cf3c61865ba21faa277d149d3cef3c5fe9c333
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-18 18:15:27 -0700

    Time: 1061 ms (70.80%) | Memory: 23.7 MB (90.18%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,22 +1,34 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        if len(nums) <= 1:
-            return nums
-        pivot = len(nums) // 2
-        left = self.sortArray(nums[:pivot])
-        right = self.sortArray(nums[pivot:])
-        return self.merge(left, right)
+        self.quick_sort(nums, 0, len(nums) - 1)
+        return nums
+        
 
-    def merge(self, left: List[int], right: List[int]) -> List[int]:
-        i, j = 0, 0
-        r = []
-        while i < len(left) and j < len(right):
-            if left[i] <= right[j]:
-                r.append(left[i])
+    def quick_sort(self, nums, left, right):
+        if left >= right:
+            return
+        lt, gt = self.partition(nums, left, right)
+        self.quick_sort(nums, left, lt - 1)
+        self.quick_sort(nums, gt + 1, right)
+
+    def partition(self, nums, left, right):
+        idx = random.randint(left, right)
+        pivot = nums[idx]
+        nums[idx], nums[left] = nums[left], nums[idx]
+
+        lt = left
+        gt = right
+
+        i = left + 1
+
+        while i <= gt:
+            if nums[i] < pivot:
+                nums[i], nums[lt] = nums[lt], nums[i]
                 i += 1
+                lt += 1
+            elif nums[i] > pivot:
+                nums[i], nums[gt] = nums[gt], nums[i]
+                gt -= 1
             else:
-                r.append(right[j])
-                j += 1
-        r.extend(left[i:])
-        r.extend(right[j:])
-        return r
+                i += 1
+        return lt, gt
