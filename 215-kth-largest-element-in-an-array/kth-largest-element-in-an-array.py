1c0acbe6b14100b6d6f7c2ce1fc04494e35d485b
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-18 18:56:54 -0700

    Time: 502 ms (23.29%) | Memory: 37.2 MB (5.03%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,7 +1,6 @@
 class Solution:
     # https://leetcode.com/problems/sort-an-array/ quick sort
     def findKthLargest(self, nums: List[int], k: int) -> int:
-
         def partition(left: int, right: int) -> int:
             idx = random.randint(left, right)
             pivot = nums[idx]
