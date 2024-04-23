73a55790de5dd8d6b0e9fb0a8284c47d11e0cbbb
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-22 18:34:23 -0700

    Time: 48 ms (5.67%) | Memory: 16.5 MB (59.57%) - LeetSync

---
+++ 1011-flip-binary-tree-to-match-preorder-traversal/flip-binary-tree-to-match-preorder-traversal.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flipMatchVoyage(self, root: Optional[TreeNode], voyage: List[int]) -> List[int]:
+        flipped = []
+        idx = 0
+        can_flip = True
+
+        def dfs(node):
+            nonlocal can_flip, idx
+            if not node or not can_flip:
+                return
+            if node.val != voyage[idx]:
+                can_flip = False
+                return
+            idx += 1
+            if idx <= len(voyage) and node.left and node.left.val != voyage[idx]:
+                flipped.append(node.val)
+                dfs(node.right)
+                dfs(node.left)
+            else:
+                dfs(node.left)
+                dfs(node.right)
+        dfs(root)
+        return flipped if can_flip else [-1]
