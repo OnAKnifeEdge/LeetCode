bb656013a27f5cda864633091645f980c01e8a4d
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-22 20:48:21 -0700

    Time: 35 ms (66.08%) | Memory: 16.5 MB (68.78%) - LeetSync

---
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        parent_x, parent_y = None, None
+        d_x, d_y = 0, 0
+
+        def dfs(node, parent, depth):
+            nonlocal parent_x, parent_y, d_x, d_y
+            if not node:
+                return
+            if node.val == x:
+                parent_x = parent
+                d_x = depth
+            elif node.val == y:
+                parent_y = parent
+                d_y = depth
+            dfs(node.left, node, depth + 1)
+            dfs(node.right, node, depth + 1)
+        
+        dfs(root, None, 0)
+        if d_x == d_y and parent_x != parent_y:
+            return True
+        return False
+        
