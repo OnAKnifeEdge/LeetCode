bf1e15d0d19058f7a3320316fa6b4e6921275d97
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-22 20:35:00 -0700

    Time: 38 ms (49.54%) | Memory: 16.8 MB (23.73%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -6,39 +6,32 @@
 #         self.right = right
 class Solution:
 
-    class Position(NamedTuple):
-        val: int
-        row: int
-        col: int
-
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
 
-        self.nodes = []
+        self.d = defaultdict(list)
+        self.min = 0
+        self.max = 0
 
         def traverse(node, row, col):
             if not node:
                 return
-            self.nodes.append(self.Position(val=node.val, row=row, col=col))
+            self.d[col].append((row, node.val))
+            self.min = min(col, self.min)
+            self.max = max(col, self.max)
             traverse(node.left, row + 1, col - 1)
             traverse(node.right, row + 1, col + 1)
 
         traverse(root, 0, 0)
-        self.nodes.sort(key = lambda x: (x.col, x.row, x.val))
-
-
-        column_map = defaultdict(list)
-        min_col, max_col = 0, 0  # Initializing to track the range of columns.
-
-        for node in self.nodes:
-            column_map[node.col].append(node.val)
-            min_col = min(min_col, node.col)  # Update the minimum column.
-            max_col = max(max_col, node.col)  # Update the maximum column.
 
         # Build the result by iterating over the computed range.
-        result = [column_map[col] for col in range(min_col, max_col + 1)]
+
+        result = []
+        for col in range(self.min, self.max + 1):
+            # sort first by 'row', then by 'value', in ascending order using a lambda function as the sort key
+            result.append([val for row, val in sorted(self.d[col])])
 
         return result
 
-        return result
+
 
         
