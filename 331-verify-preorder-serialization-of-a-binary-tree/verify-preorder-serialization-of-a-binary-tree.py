0f63d3c3062dae3de335c3f802dfc1f68bbaa3b5
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-25 18:23:47 -0700

    Time: 42 ms (24.30%) | Memory: 16.6 MB (43.84%) - LeetSync

---
+++ 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValidSerialization(self, preorder: str) -> bool:
+        # 每个非空的二叉树节点都会产生两条边，并消耗一条边；而每个空节点只会消耗一条边
+        edge = 1
+        for val in preorder.split(","):
+            if val == "#":
+                edge -= 1
+                if edge < 0:
+                    return False
+            else:
+                edge -= 1
+                if edge < 0:
+                    return False
+                edge += 2
+
+        return edge == 0
