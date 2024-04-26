670accb15c954c87054c31d26ceb80fea9d01fc6
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-25 19:56:51 -0700

    Time: 40 ms (39.47%) | Memory: 17.1 MB (34.02%) - LeetSync

--- 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -7,6 +7,9 @@ class Node:
         self.right = right
 """
 
+# left： prev
+# right: next
+
 
 class Solution:
     def treeToDoublyList(self, root: "Optional[Node]") -> "Optional[Node]":
@@ -18,19 +21,22 @@ class Solution:
 
         # Connect root with its left subtree (if it exists)
         if left_head:
-            left_tail = left_head.left
-            left_tail.right, root.left = root, left_tail
+            left_tail = left_head.left  # left_head 前一个是 left_tail
+            left_tail.right = root  # left_tail 下一个是 root
+            root.left = left_tail  # root 前一个是 left_tail
         else:
             left_head = root  # If no left subtree, root itself is the smallest element
 
         # Connect root with its right subtree (if it exists)
         if right_head:
             right_tail = right_head.left
-            right_head.left, root.right = root, right_head
+            right_head.left = root  # right_head 前一个是 root
+            root.right = right_head  # root 下一个是 right_head
         else:
             right_tail = root  # If no right subtree, root itself is the biggest element
 
         # Make the list circular by connecting the heads and tails
-        left_head.left, right_tail.right = right_tail, left_head
+        left_head.left = right_tail
+        right_tail.right = left_head
 
         return left_head
