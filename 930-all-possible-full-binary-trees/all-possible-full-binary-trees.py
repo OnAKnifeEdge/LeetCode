46b34d1fd2c3ab975648434b4308cb3e9e5f94b8
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-25 20:19:11 -0700

    Time: 108 ms (73.65%) | Memory: 19.8 MB (69.33%) - LeetSync

--- 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
@@ -7,15 +7,18 @@
 class Solution:
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
 
-        cache = {0: [], 1: [TreeNode()]} # map n: list of FBT
+        if n % 2 == 0:
+            return []
+
+        cache = {0: [], 1: [TreeNode()]}  # map n: list of FBT
 
         def dp(n):
             if n in cache:
                 return cache[n]
-            
+
             result = []
 
-            for i in range(n): # 0 to n - 1
+            for i in range(1, n, 2):  # 0 to n - 1
                 j = n - 1 - i
                 left = dp(i)
                 right = dp(j)
@@ -25,5 +28,5 @@ class Solution:
                         result.append(TreeNode(val=0, left=l, right=r))
             cache[n] = result
             return result
-        
+
         return dp(n)
