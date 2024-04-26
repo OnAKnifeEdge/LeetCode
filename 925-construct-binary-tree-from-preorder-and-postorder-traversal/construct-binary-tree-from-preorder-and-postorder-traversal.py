63c96b0a141cfe2eeb661df492cc7a94d1f0ec85
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-25 17:35:38 -0700

    Time: 43 ms (90.84%) | Memory: 16.7 MB (24.28%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -5,30 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+    def constructFromPrePost(
+        self, preorder: List[int], postorder: List[int]
+    ) -> Optional[TreeNode]:
         if not preorder or not postorder:
-            return
+            return None
 
         d = {val: idx for idx, val in enumerate(postorder)}
-        
+
         def build_subtree(left, right):
             if left > right:
-                return
-            
+                return None
+
             root_val = preorder.pop(0)
             root = TreeNode(root_val)
 
             if left == right:
                 return root
 
-            idx = d[preorder[0]]
+            left_root_idx = d[preorder[0]]
 
-            root.left = build_subtree(left, idx)
-            root.right = build_subtree(idx + 1, right - 1)
+            root.left = build_subtree(left, left_root_idx)
+            root.right = build_subtree(left_root_idx + 1, right - 1)
 
             return root
 
         return build_subtree(0, len(postorder) - 1)
-
-
-        
