823759da46c4d1dbb99266bf06296b2ad671e4c6
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-25 20:24:12 -0700

    Time: 105 ms (83.45%) | Memory: 20.2 MB (58.00%) - LeetSync

--- 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
@@ -5,28 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
 
+    def allPossibleFBT(self, n: int) -> List[TreeNode]:
         if n % 2 == 0:
             return []
 
-        cache = {0: [], 1: [TreeNode()]}  # map n: list of FBT
+        return self.dp(n)
 
-        def dp(n):
-            if n in cache:
-                return cache[n]
+    @cache
+    def dp(self, n: int) -> List[TreeNode]:
+        if n == 0:
+            return []
+        if n == 1:
+            return [TreeNode()]
 
-            result = []
+        result = []
 
-            for i in range(1, n, 2):  # 0 to n - 1
-                j = n - 1 - i
-                left = dp(i)
-                right = dp(j)
+        for i in range(1, n, 2):
+            left = self.dp(i)
+            right = self.dp(n - 1 - i)
 
-                for l_tree, r_tree in itertools.product(left, right):
-                    result.append(TreeNode(val=0, left=l_tree, right=r_tree))
+            for l_tree, r_tree in product(left, right):
+                result.append(TreeNode(val=0, left=l_tree, right=r_tree))
 
-            cache[n] = result
-            return result
-
-        return dp(n)
+        return result
