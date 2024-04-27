81d4c8810fad2e51dc00722497ef34315516c051
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-26 22:07:20 -0700

    Time: 38 ms (59.65%) | Memory: 16.7 MB (50.98%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -9,22 +9,18 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        if not root:
-            return
 
-        self.flatten(root.left)
-        self.flatten(root.right)
-
-        left = root.left
-        right = root.right
-
-        root.left = None
-        root.right = left
-
-        p = root
-        while p.right:
-            p = p.right
-
-        p.right = right
-        
-        
+        def helper(node) -> Optional[TreeNode]:  # return tail node
+            if not node:
+                return None
+            if not node.left and not node.right:
+                return node
+            left_tail = helper(node.left)
+            right_tail = helper(node.right)
+            if left_tail:
+                left_tail.right = node.right
+                node.right = node.left
+                node.left = None
+            return right_tail or left_tail or node
+        helper(root)
+        return root
