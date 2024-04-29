47d4af614c387242f0fe0c2c4b9a14b0d6650665
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-29 16:47:47 -0700

    Time: 50 ms (26.74%) | Memory: 18.7 MB (72.82%) - LeetSync

--- 508-most-frequent-subtree-sum/most-frequent-subtree-sum.py
+++ 508-most-frequent-subtree-sum/most-frequent-subtree-sum.py
@@ -7,16 +7,19 @@
 class Solution:
     def findFrequentTreeSum(self, root: Optional[TreeNode]) -> List[int]:
         frequency = {}
+        max_frequency = 0
 
         def get_tree_sum(node) -> int:
+            nonlocal max_frequency
             if not node:
                 return 0
             left = get_tree_sum(node.left)
             right = get_tree_sum(node.right)
             s = left + right + node.val
             frequency[s] = frequency.get(s, 0) + 1
+            max_frequency = max(max_frequency, frequency[s])
             return s
 
         get_tree_sum(root)
-        max_frequency = max(frequency.values())
+        # max_frequency = max(frequency.values())
         return [f for f in frequency if frequency[f] == max_frequency]
