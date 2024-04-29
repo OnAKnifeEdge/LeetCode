59f4d17869ca827b63bedb20682174e8f9eb9552
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-04-29 16:35:16 -0700

    Time: 63 ms (63.06%) | Memory: 20.9 MB (77.29%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -12,15 +12,9 @@ class Solution:
             nonlocal s
             if not node:
                 return 0
-            # Recursively find the maximum path sum of the left subtree, avoid negative paths.
             left = max(findMaxPath(node.left), 0)
-            # Recursively find the maximum path sum of the right subtree, avoid negative paths.
             right = max(findMaxPath(node.right), 0)
-
-            # The current node's max path could be the sum of itself and max paths through both subtrees.
-            s = max(left + right + node.val, s)
-
-            # Return the maximum value path including the node and one of its subtrees.
+            s = max(left + node.val + right, s)
             return max(left + node.val, right + node.val)
 
         findMaxPath(root)
