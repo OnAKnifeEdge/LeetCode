3148bef10abaed2414c4267f467ce146eb5eba79
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-05 08:56:02 -0700

    Time: 33 ms (84.66%) | Memory: 16.7 MB (8.96%) - LeetSync

--- 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.py
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.py
@@ -14,6 +14,12 @@ class Solution:
                 return 0
             left, right = dfs(node.left), dfs(node.right)
             steps += abs(left) + abs(right)
+
+            # After adjusting the subtree rooted at the current node, 
+            # this line calculates the net excess or deficit of coins
+            # taking into account the current node's initial coins, 
+            # adjustments made in its subtrees, and ensuring it itself ends up with one coin.
+
             return node.val + left + right - 1
 
         dfs(root)
