0213a38468e69e4bd4ee0e09d5442d37be63e42f
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-06 12:05:14 -0700

    Time: 106 ms (5.08%) | Memory: 17.9 MB (97.00%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -8,19 +8,19 @@ class Node:
         self.next = next
 """
 
+
 class Solution:
-    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+    def connect(self, root: "Optional[Node]") -> "Optional[Node]":
         if not root or not root.left:
             return root
-        self.traverse(root.left, root.right)
+
+        def traverse(left, right):
+            if not left or not right:
+                return
+            left.next = right
+            traverse(left.left, left.right)
+            traverse(left.right, right.left)
+            traverse(right.left, right.right)
+
+        traverse(root.left, root.right)
         return root
-
-
-    def traverse(self, a: 'Optional[Node]', b: 'Optional[Node]') -> None:
-        if not a or not b:
-            return
-        a.next = b
-        self.traverse(a.left, a.right)
-        self.traverse(a.right, b.left)
-        self.traverse(b.left, b.right)
-
