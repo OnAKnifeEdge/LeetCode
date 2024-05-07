83a026ef7ae30af612a7ebbfe6c0601d6b86c742
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-07 14:52:40 -0700

    Time: 35 ms (61.47%) | Memory: 16.6 MB (6.97%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.py
+++ 832-binary-tree-pruning/binary-tree-pruning.py
@@ -10,6 +10,8 @@ class Solution:
             return None
         root.left = self.pruneTree(root.left)
         root.right = self.pruneTree(root.right)
-        if root.val != 1 and not root.left and not root.right:
+
+        if root.val == 0 and not root.left and not root.right:
             return None
+
         return root
