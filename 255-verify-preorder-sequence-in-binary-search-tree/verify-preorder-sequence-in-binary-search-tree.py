35aa8d936472cb62b8e2202dffb5b300103767a5
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-07 21:35:14 -0700

    Time: 168 ms (79.66%) | Memory: 17.7 MB (54.50%) - LeetSync

--- 255-verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.py
+++ 255-verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.py
@@ -1,12 +1,15 @@
 class Solution:
     def verifyPreorder(self, preorder: List[int]) -> bool:
         parent = None
-        stack = []  # decresing
+        stack = []  # decreasing stack simulates the left edges of the BST
 
         for num in preorder:
             while stack and num > stack[-1]:
                 parent = stack.pop()
+            # reach to a node which should be in the right tree
             if parent and num <= parent:
                 return False
+            # If the current number passed the parent check or the parent is still None,
+            # we append the current number to the stack, possibly becoming a new parent.
             stack.append(num)
         return True
