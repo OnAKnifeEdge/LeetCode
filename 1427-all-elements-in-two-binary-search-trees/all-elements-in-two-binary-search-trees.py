6d3db48f23fb68308fbe82ca6e30e5813e53cba0
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-08 00:08:02 -0700

    Time: 208 ms (19.92%) | Memory: 19.8 MB (86.75%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.py
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.py
@@ -4,6 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 
 class BSTIterator:
@@ -30,6 +36,7 @@ class BSTIterator:
 
 class Solution:
     # 173, 21
+
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
         i1 = BSTIterator(root1)
         i2 = BSTIterator(root2)
@@ -49,3 +56,30 @@ class Solution:
             result.append(i2.next())
 
         return result
+
+    def getAllElementsIDontKnow(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        stack1, stack2, output = [], [], []
+
+        while root1 or root2 or stack1 or stack2:
+            # update both stacks
+            # by going left till possible
+            while root1:
+                stack1.append(root1)
+                root1 = root1.left
+            while root2:
+                stack2.append(root2)
+                root2 = root2.left
+
+            # Add the smallest value into output,
+            # pop it from the stack,
+            # and then do one step right
+            if not stack2 or stack1 and stack1[-1].val <= stack2[-1].val:
+                root1 = stack1.pop()
+                output.append(root1.val)
+                root1 = root1.right
+            else:
+                root2 = stack2.pop()
+                output.append(root2.val)
+                root2 = root2.right
+
+        return output
