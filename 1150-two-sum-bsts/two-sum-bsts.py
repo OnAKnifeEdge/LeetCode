8fa441ae02c70b0b25ed324f13e4b429e304afca
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-07 21:57:17 -0700

    Time: 55 ms (69.79%) | Memory: 19.9 MB (10.88%) - LeetSync

--- 1150-two-sum-bsts/two-sum-bsts.py
+++ 1150-two-sum-bsts/two-sum-bsts.py
@@ -5,7 +5,37 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+
     def twoSumBSTs(
+        self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int
+    ) -> bool:
+
+        if not root1 or not root2:
+            return False
+
+        needs = set()
+
+        def fill_needs(node):
+            if not node:
+                return
+            fill_needs(node.left)
+            needs.add(target - node.val)
+            fill_needs(node.right)
+
+        fill_needs(root1)
+
+        # find if any value in root2 tree is in needs
+
+        def check_need(node):
+            if not node:
+                return False
+            if node.val in needs:
+                return True
+            return check_need(node.left) or check_need(node.right)
+
+        return check_need(root2)
+
+    def twoSumBSTsTwoPointers(
         self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int
     ) -> bool:
         if not root1 or not root2:
@@ -21,7 +51,7 @@ class Solution:
         i = 0
         j = len(arr2) - 1
 
-        while i < len(arr1) and j > 0:
+        while i < len(arr1) and j >= 0:
             if arr1[i] + arr2[j] > target:
                 j -= 1
             elif arr1[i] + arr2[j] < target:
