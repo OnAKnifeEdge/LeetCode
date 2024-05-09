725ea001c1f979747282cf21e1e6348ed9c9bc3a
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-08 23:03:03 -0700

    Time: 112 ms (90.01%) | Memory: 22.3 MB (90.17%) - LeetSync

--- 359-logger-rate-limiter/logger-rate-limiter.py
+++ 359-logger-rate-limiter/logger-rate-limiter.py
@@ -1,20 +1,15 @@
 class Logger:
 
     def __init__(self):
-        self.set = set()
-        self.q = deque()
+        self.d = {}
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        while self.q:
-            msg, ts = self.q[0]
-            if timestamp - ts < 10:
-                break
-            self.q.popleft()
-            self.set.remove(msg)
+        if message not in self.d:
+            self.d[message] = timestamp
+            return True
 
-        if message not in self.set:
-            self.set.add(message)
-            self.q.append((message, timestamp))
+        if timestamp - self.d[message] >= 10:
+            self.d[message] = timestamp
             return True
 
         return False
