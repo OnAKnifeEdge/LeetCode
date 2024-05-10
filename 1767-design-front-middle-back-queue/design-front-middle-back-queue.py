d1e3ddcb6acac5e3e8feab92091e05aceb95a67b
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-10 15:39:28 -0700

    Time: 62 ms (69.94%) | Memory: 17.5 MB (12.66%) - LeetSync

--- 1767-design-front-middle-back-queue/design-front-middle-back-queue.py
+++ 1767-design-front-middle-back-queue/design-front-middle-back-queue.py
@@ -38,70 +38,77 @@ class FrontMiddleBackQueue:
 
         self.size += 1
 
-    # def push_node_to_mid(self, node):
-    #     nxt = self.mid.next
-    #     self.mid.next = node
-    #     node.prev = self.mid
-    #     node.next = nxt
-    #     nxt.prev = node
-
-    # def pushMiddle(self, val: int) -> None:
-    #     node = Node(val)
-    #     if self.size == 0:
-    #         self.create_first_node(node)
-    #     elif self.size % 2 == 0:
-    #         #      even to odd: mid is node
-    #         #        [1, 2] -> [1, 3, 2]
-    #         p = self.mid
-    #         n = self.mid.next
-    #         self.mid = node
-    #         self.mid.next = n
-    #         self.mid.prev = p
-    #         p.next = self.mid
-    #         n.prev = self.mid
-
-    #     else:
-    #         prev = self.mid.prev
-    #         next = self.mid
-    #         self.mid = node
-    #         self.mid.next = next
-    #         self.mid.prev = prev
-    #         prev.next = self.mid
-    #         next.prev = self.mid
-
-    #     self.size += 1
+    def push_node_to_mid(self, node):
+        nxt = self.mid.next
+        self.mid.next = node
+        node.prev = self.mid
+        node.next = nxt
+        nxt.prev = node
 
     def pushMiddle(self, val: int) -> None:
-        newNode = Node(val)
+        node = Node(val)
         if self.size == 0:
-            self.head = self.mid = self.tail = newNode
+            self.create_first_node(node)
         elif self.size == 1:
-            self.head = self.mid = newNode
-            self.head.next = self.tail
-            self.tail.prev = self.head
-        elif self.size == 2:
-            self.mid = newNode
-            self.mid.next = self.tail
-            self.mid.prev = self.head
-            self.head.next = self.mid
-            self.tail.prev = self.mid
-        elif self.size % 2 == 1:
-            prev = self.mid.prev
-            next = self.mid
-            self.mid = newNode
-            self.mid.next = next
-            self.mid.prev = prev
-            prev.next = self.mid
-            next.prev = self.mid
-        else:
+            node.next = self.tail
+            self.tail.prev = node
+            self.head = node
+            self.mid = node
+
+
+        elif self.size % 2 == 0:
+            #      even to odd: mid is node
+            #        [1, 2] -> [1, 3, 2]
             p = self.mid
             n = self.mid.next
-            self.mid = newNode
+            self.mid = node
             self.mid.next = n
             self.mid.prev = p
             p.next = self.mid
             n.prev = self.mid
+
+        else:
+            prev = self.mid.prev
+            next = self.mid
+            self.mid = node
+            self.mid.next = next
+            self.mid.prev = prev
+            prev.next = self.mid
+            next.prev = self.mid
+
         self.size += 1
+
+    # def pushMiddle(self, val: int) -> None:
+    #     newNode = Node(val)
+    #     if self.size == 0:
+    #         self.head = self.mid = self.tail = newNode
+    #     elif self.size == 1:
+    #         self.head = self.mid = newNode
+    #         self.head.next = self.tail
+    #         self.tail.prev = self.head
+    #     elif self.size == 2:
+    #         self.mid = newNode
+    #         self.mid.next = self.tail
+    #         self.mid.prev = self.head
+    #         self.head.next = self.mid
+    #         self.tail.prev = self.mid
+    #     elif self.size % 2 == 1:
+    #         prev = self.mid.prev
+    #         next = self.mid
+    #         self.mid = newNode
+    #         self.mid.next = next
+    #         self.mid.prev = prev
+    #         prev.next = self.mid
+    #         next.prev = self.mid
+    #     else:
+    #         p = self.mid
+    #         n = self.mid.next
+    #         self.mid = newNode
+    #         self.mid.next = n
+    #         self.mid.prev = p
+    #         p.next = self.mid
+    #         n.prev = self.mid
+    #     self.size += 1
 
     def pushBack(self, val: int) -> None:
         newNode = Node(val)
