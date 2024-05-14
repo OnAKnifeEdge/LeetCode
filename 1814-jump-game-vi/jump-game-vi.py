6f18f79660f54dd9156279dcd1bd0721202853f3
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-14 10:20:59 -0700

    Time: 651 ms (92.08%) | Memory: 30.5 MB (52.48%) - LeetSync

--- 1814-jump-game-vi/jump-game-vi.py
+++ 1814-jump-game-vi/jump-game-vi.py
@@ -3,13 +3,14 @@ class Solution:
         # dp[i] will store the maximum score you can get by reaching index i
         dp = [float("-inf")] * len(nums)
         dp[0] = nums[0]
-        # dp[i] = dp[j] + nums[i] for dp[j] is the max for j in [i - k, i)
+        # dp[i] = nums[i] + max(dp[j] for j in range(max(0, i-k), i))
         mono_q = deque([0])  # decreasing dp[i]'s index
         for i in range(1, len(nums)):
             # Remove indices that are out of reach (more than k steps behind)
             while mono_q and mono_q[0] < i - k:
                 mono_q.popleft()
             dp[i] = dp[mono_q[0]] + nums[i]
+            # Everything smaller than the current dp val will be popped out
             while mono_q and dp[mono_q[-1]] < dp[i]:
                 mono_q.pop()
             mono_q.append(i)
