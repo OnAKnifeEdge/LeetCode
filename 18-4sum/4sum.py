ad7ab5c03a06ca853f7f6969a6ea99a56934ef54
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-14 16:29:35 -0700

    Time: 392 ms (70.71%) | Memory: 16.5 MB (87.99%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -8,16 +8,18 @@ class Solution:
         if not nums:
             return result
 
-        avg = target // k
+        # avg = target / k
 
-        if avg < nums[0] or nums[-1] < avg:
-            return result
+        # if avg < nums[0] or nums[-1] < avg:
+        #     return result
 
         if k == 2:
             return self.two_sum(nums, target)
 
-        for i in range(len(nums)):
-            if i != 0 and nums[i] == nums[i - 1]:
+        for i, num in enumerate(nums):
+            # if num > target:
+            #     break
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
             for subset in self.k_sum(nums[i + 1:], target - nums[i], k - 1):
                 result.append([nums[i]] + subset)
@@ -28,18 +30,21 @@ class Solution:
         lo, hi = 0, len(nums) - 1
 
         while lo < hi:
-            s = nums[lo] + nums[hi]
-            if s < target or (lo > 0 and nums[lo] == nums[lo - 1]):
+            left = nums[lo]
+            right = nums[hi]
+            total = left + right
+            if total < target:
                 lo += 1
-            elif s > target or (hi < len(nums) - 1 and nums[hi] == nums[hi + 1]):
+            elif total > target:
                 hi -= 1
             else:
-                result.append([nums[lo], nums[hi]])
-                lo += 1
-                hi -= 1
+                result.append([left, right])
+                while lo < hi and nums[lo] == left:
+                    lo += 1
+                while lo < hi and nums[hi] == right:
+                    hi -= 1
         return result
 
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         return self.k_sum(nums, target, 4)
-        
