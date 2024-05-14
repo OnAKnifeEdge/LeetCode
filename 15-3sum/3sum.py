93d88048dd76f56feee0b872c94578bce135031d
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-14 16:17:00 -0700

    Time: 788 ms (40.29%) | Memory: 20.6 MB (71.71%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,41 +1,36 @@
 class Solution:
 
-    def twoSum(self, nums: List[int], i: int, result: List[List[int]])  -> List[List[int]]:
-        # nums is sorted
-        lo, hi = i + 1, len(nums) - 1
-
-        while lo < hi:
-            left, right = nums[lo], nums[hi]
-            s = left + right + nums[i]
-            if s < 0:
-                lo += 1
-                continue
-                # while lo < hi and nums[lo] == left:
-                #     lo += 1
-                # continue
-            elif s > 0:
-                hi -= 1
-                continue
-                # while lo < hi and nums[hi] == right:
-                #     hi -= 1
-                # continue
-            result.append([nums[i], left, right])
-            while lo < hi and nums[lo] == left:
-                lo += 1
-            while lo < hi and nums[hi] == right:
-                hi -= 1
-        return result
-            
-
-    def threeSum(self, nums: List[int]) -> List[List[int]]:      
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         result = []
 
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                break
-            if i == 0 or nums[i - 1] != nums[i]:
-                self.twoSum(nums, i, result)
-        return result
+        def twoSum(i):
+            lo = i + 1
+            hi = len(nums) - 1
 
-      
+            while lo < hi:
+                left = nums[lo]
+                right = nums[hi]
+                total = nums[i] + left + right
+                if total < 0:
+                    while lo < hi and nums[lo] == left:
+                        lo += 1
+                    continue
+                elif total > 0:
+                    while lo < hi and nums[hi] == right:
+                        hi -= 1
+                    continue
+                result.append([nums[i], left, right])
+                while lo < hi and nums[lo] == left:
+                    lo += 1
+                while lo < hi and nums[hi] == right:
+                    hi -= 1
+
+        for i, num in enumerate(nums):
+            if num > 0:
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            twoSum(i)
+
+        return result
