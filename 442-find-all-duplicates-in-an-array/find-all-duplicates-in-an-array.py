73f1cd348b7e717b9c0b9d0e70596678265a806d
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-14 20:40:32 -0700

    Time: 243 ms (95.35%) | Memory: 23.6 MB (95.64%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,5 +1,9 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
+        # https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+        # https://leetcode.com/problems/find-all-duplicates-in-an-array/
+        # https://leetcode.com/problems/first-missing-positive/
+
         seen = [False] * len(nums)
         result = []
         for num in nums:
