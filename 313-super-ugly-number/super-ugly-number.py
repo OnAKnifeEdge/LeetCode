079b76c1b8e549583a0127f3fa6d7697f659b953
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-15 23:00:16 -0700

    Time: 291 ms (79.76%) | Memory: 20.4 MB (65.59%) - LeetSync

--- 313-super-ugly-number/super-ugly-number.py
+++ 313-super-ugly-number/super-ugly-number.py
@@ -1,11 +1,15 @@
 class Solution:
     def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
-        q = [1]
-        while n > 1: 
-            n -= 1
-            num = heapq.heappop(q)
-            for prime in primes:
-                heapq.heappush(q, prime * num)
-                if num % prime == 0:
-                    break
-        return q[0]
+        uglies = [1]
+        # primes = [2, 3, 5] => [(2, 2, 0), (3, 3, 0), (5, 5, 0)]
+        min_heap = [(prime, prime, 0) for prime in primes]
+
+        while len(uglies) < n:
+            next_ugly, prime, idx = heappop(min_heap)
+
+            if next_ugly != uglies[-1]:
+                uglies.append(next_ugly)
+
+            heappush(min_heap, (uglies[idx] * prime, prime, idx + 1))
+
+        return uglies[-1]
