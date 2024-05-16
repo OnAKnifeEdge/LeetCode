ab86eff0f867de4bdc02c2a580bf64bf685719ea
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-15 17:59:31 -0700

    Time: 34 ms (77.68%) | Memory: 16.6 MB (30.66%) - LeetSync

--- 677-map-sum-pairs/map-sum-pairs.py
+++ 677-map-sum-pairs/map-sum-pairs.py
@@ -1,7 +1,7 @@
 class TrieNode:
 
     def __init__(self):
-        self.val = None
+        self.val = 0
         self.children = {}
 
 
@@ -9,36 +9,26 @@ class MapSum:
 
     def __init__(self):
         self.root = TrieNode()
+        self.keys = defaultdict(int)  # additional dictionary to keep track of all keys
 
     def insert(self, key: str, val: int) -> None:
         node = self.root
+        delta = val - self.keys[key]
+        self.keys[key] = val
         for c in key:
             if c not in node.children:
                 node.children[c] = TrieNode()
             node = node.children[c]
-        node.val = val
+            node.val += delta
 
-    def get_node(self, prefix):
+    def sum(self, prefix: str) -> int:
         node = self.root
         for c in prefix:
             if c not in node.children:
-                return None
+                return 0
             node = node.children[c]
-        return node
+        return node.val
 
-    def dfs(self, node):
-        total = 0
-        if not node:
-            return None
-        if node.val is not None:
-            total += node.val
-        for c in node.children:
-            total += self.dfs(node.children[c])
-        return total
-
-    def sum(self, prefix: str) -> int:
-        node = self.get_node(prefix)
-        return self.dfs(node) if node else 0
 
 
 # Your MapSum object will be instantiated and called as such:
