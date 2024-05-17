8f28e2c979276bdf8a542334348812925fb586f2
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-16 19:51:05 -0700

    Time: 66 ms (13.44%) | Memory: 26 MB (12.80%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -1,26 +1,16 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-
-        # frequency = Counter(s)
-        # sorted_chars = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
-        # return ''.join([char * freq for char, freq in sorted_chars])
-
         frequency = defaultdict(int)
         for c in s:
             frequency[c] += 1
 
-        buckets = [[] for _ in range(len(s) + 1)]
-        for char, freq in frequency.items():
-            buckets[freq].extend(char * freq)
+        bucket = [[] for _ in range(len(s) + 1)]
+        for v, f in frequency.items():
+            bucket[f].append(v)
 
-        return "".join("".join(bucket) for bucket in reversed(buckets) if bucket)
+        result = []
 
-        # for v, f in frequency.items():
-        #     bucket[f].append(v)
-
-        # result = []
-
-        # for f in reversed(range(len(s) + 1)):
-        #     for c in bucket[f]:
-        #         result.append(c * f)
-        # return "".join(result)
+        for f in reversed(range(len(s) + 1)):
+            for c in bucket[f]:
+                result.append(c * f)
+        return "".join(result)
