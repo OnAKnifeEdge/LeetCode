00535631b3b24b320622f30b76c4652e61170a02
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-16 19:43:20 -0700

    Time: 52 ms (36.61%) | Memory: 26 MB (11.29%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -0,0 +1,16 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        frequency = defaultdict(int)
+        for c in s:
+            frequency[c] += 1
+
+        bucket = [[] for _ in range(len(s) + 1)]
+        for v, f in frequency.items():
+            bucket[f].append(v)
+
+        result = []
+        
+        for f in reversed(range(len(s) + 1)):
+            for c in bucket[f]:
+                result.append(c * f)
+        return "".join(result)
