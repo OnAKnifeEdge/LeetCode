8a7cf05b079ae37d61c090299491c305e236822b
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-20 12:06:42 -0700

    Time: 38 ms (40.67%) | Memory: 16.7 MB (41.36%) - LeetSync

--- 355-design-twitter/design-twitter.py
+++ 355-design-twitter/design-twitter.py
@@ -1,86 +1,21 @@
-from heapq import heappush, heappop, heapify
-from collections import defaultdict
-
-
-class Tweet:
-    def __init__(self, timestamp, tweet_id, next=None):
-        self.timestamp = timestamp
-        self.tweet_id = tweet_id
-        self.next = next
-
-    def __lt__(self, other):
-        return self.timestamp > other.timestamp
-
-
-class Twitter:
-    """
-    你把一个用户发布的所有推文做成一条有序链表（靠近头部的推文是最近发布的），
-    那么只要合并关注用户的推文链表，即可获得按照时间线顺序排序的信息流。
-    """
+class Twitter(object):
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        # This will hold a linkedlist of tweets for each user
-        self.tweets = {}  # user: Tweet
-        # This will hold a set of users that each user is following
-        self.followees = defaultdict(set)
-        self.timestamp = 0  # This is used to order the tweets by time
+        self.timer = itertools.count(step=-1)
+        self.tweets = collections.defaultdict(collections.deque)
+        self.followees = collections.defaultdict(set)
 
-    def postTweet(self, userId: int, tweetId: int) -> None:
-        """
-        Compose a new tweet.
-        """
-        # Append the (time, tweetId) tuple to the list of tweets of userId
-        tweet = Tweet(self.timestamp, tweetId, next=self.tweets.get(userId))
-        self.tweets[userId] = tweet
-        self.timestamp += 1
+    def postTweet(self, userId, tweetId):
+        self.tweets[userId].appendleft((next(self.timer), tweetId))
 
-    def getNewsFeed(self, userId: int):
-        """
-        Retrieve the 10 most recent tweet ids in the user's news feed.
-        Each item in the news feed must be posted by users who the user followed
-        or by the user herself.
-        Tweets must be ordered from most recent to least recent.
-        """
-        heap = []
-        people = self.followees[userId] | {userId}
-        for person in people:
-            head = self.tweets.get(person)
-            if head:
-                heappush(heap, head)
+    def getNewsFeed(self, userId):
+        tweets = heapq.merge(
+            *(self.tweets[u] for u in self.followees[userId] | {userId})
+        )
+        return [t for _, t in itertools.islice(tweets, 10)]
 
-        timeline = []
-
-        while heap and len(timeline) < 10:
-            earliest = heappop(heap)
-
-            timeline.append(earliest.tweet_id)
-
-            next_tweet = earliest.next
-            if next_tweet:
-                heappush(heap, next_tweet)
-
-        return timeline
-
-    def follow(self, followerId: int, followeeId: int) -> None:
-        """
-        Follower follows a followee.
-        """
+    def follow(self, followerId, followeeId):
         self.followees[followerId].add(followeeId)
 
-    def unfollow(self, followerId: int, followeeId: int) -> None:
-        """
-        Follower unfollows a followee. If not a followee, do nothing.
-        """
-        if followeeId in self.followees[followerId]:
-            self.followees[followerId].remove(followeeId)
-
-
-# Your Twitter object will be instantiated and called as such:
-# obj = Twitter()
-# obj.postTweet(userId,tweetId)
-# param_2 = obj.getNewsFeed(userId)
-# obj.follow(followerId,followeeId)
-# obj.unfollow(followerId,followeeId)
+    def unfollow(self, followerId, followeeId):
+        self.followees[followerId].discard(followeeId)
