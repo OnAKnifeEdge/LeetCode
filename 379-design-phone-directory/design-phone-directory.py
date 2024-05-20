a63fcb09799a3104e00b8514a76b43ce1dcf689a
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-20 10:55:08 -0700

    Time: 78 ms (50.19%) | Memory: 20.7 MB (82.02%) - LeetSync

--- 379-design-phone-directory/design-phone-directory.py
+++ 379-design-phone-directory/design-phone-directory.py
@@ -2,24 +2,25 @@ class PhoneDirectory:
 
     def __init__(self, maxNumbers: int):
         self.slots = deque(range(maxNumbers))
-        self.is_slot_available = [True] * maxNumbers
+        self.available = [True] * maxNumbers
 
     def get(self) -> int:
         if not self.slots:
             return -1
-
         slot = self.slots.popleft()
-        self.is_slot_available[slot] = False
+        self.available[slot] = False
         return slot
 
     def check(self, number: int) -> bool:
-        return self.is_slot_available[number]
+        if not self.available:
+            return False
+        return self.available[number]
 
     def release(self, number: int) -> None:
-        if self.is_slot_available[number]:
+        if self.available[number]:
             return
         self.slots.append(number)
-        self.is_slot_available[number] = True
+        self.available[number] = True
 
 
 # Your PhoneDirectory object will be instantiated and called as such:
