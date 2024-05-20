19c7be10e4b45231386ede5fc2bb686afaccf8c4
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-20 11:10:57 -0700

    Time: 566 ms (51.78%) | Memory: 41.7 MB (29.25%) - LeetSync

--- 901-advantage-shuffle/advantage-shuffle.py
+++ 901-advantage-shuffle/advantage-shuffle.py
@@ -1,21 +1,19 @@
 class Solution:
     def advantageCount(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1.sort()
-        maxq = [(-val, i) for i, val in enumerate(nums2)]
-        heapify(maxq)
+        nums1.sort()  # ascending
+        max_q = [(-val, i) for i, val in enumerate(nums2)]
+        heapify(max_q)
 
+        result = [None] * len(nums1)
         left, right = 0, len(nums1) - 1
-        result = [0] * len(nums1)
 
-        while maxq:
-            val, i = heappop(maxq)
-            max_val = - val
-
-            if max_val < nums1[right]: # 打得过，咱就打
+        while max_q:
+            val, i = heappop(max_q)
+            max_val = -val  # nums2 里最能打的
+            if nums1[right] > max_val:  # nums1 里最能打的能打得过，咱就打！
                 result[i] = nums1[right]
                 right -= 1
-            else: # 打不过，咱就跑
+            else:  # 打不过，咱就跑
                 result[i] = nums1[left]
                 left += 1
-        
         return result
