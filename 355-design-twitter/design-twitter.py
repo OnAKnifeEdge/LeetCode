68980562fbd39ef2c5d989a39f7e2c447befd21c
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-20 12:50:59 -0700

    Time: 26 ms (96.33%) | Memory: 16.7 MB (42.08%) - LeetSync

--- 355-design-twitter/design-twitter.py
+++ 355-design-twitter/design-twitter.py
@@ -1,21 +1,29 @@
-class Twitter(object):
+class Twitter:
 
     def __init__(self):
         self.timer = itertools.count(step=-1)
-        self.tweets = defaultdict(collections.deque)
+        self.tweets = defaultdict(deque)
         self.followees = defaultdict(set)
 
-    def postTweet(self, userId, tweetId):
+    def postTweet(self, userId: int, tweetId: int) -> None:
         self.tweets[userId].appendleft((next(self.timer), tweetId))
 
-    def getNewsFeed(self, userId):
+    def getNewsFeed(self, userId: int) -> List[int]:
         tweets = heapq.merge(
             *(self.tweets[u] for u in self.followees[userId] | {userId})
         )
         return [t for _, t in itertools.islice(tweets, 10)]
 
-    def follow(self, followerId, followeeId):
+    def follow(self, followerId: int, followeeId: int) -> None:
         self.followees[followerId].add(followeeId)
 
-    def unfollow(self, followerId, followeeId):
+    def unfollow(self, followerId: int, followeeId: int) -> None:
         self.followees[followerId].discard(followeeId)
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
