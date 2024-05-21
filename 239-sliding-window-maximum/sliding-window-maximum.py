a6ab6ac45311925ecfe5a247e4f45bd25322cd8e
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-21 10:53:50 -0700

    Time: 1070 ms (89.73%) | Memory: 31.9 MB (97.38%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,16 +1,20 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        mono_queue = deque()  # stores indices
+        # stores idx of nums. [7, 6, 5]
+        # mono_queue[0] will be the max
+
+        mono_queue = deque()
         result = []
         for i, num in enumerate(nums):
-            if mono_queue and mono_queue[0] < i - k + 1:  # window starts at (i - k + 1)
+            # if window ends at i -> start = i - k + 1
+            left = i - k + 1
+            # 把 window 外的 pop 掉
+            if mono_queue and mono_queue[0] < left:
                 mono_queue.popleft()
-
-            while mono_queue and nums[mono_queue[-1]] < num:
+            # 把比自己小的都 pop 掉
+            while mono_queue and num > nums[mono_queue[-1]]:
                 mono_queue.pop()
-
             mono_queue.append(i)
-
-            if i >= k - 1:  # reaches window size:
+            if left >= 0:
                 result.append(nums[mono_queue[0]])
         return result
