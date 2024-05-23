1883303e7bbf5e3e0ddb52ce3802e9368043822e
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-23 14:47:07 -0700

    Time: 97 ms (24.55%) | Memory: 18.1 MB (97.37%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -4,27 +4,28 @@ class Solution:
         for course, pre_course in prerequisites:
             d[pre_course].append(course)
 
-        visited = [0] * numCourses   # 0: unvisited, 1: visiting, 2: visited
+        visited = [False] * numCourses
+        exploring = [False] * numCourses
         post_order = []
 
         def dfs(course):
-            if visited[course] == 1:  # Cycle detected
+            if exploring[course]:
                 return True
-            if visited[course] == 2:  # Already processed
-                return False
 
-            visited[course] = 1  # Mark as visiting
+            if visited[course]:
+                return False
+            visited[course] = True
+            exploring[course] = True
 
             for next_course in d[course]:
                 if dfs(next_course):
                     return True
-
-            visited[course] = 2  # Mark as processed
             post_order.append(course)
+            exploring[course] = False
             return False
 
         for course in range(numCourses):
-            if visited[course] == 0 and dfs(course):  # If cycle found, return []
+            if not visited[course] and dfs(course):
                 return []
 
         return post_order[::-1]
