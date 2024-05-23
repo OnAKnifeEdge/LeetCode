29dee4c43f9b4f077ef2acbed618b93b2d8f95b1
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-23 15:58:22 -0700

    Time: 135 ms (95.28%) | Memory: 16.9 MB (97.21%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,5 +1,27 @@
 class Solution:
+
     def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+
+        # 0 means not colored, 1, -1 means different colors
+        color = [0] * n
+
+        for i in range(n):
+            if color[i] != 0:  # colored already
+                continue
+            q = deque([i])  # put uncolored to q
+            color[i] = 1
+            while q:
+                current = q.popleft()
+                for neighbor in graph[current]:
+                    if color[neighbor] == 0:
+                        color[neighbor] = -color[current]  # a different color
+                        q.append(neighbor)
+                    elif color[neighbor] == color[current]:
+                        return False
+        return True
+
+    def isBipartite_dfs(self, graph: List[List[int]]) -> bool:
         n = len(graph)
 
         # 0 means not colored, 1, -1 means different colors
