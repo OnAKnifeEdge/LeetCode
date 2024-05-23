649c4f6a55fcb02ae9c1e7b6c3a6fb68689a458f
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-23 15:51:45 -0700

    Time: 144 ms (65.21%) | Memory: 17.3 MB (14.15%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -9,12 +9,12 @@ class Solution:
             color[node] = c
             for neighbor in graph[node]:
                 if color[neighbor] == c:
-                    return False
-                if color[neighbor] == 0 and not dfs(neighbor, -c):
-                    return False
-            return True
+                    return True
+                if color[neighbor] == 0 and dfs(neighbor, -c):
+                    return True
+            return False
 
         for i in range(n):
-            if color[i] == 0 and not dfs(i, 1):
+            if color[i] == 0 and dfs(i, 1):
                 return False
         return True
