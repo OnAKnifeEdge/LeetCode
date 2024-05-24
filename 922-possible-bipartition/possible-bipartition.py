114fc98b35816c126164add6a1cbb0858235748c
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-23 21:47:56 -0700

    Time: 545 ms (95.84%) | Memory: 21.7 MB (92.68%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.py
+++ 922-possible-bipartition/possible-bipartition.py
@@ -1,5 +1,29 @@
 class Solution:
+
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        d = [[] for _ in range(n + 1)]
+        for a, b in dislikes:
+            d[a].append(b)
+            d[b].append(a)
+
+        color = [0] * (n + 1)
+
+        for node in range(1, n + 1):
+            if color[node] != 0:  # colored already
+                continue
+            q = deque([node])
+            color[node] = 1
+            while q:
+                current = q.popleft()
+                for neighbor in d[current]:
+                    if color[neighbor] == color[current]:
+                        return False
+                    if color[neighbor] == 0:
+                        q.append(neighbor)
+                        color[neighbor] = -color[current]
+        return True
+
+    def possibleBipartition_dfs(self, n: int, dislikes: List[List[int]]) -> bool:
         d = [[] for _ in range(n + 1)]
         for a, b in dislikes:
             d[a].append(b)
