766bb29a0d4c15851432d22ae9bbd3bbb3debc2c
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-23 23:16:53 -0700

    Time: 116 ms (76.94%) | Memory: 17.9 MB (54.74%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,5 +1,51 @@
 class Solution:
+
     def solve(self, board: List[List[str]]) -> None:
+        if not board or not board[0]:
+            return
+
+        R = len(board)
+        C = len(board[0])
+
+        # 1. find all border cells
+        border_rows = [0, R - 1]
+        border_cols = [0, C - 1]
+
+        border_row_cells = list(itertools.product(range(R), border_cols))
+        border_col_cells = list(itertools.product(border_rows, range(C)))
+
+        borders = border_row_cells + border_col_cells
+
+        # 2. mark escape cells with any placeholder
+
+        def bfs(r, c):
+            q = deque([(r, c)])
+            while q:
+                row, col = q.popleft()
+                if row < 0 or row == R:
+                    continue
+                if col < 0 or col == C:
+                    continue
+                if board[row][col] != "O":
+                    continue
+                board[row][col] = "E"
+                q.append((row, col + 1))
+                q.append((row, col - 1))
+                q.append((row + 1, col))
+                q.append((row - 1, col))
+
+        for row, col in borders:
+            bfs(row, col)
+
+        # 3. flip captured cells to X and reset the placeholder back to O
+
+        for r, c in itertools.product(range(R), range(C)):
+            if board[r][c] == "O":
+                board[r][c] = "X"
+            elif board[r][c] == "E":
+                board[r][c] = "O"
+
+    def solve_dfs(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
