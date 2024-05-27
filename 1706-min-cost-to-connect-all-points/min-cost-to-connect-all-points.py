791d64afa47b9fb010017830183b36c30ba204e1
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-27 14:09:06 -0700

    Time: 757 ms (79.46%) | Memory: 81.6 MB (77.04%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,49 +1,22 @@
-class UnionFind:
-
-    def __init__(self, size):
-        self.parent = [i for i in range(size + 1)]
-        self.rank = [0] * (size + 1)
-
-    def find(self, x):
-        if x != self.parent[x]:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x, y):
-        rx, ry = self.find(x), self.find(y)
-        if self.rank[rx] < self.rank[ry]:
-            self.parent[rx] = ry
-        elif self.rank[rx] > self.rank[ry]:
-            self.parent[ry] = rx
-        else:
-            self.parent[ry] = rx
-            self.rank[rx] += 1
-
-    def is_connected(self, x, y):
-        return self.find(x) == self.find(y)
-
-
-class Point(NamedTuple):
-    x: int
-    y: int
-    weight: int
-
-
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        p = []
-        n = len(points)
-        for i, j in itertools.combinations(range(n), 2):
-            xi, yi = points[i]
-            xj, yj = points[j]
-            p.append(Point(x=i, y=j, weight=abs(xi - xj) + abs(yi - yj)))
-        p.sort(key=lambda x: x.weight)
+        def manhattan(point1, point2):
+            return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
 
-        mst = 0
-        uf = UnionFind(n)
-        for point in p:
-            if uf.find(point.x) == uf.find(point.y):
-                continue
-            uf.union(point.x, point.y)
-            mst += point.weight
-        return mst
+        n = len(points)
+
+        total_cost = 0
+        visited = set()
+
+        queue = [(0, 0)]  # (distance, vertex)
+
+        while len(visited) < n:
+            d, u = heappop(queue)
+            if u not in visited:
+                visited.add(u)
+                total_cost += d
+                for v in range(n):
+                    if v not in visited:
+                        d = manhattan(points[u], points[v])
+                        heappush(queue, (d, v))
+        return total_cost
