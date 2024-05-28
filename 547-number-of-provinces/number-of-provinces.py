d4a24df8a56c7049f744a2ba3ca2d903c526b791
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-05-28 11:40:26 -0700

    Time: 180 ms (75.73%) | Memory: 17.4 MB (43.39%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,32 +1,48 @@
+class UnionFind:
+
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [0] * size
+        self.count = size
+
+    def find(self, x):
+        if x != self.root[x]:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x == root_y:
+            return
+        # put the smaller one to the larger one
+        if self.rank[root_x] < self.rank[root_y]:
+            self.root[root_x] = self.root[root_y]
+        elif self.rank[root_x] > self.rank[root_y]:
+            self.root[root_y] = self.root[root_x]
+        else:
+            # root_y is attached to root_x if equals
+            self.root[root_y] = self.root[root_x]
+            self.rank[root_x] += 1
+        self.count -= 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def get_count(self):
+        return self.count
+
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        graph = collections.defaultdict(list)
-
         if not isConnected:
             return 0
 
         n = len(isConnected)
 
+        uf = UnionFind(n)
+
         for i, j in itertools.combinations(range(n), 2):
-            if isConnected[i][j] == 0:
-                continue
-            graph[i].append(j)
-            graph[j].append(i)
+            if isConnected[i][j] == 1:
+                uf.union(i, j)
 
-        visited = set()
-
-        def dfs(u):
-            for v in graph[u]:
-                if v not in visited:
-                    visited.add(v)
-                    dfs(v)
-
-        count = 0
-
-        for i in range(n):
-            if i not in visited:
-                count += 1
-                visited.add(i)
-                dfs(i)
-
-        return count
+        return uf.get_count()
