51c8eb7badcc41fbf12e02de231b7221247b7a53
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-05 14:48:50 -0700

    Time: 70 ms (73.86%) | Memory: 23.9 MB (70.70%) - LeetSync

--- 89-gray-code/gray-code.py
+++ 89-gray-code/gray-code.py
@@ -1,26 +1,6 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        # n = 2
-        #  00, 01, 11, 10
-        # n = 3
-        # 000, 001, 011, 010: add 0 to the front
-        # 110, 111, 101, 100: reverse graycode of (n -1) and add 1 to the front
-
-        """
-        To generate the sequence for n,
-        we take the sequence for (n - 1),
-        add 0 to the front of each item in the sequence,
-        add 1 to the front of each of the items from the reversed sequence of (n - 1)
-        This ensures that all adjacent items differ by exactly one bit,
-        and the first and last items also differ by exactly one bit.
-
-        """
-        # if n == 0:
-        #     return [0]
-        # sequence = self.grayCode(n - 1)
-        # return sequence + [(1 << (n - 1)) + x for x in reversed(sequence)]
-
         result = [0]
         for i in range(n):
-            result += [x + (1 << i) for x in reversed(result)]
+            result += [(1 << i) + x for x in reversed(result)]
         return result
