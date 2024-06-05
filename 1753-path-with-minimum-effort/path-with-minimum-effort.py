b1ec962f3d09f62ace952688a7803da7d565ef4c
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-05 13:54:40 -0700

    Time: 501 ms (62.96%) | Memory: 19.1 MB (28.86%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -1,32 +1,33 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        if not heights:
+            return 0
         ROWS = len(heights)
         COLS = len(heights[0])
-
-        def get_neighbors(x, y):
-            r = []
-            for delta_x, delta_y in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                xx, yy = x + delta_x, y + delta_y
-                if xx < 0 or yy < 0 or xx >= ROWS or yy >= COLS:
-                    continue
-                r.append((xx, yy))
-            return r
-
+        q = [(0, 0, 0)]  # abs(difference), i, j
         visited = set()
-        q = [(0, 0, 0)]  # difference, i, j
+
+        def get_neighbors(i, j):
+            neighbors = []
+            for d_x, d_y in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                x, y = i + d_x, j + d_y
+                if x < 0 or x >= ROWS or y < 0 or y >= COLS:
+                    continue
+                neighbors.append((x, y))
+            return neighbors
 
         while q:
-            diff, x, y = heappop(q)
-            if (x, y) in visited:
+            effort, i, j = heappop(q)
+            if (i, j) in visited:
                 continue
-            if x == ROWS - 1 and y == COLS - 1:  # reach destination
-                return diff
-
-            visited.add((x, y))
-
-            neighbors = get_neighbors(x, y)
-            for xx, yy in neighbors:
-                if (xx, yy) in visited:
+            if (i, j) == (ROWS - 1, COLS - 1):
+                return effort
+            visited.add((i, j))
+            neighbors = get_neighbors(i, j)
+            for x, y in neighbors:
+                if (x, y) in visited:
                     continue
-                new_diff = max(diff, abs(heights[x][y] - heights[xx][yy]))
-                heappush(q, (new_diff, xx, yy))
+                new_effort = max(effort, abs(heights[x][y] - heights[i][j]))
+                heappush(q, (new_effort, x, y))
+
+        return -1
