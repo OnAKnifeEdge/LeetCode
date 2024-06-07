4672e70fc561433453b14dc4b06bf0a65cc12f93
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-06 20:05:16 -0700

    Time: 36 ms (80.49%) | Memory: 16.6 MB (89.13%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,13 +1,13 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * len(s)
-        for i in range(len(s)):
+
+        @cache
+        def dp(i):  # if s[i:] can be segemented
+            if i == len(s):
+                return True
             for word in wordDict:
-                if i < len(word) - 1:
-                    continue
-                if i == len(word) - 1 or dp[i -len(word)]:
-                    if s[i - len(word) + 1: i + 1] == word:
-                        dp[i] = True
-                        break
-        return dp[-1]
-        
+                if s[i:].startswith(word) and dp(i + len(word)):
+                    return True
+            return False
+
+        return dp(0)
