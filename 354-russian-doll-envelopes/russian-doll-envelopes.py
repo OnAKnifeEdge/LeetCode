b9971ac2f1f58711b4f77189b4d72b6d9c070819
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-07 15:12:21 -0700

    Time: 921 ms (57.90%) | Memory: 64.2 MB (44.32%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.py
+++ 354-russian-doll-envelopes/russian-doll-envelopes.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        envelopes.sort(key=lambda x: (x[0], -x[1]))  # sort by width asc and height desc
-        heights = [h for w, h in envelopes]
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        heights = [h for _, h in envelopes]
+        return self.lis(heights)
 
-        def lis(nums):
-            sub = []
-            for num in nums:
-                idx = bisect_left(sub, num)
-                if idx == len(sub):
-                    sub.append(num)
-                sub[idx] = num
-            return len(sub)
-
-        return lis(heights)
+    def lis(self, nums):
+        cards = []
+        for num in nums:
+            idx = bisect_left(cards, num)
+            if idx == len(cards):
+                cards.append(num)
+            else:
+                cards[idx] = num
+        return len(cards)
