75f9a2f9c3f1e421c029f5b2bff3f751f50843cf
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-06 17:15:42 -0700

    Time: 252 ms (77.05%) | Memory: 72.9 MB (61.06%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,21 +1,36 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
 
-        @cache
-        def dp(i, j):
-            if j == len(t):  # match empty string t
-                return 1
-            if i == len(s):  # s[i:] is empty
-                return 0
-            if s[i] == t[j]:
-                # aab, ab => a_b, _ab
-                # Two choices: select s[i] or skip s[i]
-                return dp(i + 1, j + 1) + dp(i + 1, j)
-                # 选 s 里的，(i + 1, j + 1)
-                # 不选 s 里的， (i + 1, j)
+        # @cache
+        # def dp(i, j):
+        #     if j == len(t):  # match empty string t
+        #         return 1
+        #     if i == len(s):  # s[i:] is empty
+        #         return 0
+        #     if s[i] == t[j]:
+        #         # aab, ab => a_b, _ab
+        #         # Two choices: select s[i] or skip s[i]
+        #         return dp(i + 1, j + 1) + dp(i + 1, j)
+        #         # 选 s 里的，(i + 1, j + 1)
+        #         # 不选 s 里的， (i + 1, j)
 
-            else:
-                # Can only skip s[i]
-                return dp(i + 1, j)
+        #     else:
+        #         # Can only skip s[i]
+        #         return dp(i + 1, j)
 
-        return dp(0, 0)
+        # return dp(0, 0)
+
+        ROWS = len(s)
+        COLS = len(t)
+        dp = [[0] * (COLS + 1) for _ in range(ROWS + 1)]
+
+        for i in range(ROWS + 1):
+            dp[i][-1] = 1
+
+        for i in reversed(range(ROWS)):
+            for j in reversed(range(COLS)):
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j]
+                else:
+                    dp[i][j] = dp[i + 1][j]
+        return dp[0][0]
