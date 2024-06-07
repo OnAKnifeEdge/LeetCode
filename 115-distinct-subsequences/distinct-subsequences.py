77c29efe46587aec9824fa8058f0b7aab7c13ceb
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-06 17:20:45 -0700

    Time: 193 ms (88.54%) | Memory: 16.8 MB (90.41%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,6 +1,7 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
 
+        # top down approach
         # @cache
         # def dp(i, j):
         #     if j == len(t):  # match empty string t
@@ -20,17 +21,33 @@ class Solution:
 
         # return dp(0, 0)
 
+        # bottom up approach
+        # ROWS = len(s)
+        # COLS = len(t)
+        # dp = [[0] * (COLS + 1) for _ in range(ROWS + 1)]
+
+        # for i in range(ROWS + 1):
+        #     dp[i][-1] = 1
+
+        # for i in reversed(range(ROWS)):
+        #     for j in reversed(range(COLS)):
+        #         if s[i] == t[j]:
+        #             dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j]
+        #         else:
+        #             dp[i][j] = dp[i + 1][j]
+        # return dp[0][0]
+
+        # bottom up with space optimization
         ROWS = len(s)
         COLS = len(t)
-        dp = [[0] * (COLS + 1) for _ in range(ROWS + 1)]
-
-        for i in range(ROWS + 1):
-            dp[i][-1] = 1
+        dp = [0] * (COLS + 1)
+        dp[-1] = 1
 
         for i in reversed(range(ROWS)):
+            prev_dp = dp[:]
             for j in reversed(range(COLS)):
                 if s[i] == t[j]:
-                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j]
+                    dp[j] = prev_dp[j + 1] + prev_dp[j]
                 else:
-                    dp[i][j] = dp[i + 1][j]
-        return dp[0][0]
+                    dp[j] = prev_dp[j]
+        return dp[0]
