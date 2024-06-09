72974c8eea2b4c812c7658420604bafa0de9dcb5
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-08 18:50:05 -0700

    Time: 1286 ms (33.94%) | Memory: 31.9 MB (48.77%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,4 +1,6 @@
 class Solution:
+
+    # 给一个可装载重量为 sum / 2 的背包和 N 个物品，每个物品的重量为 nums[i]。现在让你装物品，是否存在一种装法，能够恰好将背包装满？
     def canPartition(self, nums: List[int]) -> bool:
         total_sum = sum(nums)
 
@@ -9,16 +11,14 @@ class Solution:
 
         # dp[i][sum]
         dp = [[False] * (subset_sum + 1) for _ in range(n + 1)]
-        dp[0][0] = True
+        for i in range(n + 1):
+            dp[i][0] = True
 
         for i in range(1, n + 1):
-            for s in range(subset_sum + 1):
+            for j in range(subset_sum + 1):
                 num = nums[i - 1]
-                if s < num:
-                    dp[i][s] = dp[i - 1][s]  # do not include i
+                if num > j:
+                    dp[i][j] = dp[i - 1][j]
                 else:
-                    dp[i][s] = dp[i - 1][s] or dp[i - 1][s - num]
-                if s == subset_sum and dp[i][s]:
-                    return True
-
-        return False
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - num]
+        return dp[n][subset_sum]
