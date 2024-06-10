8ebf4f56f53c78ca98b9169a0d8d21a335a217f1
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-10 15:12:14 -0700

    Time: 184 ms (71.32%) | Memory: 43.9 MB (8.08%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        memo = {} # (i, target) => number of ways
+        # memo = {}  # (i, target) => number of ways
 
         @cache
         def dp(i, total):
-            if i == len(nums): # reached the end
+            if i == len(nums):  # reached the end
                 return 1 if total == target else 0
-            memo[(i, total)] = dp(i + 1, total + nums[i]) + dp(i + 1, total - nums[i])
-            return memo[(i, total)]
+            return dp(i + 1, total + nums[i]) + dp(i + 1, total - nums[i])
+            # return memo[(i, total)]
 
         return dp(0, 0)
