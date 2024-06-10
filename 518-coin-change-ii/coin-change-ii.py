204243d348349e3468d14273d5efbb9f5c5756e7
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-10 14:31:25 -0700

    Time: 224 ms (53.19%) | Memory: 31.9 MB (47.91%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -6,6 +6,13 @@ class Solution:
         for i in range(n):
             dp[i][0] = 1  # if amount is 0, there is only 1 way
 
+        """
+        The loop over the coins should be the outer loop,
+        and the loop over the amounts should be the inner loop.
+        This is to ensure the i - 1 states do not get overwritten
+        before they have been used to calculate the i states,
+        as i represents the number of kinds of coins used.
+        """
         for i in range(n):
             for amt in range(1, amount + 1):
                 dp[i][amt] = dp[i - 1][amt] if i >= 1 else 0
