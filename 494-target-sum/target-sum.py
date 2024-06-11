415b0a3fe708cae6b6bc55aa25058e4a275d07e2
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-10 22:04:32 -0700

    Time: 66 ms (94.62%) | Memory: 16.6 MB (89.51%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -36,44 +36,4 @@ class Solution:
         # The last entry in DP array is our answer - the number of ways to sum to d
         return dp[d]
 
-        """
-
-        Suppose your `nums` array is `[1, 2, 3]`, and your goal (`d`) is `3`. We need to calculate the number of ways to achieve all sums up to `3` using these numbers.
-
-        Initial `dp` array: `[1, 0, 0, 0]` (1 way to make sum `0`: by choosing no numbers)
-
-        ### Right-to-Left Iteration (Correct)
-        Let's start with `num = 1` (first iteration):
-
-        - `dp` before iteration: `[1, 0, 0, 0]`
-        - Updating `dp[i]` for each `i` in `reversed(range(1, 4))`: `i` goes `3, 2, 1`
-            - `dp[3]` becomes `0 + dp[2]` (previous dp[3] plus dp[2]), dp does not change this pass since dp[2] is currently 0.
-            - `dp[2]` becomes `0 + dp[1]` (previous dp[2] plus dp[1]), dp does not change this pass since dp[1] is currently 0.
-            - `dp[1]` becomes `0 + dp[0]` (previous dp[1] plus dp[0]), so `dp[1] = 1`.
-        - `dp` after first iteration: `[1, 1, 0, 0]`
-
-        Next `num` is `2` (second iteration):
-
-        - `dp` before iteration: `[1, 1, 0, 0]`
-        - Updating `dp[i]` for each `i` in `reversed(range(2, 4))`: `i` goes `3, 2`
-            - `dp[3]` becomes `0 + dp[1]`, so `dp[3] = 1`.
-            - `dp[2]` becomes `0 + dp[0]`, so `dp[2] = 1`.
-        - `dp` after second iteration: `[1, 1, 1, 1]`
-
-        And so on for `num = 3`. Only after `num` is processed do we update `dp[i]`, so we're always using the untainted, original counts.
-
-        ### Left-to-Right Iteration (Incorrect)
-
-        Let's again start with `num = 1` (first iteration):
-
-        - `dp` before iteration: `[1, 0, 0, 0]`
-        - Updating `dp[i]` for each `i` in `range(1, 4)`: `i` goes `1, 2, 3`
-            - `dp[1]` becomes `0 + dp[0]`, so `dp[1] = 1`.
-            - `dp[2]` becomes `0 + dp[1]` (which was just updated), so `dp[2] = 1`.
-            - `dp[3]` becomes `0 + dp[2]` (which was just updated), so `dp[3] = 1`.
-        - `dp` after first iteration: `[1, 1, 1, 1]`
-
-        Notice here that after immediately updating `dp[1]`, we used that new value to calculate `dp[2]`, and the same for `dp[3]`, which means we’ve effectively counted `num = 1` multiple times, which is incorrect.
-
-        This is why traversing the array in reverse is crucial. By going right-to-left, we always use the state of the `dp` array before the current `num` is considered, ensuring that each number is only counted once for each sum.
-        """
+    
