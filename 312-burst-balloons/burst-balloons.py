f086c28e4cae351b58fafa4497a7b4cdf7f69ee4
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-17 14:08:37 -0700

    Time: 2787 ms (81.42%) | Memory: 18.5 MB (90.12%) - LeetSync

--- 312-burst-balloons/burst-balloons.py
+++ 312-burst-balloons/burst-balloons.py
@@ -2,9 +2,10 @@ class Solution:
     def maxCoins(self, nums: List[int]) -> int:
         nums = [1] + nums + [1]
         n = len(nums)
+        # dp[i][j] = x 表示，戳破气球 i 和气球 j 之间（开区间，不包括 i 和 j）的所有气球，可以获得的最高分数为 x。
         dp = [[0] * n for _ in range(n)]
 
-        # dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j] + points[i]*points[j]*points[k])
+        # dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j] + nums[i]*nums[j]*nums[k])
         # 从下往上，从左往右
         for i in reversed(range(n)):
             for j in range(i + 1, n):
