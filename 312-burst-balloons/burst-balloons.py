6b01209898bd82326f31456ccf1c9a40a34dbcd4
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-17 14:05:50 -0700

    Time: 3292 ms (71.75%) | Memory: 18.4 MB (97.81%) - LeetSync

--- 312-burst-balloons/burst-balloons.py
+++ 312-burst-balloons/burst-balloons.py
@@ -1,25 +1,16 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        if len(nums) > 1 and len(set(nums)) == 1:
-            return (len(nums) - 2) * nums[0] ** 3 + nums[0] ** 2 + nums[0]
-
         nums = [1] + nums + [1]
         n = len(nums)
-
-        # dp[i][j] represents maxCoins if burst all from [i, j]
-
         dp = [[0] * n for _ in range(n)]
 
-        # do not burst the fake ballons
-        # 从下到上，从左到右
-        for left in reversed(range(1, n - 1)):
-            for right in range(left, n - 1):
-                # find the last burst one in nums[left]...nums[right]
-                for i in range(left, right + 1):
-                    # nums[i] is the last burst one
-                    gain = nums[left - 1] * nums[i] * nums[right + 1]
-                    # we burst all balloons except the ith balloon. What we gain is
-                    remaining = dp[left][i - 1] + dp[i + 1][right]
-                    dp[left][right] = max(remaining + gain, dp[left][right])
+        # dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j] + points[i]*points[j]*points[k])
+        # 从下往上，从左往右
+        for i in reversed(range(n)):
+            for j in range(i + 1, n):
+                for k in range(i + 1, j):
+                    dp[i][j] = max(
+                        dp[i][j], dp[i][k] + dp[k][j] + nums[i] * nums[j] * nums[k]
+                    )
 
-        return dp[1][-2]
+        return dp[0][n - 1]
