5b6c2107409331a62f2ce18096002c2363303ece
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-20 15:27:19 -0700

    Time: 62 ms (30.46%) | Memory: 17.8 MB (29.12%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,12 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
         # buy: Maximum profit till now with the last action being a buy
         # sell: Maximum profit till now with the last action being a sell
         # buy is initially -infinity because we have not made any purchase yet,
         # thus cannot have a 'profit' from buying
         # sell is 0 because no transaction is a valid initial state with 0 profit
 
-        buy, sell = float("-inf"), 0
+        buy, sell = -prices[0], 0
         for price in prices:
             # Can we make more money by buying today
             buy = max(buy, sell - price)
