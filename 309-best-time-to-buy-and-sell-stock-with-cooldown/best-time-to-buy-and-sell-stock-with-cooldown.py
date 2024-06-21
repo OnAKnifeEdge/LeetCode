42a48697b1f91563cb7f819e61080d71c344c0a6
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-20 18:48:57 -0700

    Time: 34 ms (96.27%) | Memory: 17 MB (64.64%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,10 +4,11 @@ class Solution:
 
         for price in prices:
             prev_sold = sell
-            # Realize profit from selling the stock bought
-            sell = buy + price
+
             # Keeping the current bought stock or buying new after cooldown
             buy = max(buy, cooldown - price)
+            # Realize profit from selling the stock bought
+            sell = max(sell, buy + price)
             # Transition into cooldown after selling or stay in cooldown
             cooldown = max(cooldown, prev_sold)
         return max(sell, cooldown)
