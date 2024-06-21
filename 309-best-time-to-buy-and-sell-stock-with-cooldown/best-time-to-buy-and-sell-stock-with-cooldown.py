7b3ba32f21a5bbcf8405e585874f2fb35575d77f
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-20 18:47:11 -0700

    Time: 39 ms (84.74%) | Memory: 16.8 MB (92.45%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,13 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        buy, cooldown, sold = float("-inf"), 0, 0
+        buy, cooldown, sell = float("-inf"), 0, 0
 
         for price in prices:
-            prev_sold = sold
+            prev_sold = sell
             # Realize profit from selling the stock bought
-            sold = buy + price
+            sell = buy + price
             # Keeping the current bought stock or buying new after cooldown
             buy = max(buy, cooldown - price)
             # Transition into cooldown after selling or stay in cooldown
             cooldown = max(cooldown, prev_sold)
-        return max(sold, cooldown)
+        return max(sell, cooldown)
