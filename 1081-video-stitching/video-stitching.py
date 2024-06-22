5d8816d7c8f28548e64125bf8069484ef354bb10
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-21 20:40:50 -0700

    Time: 32 ms (94.15%) | Memory: 16.3 MB (99.71%) - LeetSync

--- 1081-video-stitching/video-stitching.py
+++ 1081-video-stitching/video-stitching.py
@@ -1,40 +1,30 @@
 class Solution:
     def videoStitching(self, clips: List[List[int]], time: int) -> int:
-        # If the target time is 0 or less, no clips are needed.
-        if time <= 0:
+        if not clips:
             return 0
-
-        # Sort clips based on their start times; for ties, longer clips come first.
         clips.sort(key=lambda x: (x[0], -x[1]))
-
+        current_end, farthest_end = 0, 0
         count = 0
-        current_end, furtherest_end = 0, 0
-        for start, end in clips:
-            # Skip any clips that start after the target
-            if start > time:
-                break
 
-            if start > current_end and start > furtherest_end:
-                # if gap
+        for start, end in clips:
+            if start > time:
+                continue
+
+            if start > current_end and start > farthest_end:
                 return -1
 
-            # Making a Jump:
-            # If a clip starts after the current_end but not beyond the next_end,
-            # the coverage is extended to next_end, and count is incremented.
             if start > current_end:
-                current_end = furtherest_end
+                current_end = farthest_end
                 count += 1
-                # if current_end >= time:
-                #     # If we already cover the required time interval
-                #     return count
 
-            # Extending furtherest_end that can be reached without making another jump
-            # for clips that start within the current coverage (start <= current_end)
+                if current_end >= time:
+                    return count
+
             if start <= current_end:
-                furtherest_end = max(furtherest_end, end)
+                farthest_end = max(farthest_end, end)
 
         if current_end < time:
-            if furtherest_end < time:
+            if farthest_end < time:
                 return -1
             count += 1
         return count
