77657f42d03a58dcabc3ecea1eb916a0d262a727
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-22 16:55:19 -0700

    Time: 159 ms (52.74%) | Memory: 17.5 MB (43.53%) - LeetSync

--- 694-number-of-distinct-islands/number-of-distinct-islands.py
+++ 694-number-of-distinct-islands/number-of-distinct-islands.py
@@ -1,7 +1,22 @@
 class Solution:
+    """
+    1, 1, 0
+    0, 1, 1
+    0, 0, 0
+    1, 1, 1
+    0, 1, 0
+
+    R -> D -> R
+
+    有撤回了话：
+    R -> D -> R -> E -> E -> E
+
+    R -> D -> E -> R -> E -> E
+    """
+
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        DIRECTIONS = {"r": (0, 1), "l": (0, -1), "u": (-1, 0), "d": (1, 0)}
+        DIRECTIONS = {"u": (-1, 0), "d": (1, 0), "r": (0, 1), "l": (0, -1)}
         islands = set()
 
         def dfs(i, j, path, direction):
@@ -16,13 +31,13 @@ class Solution:
             path.append(direction)
             for d, (dx, dy) in DIRECTIONS.items():
                 dfs(i + dx, j + dy, path, d)
-            path.append('e')
+            path.append("e")  # ?
 
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == 1:
                     path = []
-                    dfs(i, j, path, 's')
+                    dfs(i, j, path, "s")  # ?
                     islands.add("".join(path))
         print(islands)
         return len(islands)
