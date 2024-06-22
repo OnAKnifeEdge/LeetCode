294190baaac64b95023c1f3a288086e05c4814fc
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-22 14:53:46 -0700

    Time: 232 ms (75.94%) | Memory: 18.8 MB (72.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,19 +4,14 @@ class Solution:
         ROWS, COLS = len(grid), len(grid[0])
         DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-        def is_valid(i, j):
-            if i < 0 or i == ROWS:
-                return False
-            if j < 0 or j == COLS:
-                return False
-            return True
-
         def dfs(i, j):
-            if not is_valid(i, j):
+            if i < 0 or i >= ROWS or j < 0 or j >= COLS:
                 return
+            # if it is water or visited
             if grid[i][j] != "1":
                 return
-            grid[i][j] = "#"
+            # mark as visited
+            grid[i][j] = "-1"
             for dx, dy in DIRECTIONS:
                 di = i + dx
                 dj = j + dy
