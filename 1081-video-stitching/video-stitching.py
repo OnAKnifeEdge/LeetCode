a001011206ccb30af7e4ebcd15b34d70f79e07e1
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-21 21:27:05 -0700

    Time: 34 ms (87.43%) | Memory: 16.4 MB (83.92%) - LeetSync

--- 1081-video-stitching/video-stitching.py
+++ 1081-video-stitching/video-stitching.py
@@ -1,6 +1,19 @@
 class Solution:
     def videoStitching(self, clips: List[List[int]], time: int) -> int:
-        # If the time range to cover is zero, no clips are needed.
+        """
+        If the start time of a clip is greater than the current time target,
+            it's irrelevant, and we continue to the next.
+
+        If the clip starts within the current covered range (i.e., start <= current_end),
+            we update farthest_end to include this clip's end if it extends our reach further.
+
+        If the clip starts beyond current_end but within the range that farthest_end can extend to,
+            we "jump" to this farthest_end by updating current_end and increment the count.
+
+            Then, we consider the current clip for further extending farthest_end.
+        If the clip starts beyond both current_end and farthest_end,
+            there's a gap that cannot be covered, and hence, we return -1.
+        """
         if time == 0:
             return 0
 
@@ -18,7 +31,6 @@ class Solution:
 
             if start <= current_end:
                 farthest_end = max(farthest_end, end)
-
             elif start <= farthest_end:
                 current_end = farthest_end
                 count += 1
