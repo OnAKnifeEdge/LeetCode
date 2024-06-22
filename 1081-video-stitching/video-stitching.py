c7545aaff8fd4b35d95c091151db23860284454c
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-21 19:05:37 -0700

    Time: 29 ms (97.37%) | Memory: 16.4 MB (83.92%) - LeetSync

--- 1081-video-stitching/video-stitching.py
+++ 1081-video-stitching/video-stitching.py
@@ -7,33 +7,31 @@ class Solution:
         # Sort clips based on their start times; for ties, longer clips come first.
         clips.sort(key=lambda x: (x[0], -x[1]))
 
-        ret, cur_end, next_end = 0, 0, 0
+        count = 0
+        current_end, next_end = 0, 0
         for start, end in clips:
             # Skip any clips that start after the current target time; they are not needed.
             if start > time:
                 break
 
-            if start > cur_end and start > next_end:
+            if start > current_end and start > next_end:
                 # Start is beyond both current and next potential end, impossible to continue
                 return -1
-            elif start > cur_end:
+            elif start > current_end:
                 # We need to make a jump here
-                cur_end, ret = next_end, ret + 1
-                if cur_end >= time:
+                current_end = next_end
+                count += 1
+                if current_end >= time:
                     # If we already cover the required time interval, break early
                     break
 
-            if start <= cur_end:
+            if start <= current_end:
                 # Extend the furthest reach within the current segment
                 next_end = max(next_end, end)
 
         # Final check after processing all clips
-        if cur_end < time:
-            if next_end >= time:
-                # One last jump needed
-                ret += 1
-            else:
-                # If the next_end does not reach or exceed the time required, it's impossible
+        if current_end < time:
+            if next_end < time:
                 return -1
-
-        return ret
+            count += 1
+        return count
