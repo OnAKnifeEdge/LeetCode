e82d3643385319dcdda1e25dd4fab56c57b89473
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-22 14:50:41 -0700

    Time: 105 ms (55.39%) | Memory: 17.1 MB (70.66%) - LeetSync

---
+++ 1380-number-of-closed-islands/number-of-closed-islands.py
@@ -0,0 +1,39 @@
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        count = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        def is_valid(i, j):
+            if i < 0 or i == ROWS:
+                return False
+            if j < 0 or j == COLS:
+                return False
+            return True
+
+
+        def dfs(i, j):
+            if not is_valid(i, j):
+                return
+            if grid[i][j] != 0:
+                return
+            grid[i][j] = -1
+            for dx, dy in DIRECTIONS:
+                x, y = i + dx, j + dy
+                dfs(x, y)
+
+        for i in range(ROWS):
+            dfs(i, 0)
+            dfs(i, COLS - 1)
+
+        for j in range(COLS):
+            dfs(0, j)
+            dfs(ROWS - 1, j)
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 0:
+                    dfs(i, j)
+                    count += 1
+
+        return count
