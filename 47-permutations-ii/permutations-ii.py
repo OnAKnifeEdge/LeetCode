0bb3dd690f887e1fa267b86ea56dd4f1fb9ca946
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-22 13:35:55 -0700

    Time: 41 ms (97.30%) | Memory: 16.8 MB (70.71%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -12,6 +12,12 @@ class Solution:
             for i, num in enumerate(nums):
                 if used[i]:
                     continue
+                # 当出现重复元素时，
+                # 比如输入 nums = [1,2,2',2'']，
+                # 2' 只有在 2 已经被使用的情况下才会被选择，
+                # 同理，2'' 只有在 2' 已经被使用的情况下才会被选择，
+                # 这就保证了相同元素在排列中的相对位置保证固定。
+                # skip duplicates where the previous identical element wasn't used.
                 if i > 0 and not used[i - 1] and nums[i] == nums[i - 1]:
                     continue
                 current.append(num)
