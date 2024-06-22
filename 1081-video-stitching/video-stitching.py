fb00452e7fa6b3b14d75e00551888d6be7bf1bbe
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-21 19:20:46 -0700

    Time: 45 ms (16.67%) | Memory: 16.5 MB (83.92%) - LeetSync

--- 1081-video-stitching/video-stitching.py
+++ 1081-video-stitching/video-stitching.py
@@ -8,31 +8,33 @@ class Solution:
         clips.sort(key=lambda x: (x[0], -x[1]))
 
         count = 0
-        current_end, next_end = 0, 0
+        current_end, furtherest_end = 0, 0
         for start, end in clips:
-            # Skip any clips that start after the current target time; they are not needed.
+            # Skip any clips that start after the target
             if start > time:
                 break
 
-            if start > current_end and start > next_end:
+            if start > current_end and start > furtherest_end:
                 # if gap
                 return -1
 
+            # Making a Jump:
+            # If a clip starts after the current_end but not beyond the next_end,
+            # the coverage is extended to next_end, and count is incremented.
             if start > current_end:
-                # We need to make a jump here
-                current_end = next_end
+                current_end = furtherest_end
                 count += 1
                 if current_end >= time:
-                    # If we already cover the required time interval, break early
+                    # If we already cover the required time interval
                     return count
 
+            # Extending furtherest_end that can be reached without making another jump
+            # for clips that start within the current coverage (start <= current_end)
             if start <= current_end:
-                # Extend the furthest reach within the current segment
-                next_end = max(next_end, end)
+                furtherest_end = max(furtherest_end, end)
 
-        # Final check after processing all clips
         if current_end < time:
-            if next_end < time:
+            if furtherest_end < time:
                 return -1
             count += 1
         return count
