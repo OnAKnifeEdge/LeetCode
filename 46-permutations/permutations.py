fdd48419fb0ca047846352258070959b8334f870
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-21 22:23:46 -0700

    Time: 46 ms (21.04%) | Memory: 16.7 MB (63.06%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,48 +1,12 @@
 class Solution:
-    def permute_swap(self, nums: List[int]) -> List[List[int]]:
-        result = []
-
-        def backtrack(idx: int):
-            if idx == len(nums):
-                result.append(nums[:])
-                return
-
-            for i in range(idx, len(nums)):
-                nums[i], nums[idx] = nums[idx], nums[i]
-                backtrack(idx + 1)
-                nums[i], nums[idx] = nums[idx], nums[i]
-
-        backtrack(0)
-
-        return result
-
-    def permute_backtrack(self, nums: List[int]) -> List[List[int]]:
-        result = []
-
-        def backtrack(current: List[int]):
-            if len(current) == len(nums):
-                result.append(current[:])
-                return
-
-            for num in nums:
-                if num in current:
-                    continue
-                current.append(num)
-                backtrack(current)
-                current.pop()
-
-        backtrack([])
-
-        return result
-
     def permute(self, nums: List[int]) -> List[List[int]]:
         result = []
-        used = [False] * len(nums)
+        n = len(nums)
+        used = [False] * n
 
-        def backtrack(current: List[int]):
+        def backtrack(current):
             if len(current) == len(nums):
                 result.append(current[:])
-                return
 
             for i, num in enumerate(nums):
                 if used[i]:
@@ -50,7 +14,7 @@ class Solution:
                 current.append(num)
                 used[i] = True
                 backtrack(current)
-                current.pop()
+                current.remove(num)
                 used[i] = False
 
         backtrack([])
