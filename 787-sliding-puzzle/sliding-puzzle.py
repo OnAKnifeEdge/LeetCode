0833103ad2fb79af6765710c1ff3177f9190d21e
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-22 23:35:01 -0700

    Time: 41 ms (82.24%) | Memory: 16.4 MB (99.20%) - LeetSync

--- 787-sliding-puzzle/sliding-puzzle.py
+++ 787-sliding-puzzle/sliding-puzzle.py
@@ -1,32 +1,38 @@
 class Solution:
     def slidingPuzzle(self, board: List[List[int]]) -> int:
-        ROWS, COLS = len(board), len(board[0])
-        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        m = ROWS * COLS
-        puzzle = [0] * (m)
-        neighbors = [0] * m
+        # ROWS, COLS = len(board), len(board[0])
+        # DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        # m = ROWS * COLS
 
-        def to_idx(i, j):
-            if i < 0 or j < 0 or i == ROWS or j == COLS:
-                return None
-            return i * COLS + j
+        # puzzle = [0] * (m)
+        # neighbors = [0] * m
 
-        def map_neighbors(i, j):
-            neighbor = []
-            for dx, dy in DIRECTIONS:
-                idx = to_idx(i + dx, j + dy)
-                if idx is not None:
-                    neighbor.append(idx)
-            return neighbor
+        # def to_idx(i, j):
+        #     if i < 0 or j < 0 or i == ROWS or j == COLS:
+        #         return None
+        #     return i * COLS + j
 
-        def flatten():
-            for i in range(ROWS):
-                for j in range(COLS):
-                    idx = to_idx(i, j)
-                    puzzle[idx] = board[i][j]
-                    neighbors[idx] = map_neighbors(i, j)
+        # def map_neighbors(i, j):
+        #     neighbor = []
+        #     for dx, dy in DIRECTIONS:
+        #         idx = to_idx(i + dx, j + dy)
+        #         if idx is not None:
+        #             neighbor.append(idx)
+        #     return neighbor
 
-        flatten()
+        # def flatten():
+        #     for i in range(ROWS):
+        #         for j in range(COLS):
+        #             idx = to_idx(i, j)
+        #             puzzle[idx] = board[i][j]
+        #             neighbors[idx] = map_neighbors(i, j)
+
+        # flatten()
+
+        neighbors = ((1, 3), (0, 2, 4), (1, 5), (0, 4), (1, 3, 5), (2, 4))
+        puzzle = [*board[0], *board[1]]
+
+
         start = tuple(puzzle)
         end = (1, 2, 3, 4, 5, 0)
 
