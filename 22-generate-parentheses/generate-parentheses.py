06dafe734f8926809679417da63dd65cabe38bf4
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-22 19:01:14 -0700

    Time: 38 ms (54.48%) | Memory: 17.1 MB (5.03%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+
+        def backtrack(left, right, path):
+            if left > right:
+                return
+            if left < 0 or right < 0:
+                return
+            if left == 0 and right == 0:
+                result.append(path[:])
+                return
+
+            path.append("(")
+            backtrack(left - 1, right, path)
+            path.pop()
+
+            path.append(")")
+            backtrack(left, right - 1, path)
+            path.pop()
+
+        backtrack(n, n, [])
+        return [''.join(r) for r in result]
