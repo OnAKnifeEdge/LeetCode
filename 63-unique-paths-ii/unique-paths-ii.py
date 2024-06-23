45c78e360dbc52c74edf13a88ac0b42c8bd8bce0
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-23 15:05:00 -0700

    Time: 39 ms (84.97%) | Memory: 16.5 MB (77.88%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,13 +1,29 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         ROWS, COLS = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[0] * (COLS + 1) for _ in range(ROWS + 1)]
-        dp[1][1] = 1 if obstacleGrid[0][0] == 0 else 0
-        for i in range(1, ROWS + 1):
-            for j in range(1, COLS + 1):
-                if i == 1 and j == 1:
-                    continue
-                if obstacleGrid[i - 1][j - 1] == 1:
-                    continue
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        if obstacleGrid[0][0] == 1:
+            return 0
+
+        dp = [[0] * COLS for _ in range(ROWS)]
+        dp[0][0] = 1
+
+        for i in range(1, ROWS):
+            if obstacleGrid[i][0] == 1:
+                dp[i][0] = 0
+            else:
+                dp[i][0] = dp[i - 1][0]
+
+        for j in range(1, COLS):
+            if obstacleGrid[0][j] == 1:
+                dp[0][j] = 0
+            else:
+                dp[0][j] = dp[0][j - 1]
+
+        for i in range(1, ROWS):
+            for j in range(1, COLS):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         return dp[-1][-1]
