81899751760747c0b2f30c80fb8d7b146862a91b
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-22 22:57:29 -0700

    Time: 450 ms (46.12%) | Memory: 17.7 MB (91.11%) - LeetSync

--- 753-open-the-lock/open-the-lock.py
+++ 753-open-the-lock/open-the-lock.py
@@ -1,100 +1,42 @@
 class Solution:
-    # bidirectional bfs
     def openLock(self, deadends: List[str], target: str) -> int:
-        # A helper function to get all possible combinations from the current state
-        def neighbors(state: str) -> List[str]:
-            for i in range(4):
-                digit = int(state[i])
-                for move in [-1, 1]:
-                    new_digit = (digit + move) % 10
-                    yield state[:i] + str(new_digit) + state[i + 1:]
-
-        # Helper method to expand the BFS from one side
-        """
-        to explore all adjacent combinations (states) of the current lock combination
-        and track the number of moves (or turns) it took to get there.
-        """
-
-        def next_state(
-            q: Deque[Tuple[str, int]],
-            v: Dict[str, int],
-            vv: Dict[str, int],
-            no: Set[str],
-        ) -> int:
-            for _ in range(len(q)):
-                current, turn = q.popleft()
-                if current in vv:
-                    # If current exists in the other visited,
-                    # that means we found an intersection.
-                    # Total turns are the sum of turns from both sides
-                    return turn + vv[current]
-                for state in neighbors(current):
-                    if state not in v and state not in no:
-                        # Keep searching, add next_state to visited and q
-                        v[state] = turn + 1
-                        q.append((state, turn + 1))
-            return -1
 
         no = set(deadends)
-        if target in no or "0000" in no:
-            return -1
+        step = 0
 
-        # Initial state with turn counts
-        visited_start, visited_target = {"0000": 0}, {target: 0}
-        q1, q2 = deque([("0000", 0)]), deque([(target, 0)])
+        def plus_one(code, i):
+            c = code[i]
+            val = int(c)
+            if val == 9:
+                return code[:i] + '0' + code[i+1:]
+            return code[:i] + str(val + 1) + code[i+1:]
 
-        while q1 and q2:
-            from_start = next_state(q1, visited_start, visited_target, no)
-            if from_start != -1:
-                return from_start
+        def minus_one(code, i):
+            c = code[i]
+            val = int(c)
+            if val == 0:
+                return code[:i] + '9' + code[i+1:]
+            return code[:i] + str(val - 1) + code[i+1:]
 
-            from_target = next_state(q2, visited_target, visited_start, no)
-            if from_target != -1:
-                return from_target
+        q = deque(["0000"])
+        visited = {"0000"}
+        while q:
+            n = len(q)
+            for i in range(n):
+                code = q.popleft()
+                if code in no:
+                    continue
+                if code == target:
+                    return step
+                for j in range(4):
+                    up = plus_one(code, j)
+                    down = minus_one(code, j)
+                    if up not in visited:
+                        q.append(up)
+                        visited.add(up)
+                    if down not in visited:
+                        q.append(down)
+                        visited.add(down)
+            step += 1
 
         return -1
-
-    # one direction bfs
-    # def openLock_bfs(self, deadends: List[str], target: str) -> int:
-
-    #     no = set(deadends)
-    #     visited = set("0000")
-    #     q = deque([("0000", 0)])
-
-    #     while q:
-
-    #         current, turn = q.popleft()
-    #         # 0000 (level 0)
-    #         if current == target:
-    #             return turn
-    #         if current in no:
-    #             visited.add((current, turn))
-    #             continue
-    #         # there are four digits
-    #         for i in range(4):
-    #             # 1000 9000 0100 0900 0010 0090 0001 0009 (level 1)
-    #             # plus = self.plus_one(current, i)
-    #             plus = self.rotate(current, i, 1)
-    #             if plus not in visited and plus not in no:
-    #                 q.append((plus, turn + 1))
-    #                 visited.add(plus)
-    #             # minus = self.minus_one(current, i)
-    #             minus = self.rotate(current, i, -1)
-    #             if minus not in visited and minus not in no:
-    #                 q.append((minus, turn + 1))
-    #                 visited.add(minus)
-    #     return -1
-
-    # def plus_one(self, s, i) -> str:
-    #     if s[i] == "9":
-    #         return s[:i] + "0" + s[i + 1:]
-    #     return s[:i] + chr(ord(s[i]) + 1) + s[i + 1:]
-
-    # def minus_one(self, s, i) -> str:
-    #     if s[i] == "0":
-    #         return s[:i] + "9" + s[i + 1:]
-    #     return s[:i] + chr(ord(s[i]) - 1) + s[i + 1:]
-
-    # def rotate(self, s, i, direction) -> str:
-    #     new_digit = (int(s[i]) + direction) % 10
-    #     return s[:i] + str(new_digit) + s[i + 1:]
