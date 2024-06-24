81184f158d4c2458db24bb2e62a4b80a99f895b4
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-24 11:50:41 -0700

    Time: 88 ms (60.21%) | Memory: 17.2 MB (26.82%) - LeetSync

---
+++ 1222-remove-covered-intervals/remove-covered-intervals.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        if not intervals:
+            return 0
+        n = len(intervals)
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+        start, end = intervals[0]
+
+        overlapped = 0
+
+        for i in range(1, n):
+            left, right = intervals[i]
+            if left <= end and right <= end:
+                overlapped += 1
+            elif right > end:
+                end = right
+            elif left > end:
+                start, end = left, right
+
+        return n - overlapped
