13bdb009cdc27b00187d98c782f54f039f316bd2
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-23 23:34:38 -0700

    Time: 141 ms (82.13%) | Memory: 21.1 MB (71.28%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -1,30 +1,20 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.n = len(w)
-        self.prefix_sum = [0] * (self.n + 1)
-        self.rand = random.Random()
-        for i in range(1, len(self.prefix_sum)):
-            self.prefix_sum[i] = self.prefix_sum[i - 1] + w[i - 1]
-        
-    def pickIndex(self) -> int:
-        target = self.rand.randint(1, self.prefix_sum[-1])
-        return self.left_bount(self.prefix_sum, target) - 1
-
-    def left_bount(self, nums: List[int], target: int) -> int:
-        if len(nums) == 0:
-            return -1
-        left, right = 1, len(nums)
-        while left < right:
-            mid = (left + right) // 2
-            if nums[mid] >= target:
-                right = mid
+        n = len(w)
+        self.prefix_sum = [0] * n
+        for i in range(n):
+            # [1, 2, 3]
+            # [0, 1, 3, 6]
+            if i == 0:
+                self.prefix_sum[i] = w[i]
             else:
-                left = mid + 1
-        return left
+                self.prefix_sum[i] = w[i] + self.prefix_sum[i - 1]
+        self.random = random.Random()
 
-
-        
+    def pickIndex(self) -> int:
+        target = self.random.randint(1, self.prefix_sum[-1])
+        return bisect_left(self.prefix_sum, target)
 
 
 # Your Solution object will be instantiated and called as such:
