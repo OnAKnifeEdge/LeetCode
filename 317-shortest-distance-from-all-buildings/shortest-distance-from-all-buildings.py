f15e5ec90e42fef2a503b5993e247f00dc8fc181
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-23 18:00:29 -0700

    Time: 2760 ms (52.64%) | Memory: 17.1 MB (42.44%) - LeetSync

--- 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.py
+++ 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.py
@@ -1,54 +1,57 @@
+from collections import deque
+from typing import List
+
 class Solution:
-    def bfs(self, grid, distances, row, col, totalHouses):
-        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        ROWS, COLS = len(grid), len(grid[0])
-
-        # Use a queue to do a bfs, starting from each cell located at (row, col).
-        q = deque([(row, col)])
-        visited = [[False] * COLS for _ in range(ROWS)]
-        visited[row][col] = True
-
-        steps = 0
-
-        while q:
-            steps += 1
-            for _ in range(len(q)):
-                x, y = q.popleft()
-
-                for dx, dy in DIRECTIONS:
-                    nx, ny = x + dx, y + dy
-                    if 0 <= nx < ROWS and 0 <= ny < COLS and not visited[nx][ny]:
-                        visited[nx][ny] = True
-
-                        # If we reached an empty cell, then add the distance
-                        # and increment the count of houses reached at this cell.
-                        if grid[nx][ny] == 0:
-                            distances[nx][ny][0] += steps
-                            distances[nx][ny][1] += 1
-                            q.append((nx, ny))
-
     def shortestDistance(self, grid: List[List[int]]) -> int:
+        def bfs(grid, distances, r, c, buildings):
+            DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            ROWS, COLS = len(grid), len(grid[0])
+
+            # Use a queue to do a BFS, starting from each cell located at (r, c).
+            q = deque([(r, c)])
+            visited = [[False] * COLS for _ in range(ROWS)]
+            visited[r][c] = True
+
+            distance = 0
+
+            while q:
+                distance += 1
+                for _ in range(len(q)):
+                    x, y = q.popleft()
+
+                    for dx, dy in DIRECTIONS:
+                        i, j = x + dx, y + dy
+                        if 0 <= i < ROWS and 0 <= j < COLS and not visited[i][j]:
+                            visited[i][j] = True
+
+                            # If we reached an empty cell, then add the distance
+                            # and increment the count of houses reached at this cell.
+                            if grid[i][j] == 0:
+                                distances[i][j][0] += distance
+                                distances[i][j][1] += 1
+                                q.append((i, j))
+
         if not grid or not grid[0]:
             return -1
 
-        rows, cols = len(grid), len(grid[0])
-        totalHouses = 0
-        minDistance = float("inf")
+        ROWS, COLS = len(grid), len(grid[0])
+        buildings = 0
+        min_distance = float("inf")
 
         # Store { total_dist, houses_count } for each cell.
-        distances = [[[0, 0] for _ in range(cols)] for _ in range(rows)]
+        distances = [[[0, 0] for _ in range(COLS)] for _ in range(ROWS)]
 
-        # Count houses and start bfs from each house.
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == 1:
-                    totalHouses += 1
-                    self.bfs(grid, distances, row, col, totalHouses)
+        # Count houses and start BFS from each house.
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    buildings += 1
+                    bfs(grid, distances, r, c, buildings)
 
-        # Check all empty lands with houses count equal to total houses and find the min ans.
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == 0 and distances[row][col][1] == totalHouses:
-                    minDistance = min(minDistance, distances[row][col][0])
+        # Check all empty lands with house count equal to total houses and find the min ans.
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0 and distances[r][c][1] == buildings:
+                    min_distance = min(min_distance, distances[r][c][0])
 
-        return minDistance if minDistance != float("inf") else -1
+        return min_distance if min_distance != float("inf") else -1
