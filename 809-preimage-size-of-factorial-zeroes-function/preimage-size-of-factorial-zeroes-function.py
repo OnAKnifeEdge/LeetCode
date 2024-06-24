321bc2da2962a9ba6d1ebf15a215aceae75119d2
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-24 10:44:39 -0700

    Time: 37 ms (46.30%) | Memory: 16.6 MB (5.56%) - LeetSync

--- 809-preimage-size-of-factorial-zeroes-function/preimage-size-of-factorial-zeroes-function.py
+++ 809-preimage-size-of-factorial-zeroes-function/preimage-size-of-factorial-zeroes-function.py
@@ -1,17 +1,23 @@
 class Solution:
-    def preimageSizeFZF(self, K: int) -> int:
-        l, r = 0, 2**63 - 1
-        while l < r:
-            mid = (l + r) // 2
-            if self.trailingZeroes(mid) < K:
-                l = mid + 1
-            else:
-                r = mid
-        return 5 - l % 5 if self.trailingZeroes(l) == K else 0
+    def preimageSizeFZF(self, k: int) -> int:
+
+        def atMost_k(k: int) -> int:
+            left, right = 0, 5 * k + 4
+            while left <= right:
+                mid = (left + right) // 2
+                count = self.trailingZeroes(mid)  # Use extracted function
+                if count <= k:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return right
+
+        return atMost_k(k) - atMost_k(k - 1)
 
     def trailingZeroes(self, n: int) -> int:
-        zero_count = 0
+        """Calculates the number of trailing zeroes in the factorial of n."""
+        zeroes = 0
         while n > 0:
             n //= 5
-            zero_count += n
-        return zero_count
+            zeroes += n
+        return zeroes
