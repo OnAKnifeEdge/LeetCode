2c4e9050e205eecde7d6faae54aaf598a54804ae
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-23 20:12:25 -0700

    Time: 2914 ms (49.71%) | Memory: 17.1 MB (52.21%) - LeetSync

--- 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.py
+++ 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.py
@@ -1,52 +1,47 @@
 class Solution:
     def shortestDistance(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return -1
+
         DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         ROWS, COLS = len(grid), len(grid[0])
 
+        distances = [[0] * COLS for _ in range(ROWS)]
+        reached = [[0] * COLS for _ in range(ROWS)]
         buildings = 0
-        min_distance = float("inf")
-
-        # Store { total_dist, houses_count } for each cell.
-        distances = [[[0, 0] for _ in range(COLS)] for _ in range(ROWS)]
 
         def bfs(r, c):
-
             q = deque([(r, c, 0)])
             visited = [[False] * COLS for _ in range(ROWS)]
             visited[r][c] = True
 
-            distance = 0
-
             while q:
-
-                for _ in range(len(q)):
-                    x, y, distance = q.popleft()
-                    distance += 1
-                    for dx, dy in DIRECTIONS:
-                        i, j = x + dx, y + dy
-                        if i < 0 or j < 0 or i == ROWS or j == COLS:
-                            continue
-                        if visited[i][j]:
-                            continue
+                x, y, distance = q.popleft()
+                for dx, dy in DIRECTIONS:
+                    i, j = x + dx, y + dy
+                    if i < 0 or j < 0 or i == ROWS or j == COLS:
+                        continue
+                    if visited[i][j]:
+                        continue
+                    if grid[i][j] == 0:
                         visited[i][j] = True
+                        distances[i][j] += distance + 1
+                        reached[i][j] += 1
+                        q.append((i, j, distance + 1))
 
-                        if grid[i][j] == 0:
-                            distances[i][j][0] += distance
-                            distances[i][j][1] += 1
-                            q.append((i, j, distance))
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 1:
+                    buildings += 1
+                    bfs(i, j)
 
-        if not grid or not grid[0]:
+        min_distance = float("inf")
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 0 and reached[i][j] == buildings:
+                    min_distance = min(min_distance, distances[i][j])
+
+        if min_distance == float("inf"):
             return -1
 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1:
-                    buildings += 1
-                    bfs(r, c)
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 0 and distances[r][c][1] == buildings:
-                    min_distance = min(min_distance, distances[r][c][0])
-
-        return min_distance if min_distance != float("inf") else -1
+        return min_distance
