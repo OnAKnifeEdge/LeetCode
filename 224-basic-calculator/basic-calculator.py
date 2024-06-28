1ba3a169805220f5f9a1f8e122b8e08f8afd306a
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-28 11:37:57 -0700

    Time: 75 ms (45.41%) | Memory: 19 MB (14.79%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,10 +1,8 @@
-from collections import deque
-
 class Solution:
     def calculate(self, s: str) -> int:
         s = s.replace(" ", "")
 
-        def process(q):
+        def process(q) -> int:
             stack = []
             sign = "+"
             num = 0
@@ -13,9 +11,10 @@ class Solution:
                 c = q.popleft()
                 if c.isdigit():
                     num = 10 * num + int(c)
+                # 遇到左括号开始递归计算 num
                 elif c == "(":
                     num = process(q)
-                if c in "+-*/" or c == ")" or not q:
+                if not c.isdigit() or not q:
                     if sign == "+":
                         stack.append(num)
                     elif sign == "-":
@@ -23,13 +22,12 @@ class Solution:
                     elif sign == "*":
                         stack.append(stack.pop() * num)
                     elif sign == "/":
-                        stack.append(int(stack.pop() // num))  # Use integer division
+                        stack.append(stack.pop() // num)
                     num = 0
                     sign = c
-                    if c == ")":
-                        break
-            
+                # 遇到右括号返回递归结果
+                if c == ")":
+                    break
             return sum(stack)
-        
-        return process(deque(s))
 
+        return process(deque(s))
