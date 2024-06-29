a1e24adca5750bf6289b85126e7178226093915b
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-29 14:45:23 -0700

    Time: 87 ms (85.09%) | Memory: 17.3 MB (83.06%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.py
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        inserted_left = 0
+        insertions = 0
         right_needed = 0
         for c in s:
             if c == "(":
@@ -8,11 +8,11 @@ class Solution:
 
                 if right_needed % 2 == 1:
                     right_needed -= 1
-                    inserted_left += 1
+                    insertions += 1
 
             elif c == ")":
                 right_needed -= 1
                 if right_needed < 0:
-                    inserted_left += 1
+                    insertions += 1
                     right_needed = 1
-        return inserted_left + right_needed
+        return insertions + right_needed
