59d0e8e90855e3cb918c25dfcbad7c22ede9afd8
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-29 10:01:23 -0700

    Time: 110 ms (47.35%) | Memory: 20 MB (32.08%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -8,7 +8,8 @@ class Solution:
             c = s[i]
             if c.isdigit():
                 num = num * 10 + int(c)
-            if c in "+-*/" or i == len(s) - 1:  # Process num when an operator or end is reached
+            if c in "+-*/" or i == len(s) - 1:
+                # Process num when an operator or end is reached
                 if sign == "+":
                     stack.append(num)
                 elif sign == "-":
@@ -16,7 +17,7 @@ class Solution:
                 elif sign == "*":
                     stack.append(stack.pop() * num)  # Evaluate * immediately
                 elif sign == "/":
-                    stack.append(int(stack.pop() / num)) # Evaluate / immediately
-                sign = c  
-                num = 0  
+                    stack.append(int(stack.pop() / num))  # Evaluate / immediately
+                sign = c
+                num = 0
         return sum(stack)
