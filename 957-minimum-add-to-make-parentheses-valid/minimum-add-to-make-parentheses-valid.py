561ab1fe9f23100d88d9e6423b967289b3981cbd
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-29 14:30:00 -0700

    Time: 31 ms (81.38%) | Memory: 16.5 MB (25.34%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -1,16 +1,13 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        # https://leetcode.com/problems/minimum-insertions-to-balance-a-parentheses-string/
-        if not s:
-            return 0
-        insertions = 0
-        required_closings = 0
+        insert_left = 0
+        need_right = 0
         for c in s:
             if c == "(":
-                required_closings += 1
+                need_right += 1
             elif c == ")":
-                required_closings -= 1
-                if required_closings == -1:
-                    insertions += 1
-                    required_closings = 0
-        return insertions + required_closings
+                need_right -= 1
+                if need_right == -1:
+                    need_right = 0
+                    insert_left += 1
+        return insert_left + need_right
