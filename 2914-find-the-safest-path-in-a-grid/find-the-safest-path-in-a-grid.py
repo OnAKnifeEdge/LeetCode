53670bb6d6cd554a25bec6a62b5a9223f0d1266a
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-06-29 22:53:38 -0700

    Time: 3156 ms (64.75%) | Memory: 41.1 MB (35.32%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.py
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.py
@@ -8,15 +8,10 @@ class UnionFind:
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
 
-    def union(self, u, v):
-        root_x, root_y = self.find(u), self.find(v)
-        if root_x == root_y:
-            return False
-
-        if root_x < root_y:
+    def union(self, x, y):
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x != root_y:
             self.parent[root_x] = root_y
-        elif root_y < root_x:
-            self.parent[root_y] = root_x
 
     def connected(self, x, y):
         return self.find(x) == self.find(y)
@@ -27,21 +22,22 @@ class Solution:
         n, m = len(grid), len(grid[0])
         nm = n * m
         uf = UnionFind(nm)
-        DIRECTIONS = [(0, 1), (1, 0), (-1, 0), (0, -1)]  # Correct directions
+        DIRECTIONS = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
         def flatten(i, j):
             return i * m + j
 
-        # cells_at_distance[i] stores cells at distance i from the nearest thief
         cells_at_distance = [[]]
+
+        # Locate all thieves
         for i in range(n):
             for j in range(m):
                 if grid[i][j]:
                     cells_at_distance[0].append((i, j))
                     grid[i][j] = None
 
-        # Calculate distances using BFS
-        while grid[0][0] is not None and grid[-1][-1] is not None:
+        # Process BFS to calculate distances
+        while cells_at_distance[-1]:
             current_level = cells_at_distance[-1]
             next_level = []
             cells_at_distance.append(next_level)
@@ -53,11 +49,13 @@ class Solution:
                         next_level.append((x, y))
                         grid[x][y] = None
 
-        current_level = cells_at_distance.pop()
-        for i, j in current_level:
+        cells_at_distance.pop()  # Remove the last empty list
+
+        # Mark cells at maximum distance from thieves as safe
+        for i, j in cells_at_distance.pop():
             grid[i][j] = 0
 
-        # Use Union-Find to check for a path from start to end
+        # Use Union-Find to connect safe cells
         for i in range(n):
             for j in range(m):
                 if grid[i][j] is not None:
@@ -67,8 +65,9 @@ class Solution:
                     if j + 1 < m and grid[i][j + 1] is not None:
                         uf.union(idx, idx + 1)
 
-        if uf.find(0) == nm - 1:
-            return len(cells_at_distance)
+        # Check if a path exists from start to end
+        if uf.find(0) == uf.find(nm - 1):
+            return len(cells_at_distance)  # Max distance from thieves
 
         # Recover nodes in decreasing order of distance from thieves
         while cells_at_distance:
@@ -79,8 +78,9 @@ class Solution:
                 for dx, dy in DIRECTIONS:
                     x, y = i + dx, j + dy
                     if 0 <= x < n and 0 <= y < m and grid[x][y] is not None:
-                        uf.union(idx, x * m + y)
-            if uf.find(0) == nm - 1:
+                        uf.union(idx, flatten(x, y))
+
+            if uf.find(0) == uf.find(nm - 1):
                 break
 
         return len(cells_at_distance)
