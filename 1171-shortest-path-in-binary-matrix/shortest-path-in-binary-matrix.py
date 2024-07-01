8d1014f07a21404b18bfc31d0370942e9baa2884
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-01 12:24:44 -0700

    Time: 514 ms (27.99%) | Memory: 18 MB (49.02%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -0,0 +1,37 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return -1
+        ROWS, COLS = len(grid), len(grid[0])
+        if grid[0][0] or grid[-1][-1]:
+            return -1
+        source = (0, 0)
+        destination = (ROWS - 1, COLS - 1)
+
+        def get_neighbors(i, j):
+            neighbors = []
+            for row_offset in [-1, 0, 1]:
+                for col_offset in [-1, 0, 1]:
+                    if row_offset == 0 and col_offset == 0:
+                        continue
+                    x, y = i + row_offset, j + col_offset
+                    if 0 <= x < ROWS and 0 <= y < COLS and not grid[x][y]:
+                        neighbors.append((x, y))
+            return neighbors
+
+        visited = {source}
+        q = deque([])
+        q.append((source, 1))
+
+        while q:
+            (i, j), clear_path = q.popleft()
+            if (i, j) == destination:
+                return clear_path
+
+            for x, y in get_neighbors(i, j):
+                if (x, y) in visited:
+                    continue
+                visited.add((x, y))
+                q.append(((x, y), clear_path + 1))
+
+        return -1
