12657b624662c30fa74ea7f3c82a687bed8462d9
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-01 20:15:54 -0700

    Time: 1495 ms (54.37%) | Memory: 76.1 MB (87.49%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,25 +1,46 @@
+class UnionFind:
+
+    def __init__(self, size):
+        self.parent = [i for i in range(size + 1)]
+        self.rank = [0] * (size + 1)
+
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        rx, ry = self.find(x), self.find(y)
+        if self.rank[rx] < self.rank[ry]:
+            self.parent[rx] = ry
+        elif self.rank[rx] > self.rank[ry]:
+            self.parent[ry] = rx
+        else:
+            self.parent[ry] = rx
+            self.rank[rx] += 1
+
+    def is_connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        def manhattan(point1, point2):
-            return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+
+        def manhattan(a, b):
+            return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
         n = len(points)
+        edges = []
+        for i, j in combinations(range(n), 2):
+            distance = manhattan(points[i], points[j])
+            edges.append((i, j, distance))
+        edges.sort(key=lambda x: x[2])
 
-        total_cost = 0
-        visited = set()
-
-        queue = [(0, 0)]  # (distance, vertex)
-
-        while len(visited) < n:
-            d, u = heappop(queue)
-            if u in visited:
+        mst = 0
+        uf = UnionFind(n)
+        for i, j, distance in edges:
+            if uf.is_connected(i, j):
                 continue
-
-            visited.add(u)
-            total_cost += d
-            for v in range(n):
-                if v in visited:
-                    continue
-                d = manhattan(points[u], points[v])
-                heappush(queue, (d, v))
-        return total_cost
+            uf.union(i, j)
+            mst += distance
+        return mst
