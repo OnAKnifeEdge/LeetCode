f74dfdb5080e2580dcc4b8608ec7f505bef0a428
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-03 16:36:24 -0700

    Time: 311 ms (6.58%) | Memory: 27.4 MB (39.91%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -7,14 +7,22 @@ class Solution:
             return 0
 
         min_diff = float("inf")
-        # remove smallest: 0, 1, 2, 3
-        for i in range(k):
-            j = n - k + i
-            min_diff = min(min_diff, nums[j] - nums[i])
-        return min_diff
+        # # remove smallest: 0, 1, 2, 3
+        # for i in range(k):
+        #     j = n - k + i
+        #     min_diff = min(min_diff, nums[j] - nums[i])
+        # return min_diff
+
         # return min(
         #     nums[-1] - nums[3],  # Remove 3 smallest
         #     nums[-2] - nums[2],  # Remove 2 smallest and 1 largest
         #     nums[-3] - nums[1],  # Remove 1 smallest and 2 largest
         #     nums[-4] - nums[0],  # Remove 3 largest
         # )
+
+        smallest_four = sorted(heapq.nsmallest(4, nums))
+        largest_four = sorted(heapq.nlargest(4, nums))
+
+        for i in range(k):
+            min_diff = min(min_diff, largest_four[i] - smallest_four[i])
+        return min_diff
