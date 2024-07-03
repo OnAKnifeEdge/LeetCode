78393941ec1f79cc14dacdbfa74be305baa4fe1a
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-02 21:33:11 -0700

    Time: 96 ms (65.35%) | Memory: 18.2 MB (11.08%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -2,27 +2,22 @@ class Solution:
     def findCheapestPrice(
         self, n: int, flights: List[List[int]], src: int, dst: int, k: int
     ) -> int:
-        """
-        dp(dst, k) = min(
-                        dp(s1, k - 1) + w1,
-                        dp(s2, k - 1) + w2
-                    )
-        """
-
-        visited = {}
-
         graph = defaultdict(list)
-        for f, t, price in flights:
-            graph[f].append((t, price))
+        for fr, to, cost in flights:
+            graph[fr].append((to, cost))
 
-        min_heap = [(0, 0, src)]  # cost, stops, node
-        while min_heap:
-            cost, stops, node = heappop(min_heap)
-            if node == dst and stops <= k + 1:
+        q = [(0, 0, src)]  # cost, stop, src]
+
+        visited = {}  # src: stops
+
+        while q:
+            cost, stop, node = heappop(q)
+            if node == dst and stop <= k + 1:
                 return cost
-            if node in visited and visited[node] < stops:
-                continue  # Skip if we visited the node with fewer stops
-            visited[node] = stops
-            for next_node, next_cost in graph[node]:
-                heappush(min_heap, (next_cost + cost, stops + 1, next_node))
+            if node in visited and visited[node] < stop:
+                continue
+            visited[node] = stop
+            neighbors = graph[node]
+            for new_node, new_cost in neighbors:
+                heappush(q, (cost + new_cost, stop + 1, new_node))
         return -1
