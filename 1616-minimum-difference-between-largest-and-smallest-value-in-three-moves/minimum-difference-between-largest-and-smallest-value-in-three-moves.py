6c6ac94e6fc356c5ce68f1b5be591ad34391338d
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-03 16:14:36 -0700

    Time: 284 ms (35.37%) | Memory: 27.9 MB (12.47%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -1,11 +1,20 @@
 class Solution:
     def minDifference(self, nums: List[int]) -> int:
+        k = 4
         nums.sort()
-        if len(nums) <= 4:
+        n = len(nums)
+        if n <= k:
             return 0
-        return min(
-            nums[-1] - nums[3],  # Remove 3 smallest
-            nums[-2] - nums[2],  # Remove 2 smallest and 1 largest
-            nums[-3] - nums[1],  # Remove 1 smallest and 2 largest
-            nums[-4] - nums[0],  # Remove 3 largest
-        )
+
+        min_diff = float("inf")
+        # remove smallest: 0, 1, 2, 3
+        for i in range(k):
+            j = n - k + i
+            min_diff = min(min_diff, nums[j] - nums[i])
+        return min_diff
+        # return min(
+        #     nums[-1] - nums[3],  # Remove 3 smallest
+        #     nums[-2] - nums[2],  # Remove 2 smallest and 1 largest
+        #     nums[-3] - nums[1],  # Remove 1 smallest and 2 largest
+        #     nums[-4] - nums[0],  # Remove 3 largest
+        # )
