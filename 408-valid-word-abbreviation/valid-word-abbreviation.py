61c4b48554d8a5d4f618d8593f2cbe8598d4f11d
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-04 15:40:52 -0700

    Time: 39 ms (30.54%) | Memory: 16.6 MB (34.62%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,22 +1,20 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        i, j = 0, 0
-        n = len(word)
-
-        while i < n and j < len(abbr):
-            if abbr[j].isdigit():
-                if abbr[j] == "0":
+        nums = 0
+        p = 0
+        for i, c in enumerate(abbr):
+            if c.isdigit():
+                if nums == 0 and c == "0":  # Check for leading zeros
                     return False
-                num = 0
-                while j < len(abbr) and abbr[j].isdigit():
-                    num = num * 10 + int(abbr[j])
-                    j += 1
-                i += num
+                nums = nums * 10 + int(c)
             else:
-                if i >= len(word):
+                p += nums
+                if p >= len(word) or word[p] != c:
                     return False
-                if word[i] != abbr[j]:
-                    return False
-                i += 1
-                j += 1
-        return i == n and j == len(abbr)
+                p += 1
+                nums = 0
+
+        if nums > 0:
+            p += nums
+
+        return p == len(word)
