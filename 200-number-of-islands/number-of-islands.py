4fde7c2d8b4bae9d59b1603d801813458109dc29
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-04 18:12:55 -0700

    Time: 222 ms (91.41%) | Memory: 18.9 MB (53.67%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,25 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        count = 0
+        if not grid:
+            return 0
+
         ROWS, COLS = len(grid), len(grid[0])
-        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
         def dfs(i, j):
-            if i < 0 or i >= ROWS or j < 0 or j >= COLS:
+            if i < 0 or j < 0 or i >= ROWS or j >= COLS:
                 return
-            # if it is water or visited
             if grid[i][j] != "1":
                 return
-            # mark as visited
-            grid[i][j] = "-1"
-            for dx, dy in DIRECTIONS:
-                di = i + dx
-                dj = j + dy
-                dfs(di, dj)
+            grid[i][j] = "#"
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                dfs(i + dx, j + dy)
+
+        count = 0
 
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == "1":
                     dfs(i, j)
                     count += 1
+
         return count
