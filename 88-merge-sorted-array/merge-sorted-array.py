8bf9b3ce7e42947ef59d08d363965b252ad62c48
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-04 19:03:34 -0700

    Time: 42 ms (41.28%) | Memory: 16.6 MB (47.97%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -7,18 +7,13 @@ class Solution:
             nums1, nums2 = nums2, nums1
         # nums1 is longer
         i, j = m - 1, n - 1
-        p = m + n - 1
 
-        while i >= 0 and j >= 0:
-            if nums1[i] >= nums2[j]:
+        for p in reversed(range(m + n)):
+            if j < 0:
+                break
+            if i >= 0 and nums1[i] >= nums2[j]:
                 nums1[p] = nums1[i]
                 i -= 1
             else:
                 nums1[p] = nums2[j]
                 j -= 1
-            p -= 1
-
-        while j >= 0:
-            nums1[p] = nums2[j]
-            p -= 1
-            j -= 1
