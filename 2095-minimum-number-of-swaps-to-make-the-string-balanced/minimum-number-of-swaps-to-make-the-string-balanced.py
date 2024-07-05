a41fdde8442be7d7173cc4c2057b76a8c557d87f
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-04 17:58:29 -0700

    Time: 166 ms (90.35%) | Memory: 27.8 MB (65.37%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,11 +1,11 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
         count = 0
-        for i in s:
-            if i == "[":
-                count += 1  # increment only if we encounter an open bracket. 
+        for bracket in s:
+            if bracket == "[":
+                count += 1
             else:
-                if count > 0:  #decrement only if count is positive. Else do nothing and move on. This is because for the case " ] [ [ ] " we do not need to in
+                if count > 0:
+                    # decrement only if count is positive.
                     count -= 1
         return (count + 1) // 2
-       
