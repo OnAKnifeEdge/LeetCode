e54d4b7364ef7e9c697589d5db2c650f589d27fb
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-04 18:43:43 -0700

    Time: 123 ms (95.24%) | Memory: 20.9 MB (97.29%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -1,19 +1,15 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        n = len(w)
-        self.prefix_sum = [0] * n
-        for i in range(n):
-            # [1, 2, 3]
-            # [1, 3, 6]
+        self.prefix_sum = [0] * len(w)
+        for i, num in enumerate(w):
             if i == 0:
-                self.prefix_sum[i] = w[i]
+                self.prefix_sum[0] = num
             else:
-                self.prefix_sum[i] = w[i] + self.prefix_sum[i - 1]
-
+                self.prefix_sum[i] = num + self.prefix_sum[i - 1]
 
     def pickIndex(self) -> int:
-        target = random.randint(1, self.prefix_sum[-1])
+        target = random.random() * self.prefix_sum[-1]
         return bisect_left(self.prefix_sum, target)
 
 
