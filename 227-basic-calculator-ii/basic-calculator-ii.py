52de1de7958089b3937778431614b2130aa6de95
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-05 18:23:39 -0700

    Time: 93 ms (71.63%) | Memory: 20 MB (30.47%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -5,22 +5,22 @@ class Solution:
 
         stack = []
         num = 0
-        sign = "+"
+        previous_sign = "+"
 
         for i in range(n):
             c = s[i]
             if c.isdigit():
                 num = num * 10 + int(c)
             if not c.isdigit() or i == n - 1:
-                if sign == "+":
+                if previous_sign == "+":
                     stack.append(num)
-                elif sign == "-":
+                elif previous_sign == "-":
                     stack.append(-num)
-                elif sign == "*":
+                elif previous_sign == "*":
                     stack.append(stack.pop() * num)
-                elif sign == "/":
+                elif previous_sign == "/":
                     stack.append(int(stack.pop() / num))
-                sign = c
+                previous_sign = c
                 num = 0
 
         return sum(stack)
