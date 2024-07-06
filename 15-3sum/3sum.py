7a94474bf44a43b5757ea141338e3f71aeb512bd
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-06 14:37:46 -0700

    Time: 565 ms (87.61%) | Memory: 20 MB (91.87%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,36 +1,33 @@
 class Solution:
-
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        if n < 3:
+            return []
         nums.sort()
+
         result = []
 
-        def twoSum(i):
-            lo = i + 1
-            hi = len(nums) - 1
-
-            while lo < hi:
-                left = nums[lo]
-                right = nums[hi]
-                total = nums[i] + left + right
+        def two_sum(idx):
+            # find i, j so that nums[idx] + nums[i] + nums[j] == 0
+            i = idx + 1
+            j = n - 1
+            while i < j:
+                total = nums[idx] + nums[i] + nums[j]
                 if total < 0:
-                    while lo < hi and nums[lo] == left:
-                        lo += 1
-                    continue
+                    i += 1
                 elif total > 0:
-                    while lo < hi and nums[hi] == right:
-                        hi -= 1
-                    continue
-                result.append([nums[i], left, right])
-                while lo < hi and nums[lo] == left:
-                    lo += 1
-                while lo < hi and nums[hi] == right:
-                    hi -= 1
+                    j -= 1
+                else:
+                    result.append((nums[idx], nums[i], nums[j]))
+                    i += 1
+                    j -= 1
+                    while i < j and nums[i] == nums[i - 1]:
+                        i += 1
 
-        for i, num in enumerate(nums):
+        for idx, num in enumerate(nums):
             if num > 0:
                 break
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            twoSum(i)
+            if idx == 0 or num != nums[idx - 1]:
+                two_sum(idx)
 
         return result
