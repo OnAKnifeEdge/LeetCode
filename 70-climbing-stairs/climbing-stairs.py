c3570bda8e3fd8a72bdbac9094984053467c0210
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-06 14:10:21 -0700

    Time: 42 ms (9.79%) | Memory: 16.5 MB (55.56%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,6 +2,6 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         # f(n) = f(n - 1) + f(n - 2)
         one_step, two_step = 1, 1
-        for i in reversed(range(n - 1)):
+        for i in range(2, n + 1):
             one_step, two_step = two_step, one_step + two_step
         return two_step
