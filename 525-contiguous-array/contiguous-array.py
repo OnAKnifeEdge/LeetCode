a6fe4f108d8a741fea98eae5bd931781c87ec660
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-06 15:22:09 -0700

    Time: 628 ms (32.15%) | Memory: 22.1 MB (44.81%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,21 +1,21 @@
 class Solution:
-    def findMaxLength(self, nums: List[int]) -> int:     
+    def findMaxLength(self, nums: List[int]) -> int:
         d = {}
         result = 0
         diff = 0
 
         for idx, num in enumerate(nums):
-            diff += 1 if num else -1 # when num is 1 increment when num is 0 decrement
+            diff += 1 if num else -1  # when num is 1 increment when num is 0 decrement
 
             if diff == 0:
                 result = max(result, idx + 1)
-            elif diff in d:
-                result = max(result, idx - d[diff])
             else:
-                d[diff] = idx
+                if diff in d:
+                    result = max(result, idx - d[diff])
+                if diff not in d:
+                    d[diff] = idx
 
         return result
-
 
         # # generator https://leetcode.com/problems/contiguous-array/solutions/4883501/97-90-short-and-clear-with-generator-of-cumulated-differences
         # def findMaxLength(self, nums: List[int]) -> int:
