ad697699535e06911f03edf9fca4cde61026431a
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-07 00:24:31 -0700

    Time: 1224 ms (85.84%) | Memory: 36.2 MB (27.51%) - LeetSync

--- 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.py
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.py
@@ -3,11 +3,13 @@ class Solution:
         forbidden_set = set(forbidden)
         max_length = 0
         n = len(word)
-        right = n - 1
+        right = n - 1  # track the right boundary of the current valid window
 
         for left in reversed(range(n)):
             for k in range(left, min(left + 10, right + 1)):
                 if word[left:k + 1] in forbidden_set:
+                    # If found, the right boundary is updated to k - 1
+                    # shrinks the current valid window from the right side
                     right = k - 1
                     break
             max_length = max(max_length, right - left + 1)
