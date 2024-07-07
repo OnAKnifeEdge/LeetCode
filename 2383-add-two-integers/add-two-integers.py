7d234ec8032361d9a6c129ba62f9298c467fa888
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-07 11:31:44 -0700

    Time: 29 ms (88.43%) | Memory: 16.5 MB (54.34%) - LeetSync

---
+++ 2383-add-two-integers/add-two-integers.py
@@ -0,0 +1,4 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        # https://leetcode.com/problems/add-two-integers/solutions/1968134/21-different-ways-to-solve-this-problem
+        return num1 + num2
