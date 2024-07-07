f98b86d530bb8e139759d8f644c72f502f891895
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-07 00:17:59 -0700

    Time: 1261 ms (78.10%) | Memory: 36 MB (43.84%) - LeetSync

--- 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.py
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.py
@@ -3,12 +3,12 @@ class Solution:
         forbidden_set = set(forbidden)
         max_length = 0
         n = len(word)
-        right = n
+        right = n - 1
 
         for left in reversed(range(n)):
-            for k in range(left, min(left + 10, right) + 1):
-                if word[left:k] in forbidden_set:
+            for k in range(left, min(left + 10, right + 1)):
+                if word[left:k + 1] in forbidden_set:
                     right = k - 1
                     break
-            max_length = max(max_length, right - left)
+            max_length = max(max_length, right - left + 1)
         return max_length
