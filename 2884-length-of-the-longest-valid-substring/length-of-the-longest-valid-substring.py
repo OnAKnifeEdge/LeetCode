d824fbbf12555ef8bba39691eda45919f3c05e0b
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-07 00:16:46 -0700

    Time: 1258 ms (79.42%) | Memory: 36.1 MB (35.58%) - LeetSync

--- 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.py
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.py
@@ -1,53 +1,14 @@
-class TrieNode:
-    def __init__(self):
-        self.children = defaultdict(TrieNode)
-        self.is_end = False
-
-
-class Trie:
-
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word):
-        node = self.root
-        for c in word:
-            node = node.children[c]
-        node.is_end = True
-
-    def has_forbidden_substring(self, prefix):
-        node = self.root
-        for c in prefix:
-            if c not in node.children:
-                return False
-            node = node.children[c]
-            if node.is_end:
-                return True
-        return False
-
-
 class Solution:
     def longestValidSubstring(self, word: str, forbidden: List[str]) -> int:
-        trie = Trie()
-
-        for f in forbidden:
-            trie.insert(f)
-
+        forbidden_set = set(forbidden)
+        max_length = 0
         n = len(word)
         right = n
-        max_length = 0
 
         for left in reversed(range(n)):
-            # Check substrings starting from the current left position up to right
-            # We only need to check substrings up to length 10 (longest forbidden length)
-            for length in range(1, 11):
-                if left + length > right:
+            for k in range(left, min(left + 10, right) + 1):
+                if word[left:k] in forbidden_set:
+                    right = k - 1
                     break
-                if trie.has_forbidden_substring(word[left:left + length]):
-                    right = left + length - 1
-                    break
-
-            # Update the maximum length of the valid substring
             max_length = max(max_length, right - left)
-
         return max_length
