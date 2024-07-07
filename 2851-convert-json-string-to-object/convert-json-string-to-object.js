295c96d506887fdc7f86fb33dd0fce7665744d40
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-06 20:33:31 -0700

    Time: 208 ms (6.25%) | Memory: 80.7 MB (6.25%) - LeetSync

--- 2851-convert-json-string-to-object/convert-json-string-to-object.js
+++ 2851-convert-json-string-to-object/convert-json-string-to-object.js
@@ -2,6 +2,81 @@
  * @param {string} str
  * @return {null|boolean|number|string|Array|Object}
  */
+
+
+
+/**
+python version
+def parse_json(s: str):
+    i = 0
+
+    def parse_value():
+        nonlocal i
+        if s[i] == '{': return parse_object()
+        if s[i] == '[': return parse_array()
+        if s[i] == '"': return parse_string()
+        if s[i] == 't': return parse_true()
+        if s[i] == 'f': return parse_false()
+        if s[i] == 'n': return parse_null()
+        return parse_number()
+
+    def parse_object():
+        nonlocal i
+        obj = {}
+        i += 1
+        while s[i] != '}':
+            key = parse_string()
+            i += 1  # Skip colon
+            obj[key] = parse_value()
+            if s[i] == ',': i += 1
+        i += 1
+        return obj
+
+    def parse_array():
+        nonlocal i
+        arr = []
+        i += 1
+        while s[i] != ']':
+            arr.append(parse_value())
+            if s[i] == ',': i += 1
+        i += 1
+        return arr
+
+    def parse_string():
+        nonlocal i
+        result = ''
+        i += 1
+        while s[i] != '"':
+            result += s[i]
+            i += 1
+        i += 1
+        return result
+
+    def parse_number():
+        nonlocal i
+        num_str = ''
+        while i < len(s) and (s[i].isdigit() or s[i] in '.-'):
+            num_str += s[i]
+            i += 1
+        return float(num_str) if '.' in num_str else int(num_str)
+
+    def parse_true():
+        nonlocal i
+        i += 4
+        return True
+
+    def parse_false():
+        nonlocal i
+        i += 5
+        return False
+
+    def parse_null():
+        nonlocal i
+        i += 4
+        return None
+
+    return parse_value()
+ */
 var jsonParse = function (str) {
     let i = 0;
 
