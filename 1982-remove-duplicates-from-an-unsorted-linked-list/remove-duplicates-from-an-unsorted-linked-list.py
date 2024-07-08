d3538c85ea77b7bdb5827ad2e11625506762046b
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-08 13:49:19 -0700

    Time: 490 ms (48.16%) | Memory: 46 MB (64.88%) - LeetSync

--- 1982-remove-duplicates-from-an-unsorted-linked-list/remove-duplicates-from-an-unsorted-linked-list.py
+++ 1982-remove-duplicates-from-an-unsorted-linked-list/remove-duplicates-from-an-unsorted-linked-list.py
@@ -3,22 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
 class Solution:
     def deleteDuplicatesUnsorted(self, head: ListNode) -> ListNode:
-        c = {} # val: count
+        frequency = defaultdict(int)
+
         p = head
         while p:
-            c[p.val] = c.get(p.val, 0) + 1
+            frequency[p.val] += 1
             p = p.next
-        
+
         dummy = ListNode(0, head)
-        
         p = dummy
+
         while p:
-           unique = p.next
-           while unique and c[unique.val] > 1:
+            unique = p.next
+            while unique and frequency[unique.val] > 1:
                 unique = unique.next
-           p.next = unique
-           p = p.next
+            p.next = unique
+            p = p.next
 
         return dummy.next
