34f0e10e74168986bd69fc87e23fe76c691a8636
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-08 13:39:59 -0700

    Time: 42 ms (47.62%) | Memory: 16.4 MB (84.15%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -2,9 +2,16 @@ class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         # f(n,k)=(f(n−1,k)+k) % n
 
-        def dfs(n, k):
-            if n == 1:
-                return 0
-            return (dfs(n - 1, k) + k) % n
+        # def dfs(n, k):
+        #     if n == 1:
+        #         return 0
+        #     return (dfs(n - 1, k) + k) % n
 
-        return dfs(n, k) + 1
+        # return dfs(n, k) + 1
+
+        winner = 0
+
+        for i in range(2, n + 1):
+            winner = (winner + k) % i
+
+        return winner + 1
