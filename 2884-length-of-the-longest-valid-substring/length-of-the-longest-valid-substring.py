9970f58c559826aa1cbae8ef6c7f7c5d55465e74
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-10 20:01:17 -0700

    Time: 1264 ms (75.45%) | Memory: 36.1 MB (38.73%) - LeetSync

--- 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.py
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.py
@@ -1,16 +1,14 @@
 class Solution:
     def longestValidSubstring(self, word: str, forbidden: List[str]) -> int:
         forbidden_set = set(forbidden)
-        max_length = 0
+        longest = 0
         n = len(word)
-        right = n - 1  # track the right boundary of the current valid window
+        right = n - 1
 
         for left in reversed(range(n)):
             for k in range(left, min(left + 10, right + 1)):
-                if word[left:k + 1] in forbidden_set:
-                    # If found, the right boundary is updated to k - 1
-                    # shrinks the current valid window from the right side
+                if word[left : k + 1] in forbidden_set:
                     right = k - 1
                     break
-            max_length = max(max_length, right - left + 1)
-        return max_length
+            longest = max(longest, right - left + 1)
+        return longest
