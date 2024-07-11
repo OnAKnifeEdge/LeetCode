ecbee8abbc55a2580bc6e325719864c0ca02a64c
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-11 11:49:06 -0700

    Time: 42 ms (17.84%) | Memory: 16.9 MB (7.98%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,25 +1,17 @@
-from collections import defaultdict
-import bisect
-
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         lookup = defaultdict(list)
         for i, c in enumerate(t):
             lookup[c].append(i)
 
-        current_pos = 0
-        for char in s:
-            if char not in lookup:
+        i = 0
+        for c in s:
+            if c not in lookup:
                 return False
-            idx_list = lookup[char]
-            found = bisect.bisect_left(idx_list, current_pos)
+            idx_list = lookup[c]
+            found = bisect_left(idx_list, i)
             if found == len(idx_list):
                 return False
-            current_pos = idx_list[found] + 1
+            i = idx_list[found] + 1
 
         return True
-
-# Example usage:
-sol = Solution()
-print(sol.isSubsequence("abc", "ahbgdc"))  # Output: True
-print(sol.isSubsequence("axc", "ahbgdc"))  # Output: False
