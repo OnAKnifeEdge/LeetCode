799ca8ec61923230e44ab389b473cb00f9d678dc
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-13 15:37:52 -0700

    Time: 1190 ms (53.64%) | Memory: 40.3 MB (10.74%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
@@ -1,15 +1,24 @@
 class Solution:
-    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        # return (len(potions) - bisect_left(potions, success, key=lambda p: p * s) for s in spells)
-
+    def successfulPairs(
+        self, spells: List[int], potions: List[int], success: int
+    ) -> List[int]:
         potions.sort()
-        answer = []
+        pairs = []
 
-        max_potion = potions[- 1]
+        def binary_search(nums, target):
+            lo, hi = 0, len(nums) - 1
+            while lo <= hi:
+                mid = (lo + hi) // 2
+                if nums[mid] < target:
+                    lo = mid + 1
+                elif nums[mid] >= target:
+                    hi = mid - 1
+            return lo
+
         for spell in spells:
-            min_potion = math.ceil(success/spell)
-            i = bisect_left(potions, min_potion)
-            answer.append(len(potions) - i)
-        return answer
+            min_potion = math.ceil(success / spell)
+            # i = bisect_left(potions, min_potion)
+            i = binary_search(potions, min_potion)
+            pairs.append(len(potions) - i)
 
-        
+        return pairs
