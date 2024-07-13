61abc84395d636321f572e0272485c20bcfa8aed
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-12 18:36:51 -0700

    Time: 39 ms (38.87%) | Memory: 16.5 MB (21.35%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -12,13 +12,12 @@ class Solution:
         rightside = []
 
         def dfs(node, level):
+            if node is None:
+                return
             if level == len(rightside):
                 rightside.append(node.val)
-
-            if node.right:
-                dfs(node.right, level + 1)
-            if node.left:
-                dfs(node.left, level + 1)
+            dfs(node.right, level + 1)
+            dfs(node.left, level + 1)
 
         dfs(root, 0)
         return rightside
