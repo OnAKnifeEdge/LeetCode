0a73d30eb4854639a49eb8cea6a55ee2e72aec8d
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-12 18:36:04 -0700

    Time: 41 ms (23.15%) | Memory: 16.7 MB (21.35%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,88 +6,19 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-
-        # DFS
-        if not root:
+        if root is None:
             return []
 
-        right_side = []
+        rightside = []
 
-        def traverse(node, level):
-            if not node:
-                return
-            if level == len(right_side):
-                right_side.append(node.val)
-            traverse(node.right, level + 1)
-            traverse(node.left, level + 1)
+        def dfs(node, level):
+            if level == len(rightside):
+                rightside.append(node.val)
 
-        traverse(root, 0)
-        return right_side
+            if node.right:
+                dfs(node.right, level + 1)
+            if node.left:
+                dfs(node.left, level + 1)
 
-        # if not root:
-        #     return []
-        # q = deque([root, None])
-        # node = root
-        # rightside = []
-        # while q:
-        #     pre, node = node, q.popleft()
-
-        #     while node:
-        #         if node.left:
-        #             q.append(node.left)
-        #         if node.right:
-        #             q.append(node.right)
-        #         pre, node = node, q.popleft()
-
-        #     rightside.append(pre.val)
-
-        #     if q:
-        #         q.append(None)
-
-        # return rightside
-
-
-
-
-
-
-        # if not root:
-        #     return []
-
-        # next_level = deque([root])
-        # rightside = []
-
-        # while next_level:
-        #     current_level = next_level
-        #     next_level = deque()
-
-        #     while current_level:
-        #         node = current_level.popleft()
-
-        #         if node.left:
-        #             next_level.append(node.left)
-        #         if node.right:
-        #             next_level.append(node.right)
-
-        #     rightside.append(node.val)
-        # return rightside
-
-
-
-        # levels
-        # if not root:
-        #     return []
-        # q = deque([root])
-        # rightside = []
-        # while q:
-        #     levels = len(q)
-        #     for i in range(levels):
-        #         node = q.popleft()
-        #         if i == levels - 1:
-        #             rightside.append(node.val)
-        #         if node.left:
-        #             q.append(node.left)
-        #         if node.right:
-        #             q.append(node.right)
-        # return rightside
-        
+        dfs(root, 0)
+        return rightside
