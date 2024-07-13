9a831f494f65ec2a7860e8f109ecf9ca804c6b28
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-12 18:40:09 -0700

    Time: 27 ms (96.98%) | Memory: 16.5 MB (21.35%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -11,13 +11,15 @@ class Solution:
 
         rightside = []
 
-        def dfs(node, level):
-            if node is None:
-                return
+        q = deque([(root, 0)])
+
+        while q:
+            node, level = q.popleft()
             if level == len(rightside):
                 rightside.append(node.val)
-            dfs(node.right, level + 1)
-            dfs(node.left, level + 1)
+            if node.right:
+                q.append((node.right, level + 1))
+            if node.left:
+                q.append((node.left, level + 1))
 
-        dfs(root, 0)
         return rightside
