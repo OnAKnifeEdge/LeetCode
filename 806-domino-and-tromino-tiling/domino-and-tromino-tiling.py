0a20c975079a8959bb34d429ffa89c6550306b11
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-14 15:16:02 -0700

    Time: 36 ms (67.85%) | Memory: 16.5 MB (93.62%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -1,32 +1,19 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        # f(k)=f(k−1)+f(k−2)+2∗p(k−1)
-        # p(k)=p(k−1)+f(k−2)
         MOD = 10**9 + 7
 
         if n <= 2:
             return n
 
-        # p = [0] * (n + 1)
-        # f = [0] * (n + 1)
+        # Initialize with the first three values
+        a, b, c = 1, 2, 5
 
+        if n == 3:
+            return c
 
-        # f[1] = 1
-        # f[2] = 2
-        # p[2] = 1
+        for _ in range(4, n + 1):
 
-        # for k in range(3, n + 1):
-        #     f[k] = (f[k - 1] + f[k - 2] + 2 * p[k - 1]) % MOD
-        #     p[k] = (p[k - 1] + f[k - 2]) % MOD
+            # Shift the values
+            a, b, c = b, c, (2 * c + a) % MOD
 
-        # return f[n]
-
-        f_two_back = 1
-        f = 2
-        p = 1
-        for k in range(3, n + 1):
-            temp = f
-            f = (f + f_two_back + 2 * p) % MOD
-            p = (p + f_two_back) % MOD
-            f_two_back = temp
-        return f
+        return c
