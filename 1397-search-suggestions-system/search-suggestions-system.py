9d44a7d062f6e4ea6451c895ed574684646b0d37
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-13 23:50:07 -0700

    Time: 680 ms (7.91%) | Memory: 25.8 MB (5.11%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.py
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -1,46 +1,57 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
-        self.candidates = []
-
-    # def __str__(self):
-    #     return repr(self.children), repr(self.candidates)
+        self.children = [None] * 26
+        self.is_end = False
 
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
+        self.suggestions = []
 
-    def add_word(self, word):
+    def insert(self, word):
         node = self.root
         for c in word:
-            if c not in node.children:
-                node.children[c] = TrieNode()
-            node = node.children[c]
-            node.candidates.append(word)
+            idx = ord(c) - ord("a")
+            if not node.children[idx]:
+                node.children[idx] = TrieNode()
+            node = node.children[idx]
+        node.is_end = True
 
-    def add_products(self, products):
-        for product in products:
-            self.add_word(product)
+    def dfs_with_prefix(self, node, word):
+        if len(self.suggestions) == 3:
+            return
+        if node.is_end:
+            self.suggestions.append(word)
+        for i in range(26):
+            if node.children[i]:
+                self.dfs_with_prefix(node.children[i], word + chr(i + ord("a")))
 
-    def search(self, word):
+    def get_words_starting_with(self, prefix):
         node = self.root
-        for c in word:
-            if c in node.children:
-                node = node.children[c]
-            else:
+        for c in prefix:
+            idx = ord(c) - ord("a")
+            if not node.children[idx]:
                 return []
-        return node.candidates
+            node = node.children[idx]
+        self.suggestions = []
+        self.dfs_with_prefix(node, prefix)
+        return self.suggestions
 
 
 class Solution:
-    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        products.sort()
+    def suggestedProducts(
+        self, products: List[str], searchWord: str
+    ) -> List[List[str]]:
         trie = Trie()
-        trie.add_products(products)
-        suggestions = []
-        for i in range(1, len(searchWord) + 1):
-            word = searchWord[:i]
-            suggestions.append(sorted(trie.search(word))[:3])
-        return suggestions
-        
+        for product in products:
+            trie.insert(product)
+
+        result = []
+        prefix = ""
+
+        for c in searchWord:
+            prefix += c
+            result.append(trie.get_words_starting_with(prefix))
+
+        return result
