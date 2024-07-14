932959740d19d541f8906dfa7bf00e9d7e5383fa
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-14 14:55:28 -0700

    Time: 39 ms (50.79%) | Memory: 16.7 MB (43.60%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -7,16 +7,26 @@ class Solution:
         if n <= 2:
             return n
 
-        p = [0] * (n + 1)
-        f = [0] * (n + 1)
+        # p = [0] * (n + 1)
+        # f = [0] * (n + 1)
 
 
-        f[1] = 1
-        f[2] = 2
-        p[2] = 1
+        # f[1] = 1
+        # f[2] = 2
+        # p[2] = 1
 
+        # for k in range(3, n + 1):
+        #     f[k] = (f[k - 1] + f[k - 2] + 2 * p[k - 1]) % MOD
+        #     p[k] = (p[k - 1] + f[k - 2]) % MOD
+
+        # return f[n]
+
+        f_two_back = 1
+        f = 2
+        p = 1
         for k in range(3, n + 1):
-            f[k] = (f[k - 1] + f[k - 2] + 2 * p[k - 1]) % MOD
-            p[k] = (p[k - 1] + f[k - 2]) % MOD
-
-        return f[n]
+            temp = f
+            f = (f + f_two_back + 2 * p) % MOD
+            p = (p + f_two_back) % MOD
+            f_two_back = temp
+        return f
