685a9c349a51c20cab24e7236a3daccbbbad5ec1
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-14 21:42:16 -0700

    Time: 63 ms (50.43%) | Memory: 20.9 MB (45.82%) - LeetSync

--- 1883-find-distance-in-a-binary-tree/find-distance-in-a-binary-tree.py
+++ 1883-find-distance-in-a-binary-tree/find-distance-in-a-binary-tree.py
@@ -8,27 +8,27 @@ class Solution:
     def findDistance(self, root: TreeNode, p: int, q: int) -> int:
         if p == q:
             return 0
-        self.result = -1
+        result = -1
 
-        def dfs(node: TreeNode, p: int, q: int) -> int:
-            nonlocal self
+        def dfs(node: TreeNode) -> int:
+            nonlocal result
             if not node:
                 return -1
 
-            left = dfs(node.left, p, q)
-            right = dfs(node.right, p, q)
+            left = dfs(node.left)
+            right = dfs(node.right)
 
             if node.val == p or node.val == q:
                 # Found either p or q, but neither is a descendant of the current node.
                 if left < 0 and right < 0:
                     return 0
                 # Found either p or q, and the current node is also the LCA.
-                self.result = 1 + (left if left >= 0 else right)
+                result = 1 + (left if left >= 0 else right)
                 return -1  # Stop further search
 
             # Current node is neither p or q, but it is the LCA.
             if left >= 0 and right >= 0:
-                self.result = left + right + 2
+                result = left + right + 2
                 return -1  # Stop further search
 
             # Continue the search if either p or q is found in the subtrees.
@@ -38,7 +38,7 @@ class Solution:
                 return right + 1
 
             # Neither p or q is found in the subtrees, continue the search.
-            return -1 
+            return -1
 
-        dfs(root, p, q)
-        return self.result
+        dfs(root)
+        return result
