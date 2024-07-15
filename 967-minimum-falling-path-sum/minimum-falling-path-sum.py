756e11330b0364c1c3ddeefcd664ab4a2c7fdd19
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-15 16:00:32 -0700

    Time: 123 ms (74.09%) | Memory: 17.3 MB (64.63%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -3,14 +3,29 @@ class Solution:
         if not matrix:
             return 0
         ROWS, COLS = len(matrix), len(matrix[0])
-        dp = [[float("inf")] * COLS for _ in range(ROWS)]
-        dp[-1] = matrix[-1]
+        # dp = [[float("inf")] * COLS for _ in range(ROWS)]
+        # dp[-1] = matrix[-1][:]
+        # for i in reversed(range(ROWS - 1)):
+        #     for j in range(COLS):
+        #         best = dp[i + 1][j]
+        #         if j > 0:
+        #             best = min(best, dp[i + 1][j - 1])
+        #         if j + 1 < COLS:
+        #             best = min(best, dp[i + 1][j + 1])
+        #         dp[i][j] = best + matrix[i][j]
+        # return min(dp[0])
+
+        dp = matrix[-1][:]
+
         for i in reversed(range(ROWS - 1)):
+            new_dp = dp[:]
             for j in range(COLS):
-                group = [dp[i + 1][j]]
+                best = dp[j]
                 if j > 0:
-                    group.append(dp[i + 1][j - 1])
+                    best = min(best, dp[j - 1])
                 if j + 1 < COLS:
-                    group.append(dp[i + 1][j + 1])
-                dp[i][j] = min(group) + matrix[i][j]
-        return min(dp[0])
+                    best = min(best, dp[j + 1])
+                new_dp[j] = matrix[i][j] + best
+            dp = new_dp
+
+        return min(dp)
