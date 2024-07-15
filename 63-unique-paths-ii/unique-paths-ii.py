453bddac8b60c5e658111d21850015d1295960e4
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-15 15:06:21 -0700

    Time: 43 ms (64.53%) | Memory: 16.5 MB (71.07%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -5,20 +5,31 @@ class Solution:
         if obstacleGrid[0][0]:
             return 0
         ROWS, COLS = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[0] * COLS for _ in range(ROWS)]
-        dp[0][0] = 1
+        # dp = [[0] * COLS for _ in range(ROWS)]
+        # dp[0][0] = 1
 
-        for j in range(1, COLS):
-            if obstacleGrid[0][j] == 0:
-                dp[0][j] = dp[0][j - 1]
+        # for j in range(1, COLS):
+        #     if obstacleGrid[0][j] == 0:
+        #         dp[0][j] = dp[0][j - 1]
 
-        for i in range(1, ROWS):
-            if obstacleGrid[i][0] == 0:
-                dp[i][0] = dp[i - 1][0]
+        # for i in range(1, ROWS):
+        #     if obstacleGrid[i][0] == 0:
+        #         dp[i][0] = dp[i - 1][0]
 
-        for i in range(1, ROWS):
-            for j in range(1, COLS):
-                if obstacleGrid[i][j] == 0:
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        # for i in range(1, ROWS):
+        #     for j in range(1, COLS):
+        #         if obstacleGrid[i][j] == 0:
+        #             dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
 
-        return dp[-1][-1]
+        # return dp[-1][-1]
+        dp = [0] * COLS
+        dp[0] = 1
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if obstacleGrid[i][j] == 1:
+                    dp[j] = 0
+                elif j > 0:
+                    dp[j] += dp[j - 1]
+
+        return dp[-1]
