ca9cc2809c31dac89b2c03866856082729f32a37
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-15 14:24:02 -0700

    Time: 65 ms (94.50%) | Memory: 18.6 MB (73.99%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.py
+++ 740-delete-and-earn/delete-and-earn.py
@@ -1,57 +1,23 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
         points = defaultdict(int)
-        max_number = 0
         for num in nums:
             points[num] += num
-            max_number = max(max_number, num)
-        
-        # top down recurrence
-        # @cache
-        # def dp(num): 
-        #     # dp(i) is the max points when reach to num i
-        #     if num == 0:
-        #         return 0
-        #     if num == 1:
-        #         return points[1]
-            
-        #     return max(dp(num - 1), dp(num - 2) + points[num])
-        
-        # return dp(max_number)
-        # bottom up case
+        numbers = sorted(points.keys())
 
-        # dp = [0] * (max_number + 1)
-        # dp[1] = points[1]
+        # dp[i] = max(dp[i-1], dp[i-2] + points[nums[i]])
 
-        # for i in range(2, max_number + 1):
-        #     dp[i] = max(dp[i - 1], dp[i - 2] + points[i])
-        # return dp[max_number]
+        # dp[0] = 0
+        # dp[1] = points[numbers[0]]
 
-        # space optimazation
+        a, b = 0, points[numbers[0]]
 
-        # two = 0
-        # one = points[1]
-        # for i in range(2, max_number + 1):
-        #     two, one = one, max(one, two + points[i])
-        # return one
-
-
-        # optimization with sorting
-        elements = sorted(points.keys())
-        if not elements:
-            return 0
-        two = 0
-        one = points[elements[0]]
-        # loop elements
-        for i in range(1, len(elements)):
-            current = elements[i]
-            if current == elements[i - 1] + 1:
-                two, one = one, max(one, two + points[current])
+        for i in range(1, len(numbers)):
+            if numbers[i] == numbers[i - 1] + 1:
+                a, b = b, max(b, a + points[numbers[i]])
             else:
-                two, one = one, one + points[current]
-        return one
+                a, b = b, b + points[numbers[i]]
 
-
-
-
-        
+        return b
