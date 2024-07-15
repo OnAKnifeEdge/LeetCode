915cba5b918598d0cea0436bc7d7844c2c9419b8
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-15 14:50:36 -0700

    Time: 38 ms (39.85%) | Memory: 16.6 MB (27.63%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,14 +1,23 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0] * n for _ in range(m)]
+        # dp = [[0] * n for _ in range(m)]
 
-        for j in range(n):
-            dp[0][j] = 1
-        for i in range(m):
-            dp[i][0] = 1
+        # for j in range(n):
+        #     dp[0][j] = 1
+        # for i in range(m):
+        #     dp[i][0] = 1
 
+        # for i in range(1, m):
+        #     for j in range(1, n):
+        #         dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        # return dp[-1][-1]
+        if m < n:
+            m, n = n, m
+        # n is the smaller open
+        dp = [1] * n
         for i in range(1, m):
             for j in range(1, n):
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
-
-        return dp[-1][-1]
+                dp[j] += dp[j - 1]
+                # cell left: dp[j - 1], prev cell: dp[j]
+        return dp[-1]
