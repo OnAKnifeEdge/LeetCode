83a5f7ea565480c10ea6f28ffb4789ff6be84305
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-15 15:54:10 -0700

    Time: 124 ms (70.78%) | Memory: 17.5 MB (45.24%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,83 +1,16 @@
 class Solution:
-
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        ROWS = len(matrix)
-        COLS = len(matrix[0])
-        dp = [float("inf") for _ in range(COLS)]
-
-        dp = matrix[0]
-
-        for i in range(1, ROWS):
-            prev_dp = dp[:]
+        if not matrix:
+            return 0
+        ROWS, COLS = len(matrix), len(matrix[0])
+        dp = [[float("inf")] * COLS for _ in range(ROWS)]
+        dp[-1] = matrix[-1]
+        for i in reversed(range(ROWS - 1)):
             for j in range(COLS):
-                if j == 0:
-                    dp[j] = min(prev_dp[j], prev_dp[j + 1]) + matrix[i][j]
-                elif j == COLS - 1:
-                    dp[j] = min(prev_dp[j], prev_dp[j - 1]) + matrix[i][j]
-                else:
-                    dp[j] = min(prev_dp[j], prev_dp[j - 1], prev_dp[j + 1]) + matrix[i][j]
-
-        return min(dp)
-
-    def minFallingPathSum_bottomup(self, matrix: List[List[int]]) -> int:
-        ROWS = len(matrix)
-        COLS = len(matrix[0])
-        dp = [[float("inf") for _ in range(COLS)] for _ in range(ROWS)]
-
-        dp[0] = matrix[0]
-
-        for i in range(1, ROWS):
-            for j in range(COLS):
-                if j == 0:
-                    dp[i][j] = min(dp[i - 1][j], dp[i - 1][j + 1]) + matrix[i][j]
-                elif j == COLS - 1:
-                    dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1]) + matrix[i][j]
-                else:
-                    dp[i][j] = (
-                        min(dp[i - 1][j], dp[i - 1][j - 1], dp[i - 1][j + 1])
-                        + matrix[i][j]
-                    )
-
-        return min(dp[ROWS - 1])
-
-    def minFallingPathSum_bottomup(self, matrix: List[List[int]]) -> int:
-        ROWS = len(matrix)
-        COLS = len(matrix[0])
-        dp = [[float("inf") for _ in range(COLS)] for _ in range(ROWS)]
-
-        dp[0] = matrix[0]
-
-        for i in range(1, ROWS):
-            for j in range(COLS):
-                if j == 0:
-                    dp[i][j] = min(dp[i - 1][j], dp[i - 1][j + 1]) + matrix[i][j]
-                elif j == COLS - 1:
-                    dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1]) + matrix[i][j]
-                else:
-                    dp[i][j] = (
-                        min(dp[i - 1][j], dp[i - 1][j - 1], dp[i - 1][j + 1])
-                        + matrix[i][j]
-                    )
-
-        return min(dp[ROWS - 1])
-
-    def minFallingPathSum_topdown(self, matrix: List[List[int]]) -> int:
-        # 对于 matrix[i][j]
-        # 只有可能从 matrix[i-1][j], matrix[i-1][j-1], matrix[i-1][j+1] 这三个位置掉下来
-
-        @cache
-        def dp(i, j):
-            if j < 0 or j == len(matrix):
-                return float("inf")
-
-            if i == 0:
-                return matrix[i][j]
-
-            return min(dp(i - 1, j), dp(i - 1, j - 1), dp(i - 1, j + 1)) + matrix[i][j]
-
-        result = float("inf")
-        for j in range(len(matrix)):
-            s = dp(len(matrix) - 1, j)
-            result = min(result, s)
-
-        return result
+                group = [dp[i + 1][j]]
+                if j > 0:
+                    group.append(dp[i + 1][j - 1])
+                if j + 1 < COLS:
+                    group.append(dp[i + 1][j + 1])
+                dp[i][j] = min(group) + matrix[i][j]
+        return min(dp[0])
