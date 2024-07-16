5f44074c12a210d4827de39cc4419a6cbba35b56
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-15 20:26:58 -0700

    Time: 81 ms (97.09%) | Memory: 16.5 MB (63.72%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -3,22 +3,22 @@ class Solution:
         t = "#" + "#".join(s) + "#"
         n = len(t)
         center, right = 0, 0
-        dp = [0] * n  # dp[i] means radius of palindrome centered at i
+        p = [0] * n  # p[i] means radius of palindrome centered at i
         for i in range(n):
             mirror = 2 * center - i
             if i < right:
-                dp[i] = min(right - i, dp[mirror])
+                p[i] = min(right - i, p[mirror])
 
             # Attempt to expand palindrome centered at i
-            start, end = i - (1 + dp[i]), i + (1 + dp[i])
+            start, end = i - (1 + p[i]), i + (1 + p[i])
             while end < n and start >= 0 and t[start] == t[end]:
-                dp[i] += 1
+                p[i] += 1
                 start -= 1
                 end += 1
 
-            if i + dp[i] > right:
-                center, right = i, i + dp[i]
+            if i + p[i] > right:
+                center, right = i, i + p[i]
 
-        longest, center_idx = max((n, i) for i, n in enumerate(dp))
+        longest, center_idx = max((n, i) for i, n in enumerate(p))
         start = (center_idx - longest) // 2
         return s[start:start+longest]
