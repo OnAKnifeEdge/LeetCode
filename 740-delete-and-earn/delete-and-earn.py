1183265d3c0540209320e42830bdcfc88aff5ca4
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-17 12:56:19 -0700

    Time: 63 ms (96.82%) | Memory: 18.5 MB (74.32%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.py
+++ 740-delete-and-earn/delete-and-earn.py
@@ -5,13 +5,13 @@ class Solution:
         points = defaultdict(int)
         for num in nums:
             points[num] += num
-        numbers = sorted(points.keys())
 
         # dp[i] = max(dp[i-1], dp[i-2] + points[nums[i]])
 
         # dp[0] = 0
         # dp[1] = points[numbers[0]]
 
+        numbers = sorted(points.keys())
         a, b = 0, points[numbers[0]]
 
         for i in range(1, len(numbers)):
