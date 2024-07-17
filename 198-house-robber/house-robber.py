2f115df36130776188c8ea8941b2fed5cd5300a9
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-17 12:56:24 -0700

    Time: 43 ms (11.91%) | Memory: 16.5 MB (62.97%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,6 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
         a, b = 0, 0
         for i in range(len(nums)):
-            a, b = b, max(b, a + nums[i])
+            a, b = b, max(a + nums[i], b)
         return b
