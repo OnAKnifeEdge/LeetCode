2d4d22dc1a3c7914a1be2dcd5fd8c1f46faba318
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-07-17 12:54:40 -0700

    Time: 75 ms (93.42%) | Memory: 18.3 MB (65.66%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,48 +1,17 @@
 class Solution:
-
     def minPathSum(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return 0
+        # dp[i][j] = min(dp[i-1][j], dp[i][j - 1]) + grid[i][j]
         ROWS, COLS = len(grid), len(grid[0])
-        dp = [float("inf")] * COLS
-        # base case 相当于dp[0][j]=sum(grid[0][0...j-1])
-        dp[0] = grid[0][0]
-        for j in range(1, COLS):
-            dp[j] = dp[j - 1] + grid[0][j]
-
-        for i in range(1, ROWS):
-            # base case  相当于dp[i][0]=sum(grid[0...i-1][0])
-            dp[0] += grid[i][0]
-            for j in range(1, COLS):
-                dp[j] = min(dp[j], dp[j - 1]) + grid[i][j]
-        return dp[-1]
-
-    def minPathSum_hard(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
-
-        dp = [float("inf")] * COLS
-        dp[0] = 0
-
-        for i in range(ROWS):
-            for j in range(COLS):
-                if j == 0:
-                    dp[j] += grid[i][j]
-                else:
-                    dp[j] = min(dp[j], dp[j - 1]) + grid[i][j]
-        return dp[-1]
-
-    def minPathSum_dp(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
-
-        dp = [[float("inf")] * (COLS) for _ in range(ROWS)]
+        dp = [[float("inf")] * COLS for _ in range(ROWS)]
         dp[0][0] = grid[0][0]
-
-        for i in range(1, ROWS):
-            dp[i][0] = dp[i - 1][0] + grid[i][0]
-
         for j in range(1, COLS):
             dp[0][j] = dp[0][j - 1] + grid[0][j]
+        for i in range(1, ROWS):
+            dp[i][0] = dp[i - 1][0] + grid[i][0]
 
         for i in range(1, ROWS):
             for j in range(1, COLS):
                 dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
-
         return dp[-1][-1]
