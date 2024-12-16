5c1d3c41f4b35bcc93692ec7769b76fbffc063cd
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-12-15 22:59:38 -0800

    Time: 0 ms (100.00%) | Memory: 17.3 MB (8.02%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -1,12 +1,15 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        stk = []
+        closings_needed = 0
+        openings_needed = 0
         for c in s:
-            if c == "(":
-                stk.append(c)
-            elif c == ")":
-                if stk and stk[-1] == "(":
-                    stk.pop()
-                else:
-                    stk.append(c)
-        return len(stk)
+            if c == '(':
+                closings_needed += 1
+            elif c == ')':
+                closings_needed -=1
+                if closings_needed == -1:
+                    closings_needed = 0
+                    openings_needed +=1
+        return closings_needed + openings_needed
+
+        
