614a83287d658748ac0daf409f12eec005aa7191
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2024-12-15 22:33:40 -0800

    Time: 0 ms (100.00%) | Memory: 17.4 MB (8.02%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -1,13 +1,12 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        stack = []
+        stk = []
         for c in s:
-            # 如果是左括号，就入栈。当右括号时，同时栈顶为左括号时，则将左括号排出，最后留在栈内的括号数量就是需要加入的括号数量
             if c == "(":
-                stack.append(c)
-            else:
-                if stack and stack[-1] == "(":
-                    stack.pop()
+                stk.append(c)
+            elif c == ")":
+                if stk and stk[-1] == "(":
+                    stk.pop()
                 else:
-                    stack.append(c)
-        return len(stack)
+                    stk.append(c)
+        return len(stk)
