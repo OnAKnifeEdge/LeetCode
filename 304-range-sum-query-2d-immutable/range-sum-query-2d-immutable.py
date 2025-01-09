abaabb30a5bc449f9971118b66cbc7f5ffe57130
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-08 20:56:16 -0800

    Time: 119 ms (54.62%) | Memory: 31.2 MB (17.89%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,54 +1,15 @@
 class NumMatrix:
 
-    # preSum[i][j] 记录 matrix 中子矩阵 [0, 0, i-1, j-1] 的元素和
     def __init__(self, matrix: List[List[int]]):
-        R, C = len(matrix), len(matrix[0])
-
-        self.prefix_sum = [[0] * (C + 1) for _ in range(R + 1)]
-
-        for i in range(R):
-            for j in range(C):
-                self.prefix_sum[i + 1][j + 1] = self.prefix_sum[i][j + 1] + self.prefix_sum[i + 1][j] + matrix[i][j] - self.prefix_sum[i][j]
+        ROWS, COLS = len(matrix), len(matrix[0])
+        self.prefix_sum = [[0] * (COLS + 1) for _ in range(ROWS + 1)]
+        for i in range(1, len(self.prefix_sum)):
+            for j in range(1, len(self.prefix_sum[0])):
+                self.prefix_sum[i][j] = self.prefix_sum[i - 1][j] + self.prefix_sum[i][j - 1] + matrix[i - 1][j - 1] - self.prefix_sum[i - 1][j - 1]
+        
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         return self.prefix_sum[row2 + 1][col2 + 1] - self.prefix_sum[row1][col2 + 1] - self.prefix_sum[row2 + 1][col1] + self.prefix_sum[row1][col1]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
         
 
 
