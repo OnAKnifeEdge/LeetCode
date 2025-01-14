5f7b745d5fc2bd9d88d3c34465c340772ec42cb1
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-13 22:48:17 -0800

    Time: 43 ms (60.08%) | Memory: 28.9 MB (62.98%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -2,16 +2,14 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         # https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/editorial/
         n = len(nums)
-
-        for i in range(n):
-            if nums[i] <= 0 or nums[i] > n:
+        for i, num in enumerate(nums):
+            if num <= 0 or num > n:
                 nums[i] = n + 1
         for num in nums:
             idx = abs(num) - 1
             if 0 <= idx < n and nums[idx] > 0:
-                nums[idx] *= -1
-
-        for idx, num in enumerate(nums):
+                nums[idx] = -nums[idx]
+        for i, num in enumerate(nums):
             if num > 0:
-                return idx + 1
+                return i + 1
         return n + 1
