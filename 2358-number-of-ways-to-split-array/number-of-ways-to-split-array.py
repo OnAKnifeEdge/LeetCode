5e0c5bde90e2ddd4084763362e36ade67d0eb548
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-15 06:26:45 -0800

    Time: 73 ms (32.52%) | Memory: 33.2 MB (31.30%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -1,8 +1,20 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        half = sum(nums) / 2
-        curr, ways = 0, 0
-        for n in nums[:-1]:
-            curr += n
-            ways += curr >= half
-        return ways
+        n = len(nums)
+        prefix_sum = [0] * (n)
+        prefix_sum[0] = nums[0]
+        count = 0
+        for i in range(1, n):
+            prefix_sum[i] = prefix_sum[i - 1] + nums[i]
+        total = prefix_sum[-1]
+        for i in range(n - 1):
+            if prefix_sum[i] >= total - prefix_sum[i]:
+                count += 1
+        return count
+
+        # half = sum(nums) / 2
+        # curr, ways = 0, 0
+        # for n in nums[:-1]:
+        #     curr += n
+        #     ways += curr >= half
+        # return ways
