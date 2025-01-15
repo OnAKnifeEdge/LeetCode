41ea2c4e7695529230d40d3ab2753c87e2941c7a
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-15 04:28:14 -0800

    Time: 180 ms (5.01%) | Memory: 41 MB (5.36%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -3,19 +3,19 @@ class Solution:
         # https://leetcode.com/problems/longest-well-performing-interval/submissions/1224328300/
         # https://leetcode.com/problems/contiguous-array/description/
         # 325
-        d = {}
-        prefix_sum = 0
+        n = len(nums)
+        prefix_sum = [0] * (n + 1)
+        d = {}  # prefix_sum[i] % k: i
+        for i in range(1, n + 1):
+            prefix_sum[i] = prefix_sum[i - 1] + nums[i - 1]
 
-        for i, num in enumerate(nums):
-            prefix_sum += num
-            mod_k = prefix_sum % k
-            if mod_k == 0 and i > 0:
-               return True
+        for i in range(n + 1):
+            val = prefix_sum[i] % k
+            if val not in d:
+                d[val] = i
 
-            if mod_k not in d:
-                d[mod_k] = i
-
-            if mod_k in d and i - d[mod_k] > 1:
+        for i in range(1, n + 1):
+            target = prefix_sum[i] % k
+            if target in d and i - d[target] >= 2:
                 return True
-
         return False
