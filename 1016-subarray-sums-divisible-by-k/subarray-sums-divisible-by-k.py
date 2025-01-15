51c0e463238687f9d3f9bf02a9ea23a3f5a34695
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-15 05:39:13 -0800

    Time: 39 ms (20.28%) | Memory: 21.3 MB (6.46%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -1,22 +1,16 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        d = {}
+        count = 0
         prefix_sum = 0
-        cnt = 0
+        d = {}
         for i, num in enumerate(nums):
             prefix_sum += num
-            # Calculate the mod, adjusting immediately for negative values
-            mod = (prefix_sum % k + k) % k # handle the negative cases
-
+            mod = (prefix_sum % k + k) % k
             if mod == 0:
-                cnt += 1
-
-            # If we've seen this mod before, it contributes to additional subarrays
+                count += 1
             if mod in d:
-                cnt += d[mod]
-
-            d[mod] = d.get(mod, 0) + 1
-
-        return cnt
-
-        
+                count += d[mod]
+                d[mod] += 1
+            else:
+                d[mod] = 1
+        return count
