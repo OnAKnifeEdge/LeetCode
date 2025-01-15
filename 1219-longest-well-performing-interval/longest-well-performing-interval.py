2da47b0a8b46df4f92ce9398d4eb28cffe32f2aa
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-15 06:09:05 -0800

    Time: 27 ms (46.79%) | Memory: 18.2 MB (48.30%) - LeetSync

--- 1219-longest-well-performing-interval/longest-well-performing-interval.py
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.py
@@ -7,15 +7,13 @@ class Solution:
         for i, hour in enumerate(hours):
             diff += 1 if hour > 8 else -1
 
-            if diff > 0: # well-performing
+            if diff > 0:  # well-performing
                 result = max(result, i + 1)
                 continue
-
+            # diff < 0 means 休息日更多
             if diff not in d:
-                d[diff] = i
+                d[diff] = i  # 只存最早的
             if diff - 1 in d:
                 result = max(result, i - d[diff - 1])
 
         return result
-
-        
