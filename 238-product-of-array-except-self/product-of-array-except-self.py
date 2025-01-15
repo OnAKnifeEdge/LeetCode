370ec49f16c3f648242d1d6bf0d84175013e937c
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-14 23:42:57 -0800

    Time: 32 ms (31.54%) | Memory: 26.5 MB (19.79%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,29 +1,12 @@
 class Solution:
-    # def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # left = [1] * len(nums)
-        # for i in range(1, len(nums)):
-        #     left[i] = nums[i - 1] * left[i - 1]
-        #     # 1， 1， 2， 6
-        # right = [1] * len(nums)
-        # for j in reversed(range(len(nums) - 1)):
-        #     right[j] = nums[j + 1] * right[j + 1]
-        #     # 24，12，4，1
-        # return [a * b for a, b in zip(left, right)]
-
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        answer = [1] * n
-
-        # [1, 2, 3, 4]
-
+        left = [1] * n
+        right = [1] * n
+        product = [1] * n
         for i in range(1, n):
-            answer[i] = nums[i - 1] * answer[i - 1]
-            # [1, 1, 2, 6]
+            left[i] = left[i - 1] * nums[i - 1]
+        for i in reversed(range(n - 1)):
+            right[i] = right[i + 1] * nums[i + 1]
+        return [a * b for a, b in zip(left, right)]
         
-        suffix = 1
-        for i in reversed(range(n)):
-            answer[i] *= suffix
-            suffix *= nums[i]
-
-        
-        return answer
