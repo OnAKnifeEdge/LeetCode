8ae2d01ef80a143692e99010f802cc90df5451bb
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-15 02:49:04 -0800

    Time: 115 ms (28.77%) | Memory: 23.9 MB (8.10%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,19 +1,16 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        prefix_sum = [0] * (n + 1)
         d = {}
         max_length = 0
-        cumulative_diff = 0
+        for i in range(n):
+            prefix_sum[i + 1] = prefix_sum[i] + (-1 if nums[i] == 0 else 1)
 
-        for i, num in enumerate(nums):
-            if num == 1:
-                cumulative_diff += 1
-            elif num == 0:
-                cumulative_diff -= 1
-            if cumulative_diff == 0:  # find a target
-                max_length = max(max_length, i + 1)
+        for i in range(n + 1):
+            s = prefix_sum[i]
+            if s in d:
+                max_length = max(max_length, i - d[s])
             else:
-                if cumulative_diff in d:
-                    max_length = max(max_length, i - d[cumulative_diff])
-                else:
-                    d[cumulative_diff] = i
+                d[s] = i
         return max_length
