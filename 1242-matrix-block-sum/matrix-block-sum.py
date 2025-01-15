0197bfc2b776c7e685129c93f4ea460a6b55ed74
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-14 22:34:17 -0800

    Time: 19 ms (49.11%) | Memory: 19.2 MB (31.30%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.py
+++ 1242-matrix-block-sum/matrix-block-sum.py
@@ -1,41 +1,25 @@
 class Solution:
-    # https://leetcode.com/submissions/detail/1223715446/
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
-        R, C = len(mat), len(mat[0])
-        num_matrix = NumMatrix(mat)
-        result = [[0] * C for _ in range(R)]
+        ROWS, COLS = len(mat), len(mat[0])
+        block_sum = [[0] * COLS for _ in range(ROWS)]
+        matrix = Matrix(mat)
+        for i in range(ROWS):
+            for j in range(COLS):
+                x1 = max(0, i - k)
+                x2 = min(ROWS - 1, i + k)
+                y1 = max(0, j - k)
+                y2 = min(COLS - 1, j + k)
+                block_sum[i][j] = matrix.range_sum(x1, y1, x2, y2)
+        return block_sum
 
-        for i in range(R):
-            for j in range(C):
-                x1, y1 = max(i - k, 0), max(j - k, 0)
-                x2, y2 = min(i + k, R - 1), min(j + k, C - 1)
 
-                result[i][j] = num_matrix.sumRegion(x1, y1, x2, y2)
-        return result
+class Matrix:
+    def __init__(self, matrix):
+        ROWS, COLS = len(matrix), len(matrix[0])
+        self.prefix_sum = [[0] * (COLS + 1) for _ in range(ROWS + 1)]
+        for i in range(1, ROWS + 1):
+            for j in range(1, COLS + 1):
+                self.prefix_sum[i][j] = self.prefix_sum[i - 1][j] + self.prefix_sum[i][j - 1] - self.prefix_sum[i - 1][j - 1] + matrix[i - 1][j - 1]
 
-
-class NumMatrix:
-    # every cell at (i+1, j+1) stores the sum of the submatrix from (0,0) to (i,j).
-    def __init__(self, matrix: List[List[int]]):
-        R, C = len(matrix), len(matrix[0])
-
-        self.prefix_sum = [[0] * (C + 1) for _ in range(R + 1)]
-
-        for i in range(R):
-            for j in range(C):
-                val = (
-                    self.prefix_sum[i][j + 1]
-                    + self.prefix_sum[i + 1][j]
-                    + matrix[i][j]
-                    - self.prefix_sum[i][j]
-                )
-
-                self.prefix_sum[i + 1][j + 1] = val
-
-    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return (
-            self.prefix_sum[row2 + 1][col2 + 1]
-            - self.prefix_sum[row1][col2 + 1]
-            - self.prefix_sum[row2 + 1][col1]
-            + self.prefix_sum[row1][col1]
-        )
+    def range_sum(self, x1, y1, x2, y2):
+        return self.prefix_sum[x2 + 1][y2 + 1] - self.prefix_sum[x2 + 1][y1] - self.prefix_sum[x1][y2 + 1] + self.prefix_sum[x1][y1]
