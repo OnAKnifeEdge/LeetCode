345429cac79aea35cdc910885d7266d4d62b8333
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-15 04:51:34 -0800

    Time: 139 ms (77.80%) | Memory: 63 MB (42.01%) - LeetSync

--- 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
+++ 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
@@ -2,16 +2,14 @@ class Solution:
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
         # https://leetcode.com/problems/longest-well-performing-interval/submissions/1224328300/
         # https://leetcode.com/problems/contiguous-array/description/
-        d = {}
-        result = 0
+        d = {0: -1}
         prefix_sum = 0
-
+        max_length = 0
         for i, num in enumerate(nums):
             prefix_sum += num
-            if prefix_sum == k:
-                result = max(result, i + 1)
+            target = prefix_sum - k
+            if target in d:
+                max_length = max(max_length, i - d[target])
             if prefix_sum not in d:
                 d[prefix_sum] = i
-            if prefix_sum - k in d:
-                result = max(result, i - d[prefix_sum-k])
-        return result
+        return max_length
