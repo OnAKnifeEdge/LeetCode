35ef5eef2b04f62cc23dcc9dce932100fcbaeac0
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-15 05:45:36 -0800

    Time: 30 ms (58.76%) | Memory: 21.3 MB (16.13%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -1,16 +1,13 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         count = 0
-        prefix_sum = 0
-        d = {}
-        for i, num in enumerate(nums):
-            prefix_sum += num
-            mod = (prefix_sum % k + k) % k
-            if mod == 0:
-                count += 1
-            if mod in d:
-                count += d[mod]
-                d[mod] += 1
-            else:
-                d[mod] = 1
+        prefix_mod = 0
+        d = defaultdict(int)
+        d[0] = 1
+        for num in nums:
+            prefix_mod = (prefix_mod + num) % k
+            if prefix_mod < 0:
+                prefix_mod += k
+            count += d[prefix_mod]
+            d[prefix_mod] += 1
         return count
