82f98006ddffda407dd10273c38226c33a50efd7
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-20 06:35:01 -0800

    Time: 0 ms (100.00%) | Memory: 20 MB (10.39%) - LeetSync

--- 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.py
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.py
@@ -7,17 +7,16 @@
 class Solution:
     def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         s = 0
-
-        def traverse(root):
+        def inorder(node):
             nonlocal s
-            if root is None:
+            if node is None:
                 return
-            traverse(root.right)
-            s += root.val
-            root.val = s
-            traverse(root.left)
-            return root
+            inorder(node.right)
+            s += node.val
+            node.val = s
+            inorder(node.left)
+        inorder(root)
+        return root
 
-        return traverse(root)
-        
+
         
