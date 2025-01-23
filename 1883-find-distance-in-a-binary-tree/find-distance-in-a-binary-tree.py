da2bc568339615ca2077a362d17b9337f3038761
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-23 08:25:15 -0800

    Time: 7 ms (84.91%) | Memory: 22.2 MB (70.69%) - LeetSync

--- 1883-find-distance-in-a-binary-tree/find-distance-in-a-binary-tree.py
+++ 1883-find-distance-in-a-binary-tree/find-distance-in-a-binary-tree.py
@@ -5,47 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findDistance(self, root: TreeNode, p: int, q: int) -> int:
-        if p == q:
-            return 0
-        result = -1
+    def findDistance(self, root: Optional[TreeNode], p: int, q: int) -> int:
 
-        def dfs(node: TreeNode) -> int:
+        def find_lca(root, p, q):
+            if root is None or root.val == p or root.val == q:
+                return root
+            left = find_lca(root.left, p, q)
+            right = find_lca(root.right, p, q)
+            if left and right:
+                return root
+            return left if left else right
 
-            # -1 if neither p nor q is found in the current subtree
-            # 0 if the current node is p or q
-            # A positive integer representing the distance
-            #   from the current node to either p or q if one is found in its subtree
+        lca = find_lca(root, p, q)
 
-            nonlocal result
-            if not node:
+        def find_distance(root, target, depth):
+            if not root:
                 return -1
+            if root.val == target:
+                return depth
+            left = find_distance(root.left, target, depth + 1)
+            if left != -1:
+                return left
+            return find_distance(root.right, target, depth + 1)
 
-            left = dfs(node.left)
-            right = dfs(node.right)
 
-            if node.val == p or node.val == q:
-                # Found either p or q, but neither is a descendant of the current node.
-                if left < 0 and right < 0:
-                    # "I am one of the target nodes, but I don't have the other target in my subtrees".
-                    return 0
-                # Found either p or q, and the current node is also the LCA.
-                result = 1 + (left if left >= 0 else right)
-                return -1  # Stop further search
 
-            # Current node is neither p or q, but it is the LCA.
-            if left >= 0 and right >= 0:
-                result = left + right + 2
-                return -1  # Stop further search
-
-            # Continue the search if either p or q is found in the subtrees.
-            if left >= 0:
-                return left + 1
-            if right >= 0:
-                return right + 1
-
-            # Neither p or q is found in the subtrees, continue the search.
-            return -1
-
-        dfs(root)
-        return result
+        return find_distance(lca, p, 0) + find_distance(lca, q, 0)
