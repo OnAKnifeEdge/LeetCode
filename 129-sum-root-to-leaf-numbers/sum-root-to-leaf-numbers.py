17111efb3d04b206ac132ebcb3928f25a8e77819
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-01-23 05:58:40 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (32.36%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -8,19 +8,17 @@ class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         path_sums = 0
 
-        def traverse(node, path_sum=0, path=[]):
+        def traverse(node, path=[]):
             nonlocal path_sums
             if node is None:
                 return
-            path_sum = 10 * path_sum + node.val
-            path.append(node.val)
+            path.append(str(node.val))
             if node.left is None and node.right is None:
-                path_sums += path_sum
+                path_sums += int(''.join(path))
             else:
-                traverse(node.left, path_sum)
-                traverse(node.right, path_sum)
-            val = path.pop()
-            path_sum = (path_sum - val) // 10
+                traverse(node.left, path)
+                traverse(node.right, path)
+            path.pop()
 
         traverse(root)
         return path_sums
