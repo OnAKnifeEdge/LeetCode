5a6310531d33d975e1734d4b45d47df298d6ff60
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-02-10 11:47:45 -0800

    Time: 885 ms (73.68%) | Memory: 39.1 MB (6.02%) - LeetSync

--- 2003-distinct-numbers-in-each-subarray/distinct-numbers-in-each-subarray.py
+++ 2003-distinct-numbers-in-each-subarray/distinct-numbers-in-each-subarray.py
@@ -1,15 +1,19 @@
 class Solution:
     def distinctNumbers(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
-        frequency = Counter(nums[:k])
+        frequency = {}
         result = []
-        result.append(len(frequency))
-        for i in range(k, n):
+        for i in range(n):
             num = nums[i]
             if num not in frequency:
                 frequency[num] = 1
             else:
                 frequency[num] += 1
+            if i == k - 1:
+                result.append(len(frequency))
+                continue
+            if i < k:
+                continue
             to_be_deleted = nums[i - k]
             frequency[to_be_deleted] -= 1
             if frequency[to_be_deleted] == 0:
