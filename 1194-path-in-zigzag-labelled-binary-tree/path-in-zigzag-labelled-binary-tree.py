625bd6d50642864bc61ef074d972d75a561d90d7
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-02-12 17:08:26 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.00%) - LeetSync

--- 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.py
+++ 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.py
@@ -9,10 +9,8 @@ class Solution:
 
         while label > 0:
             path.append(label)
-            # min_val = 1 << (level - 1)  # 2 ** (n - 1)
-            # max_val = 1 << level - 1  # 2 ** n - 1
-            min_val = 1 << (level - 1)  # # 2 ** (n - 1)
-            max_val = (1 << level) - 1  #  # 2 ** n - 1
+            min_val = 1 << (level - 1)
+            max_val = (1 << level) - 1
             label = min_val + max_val - label
             label //= 2
             level -= 1
