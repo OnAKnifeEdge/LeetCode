a6e8f7f6684ea5f812c38862215ac9270877798b
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-02-12 17:36:46 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (40.87%) - LeetSync

--- 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.py
+++ 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.py
@@ -9,14 +9,14 @@ class Solution:
             mask = (mask << 1) | 1  # Expanding mask by shifting left and adding 1
 
         x = label
-        res = deque()
+        result = []
 
         # Step 2: Traverse up the tree while flipping labels at each level
         while x:
-            res.appendleft(x)
+            result.append(x)
             # Store current label at the front (since we go bottom-up)
             x >>= 1  # Move to parent
             mask >>= 1  # Reduce the mask for the next level
             x ^= mask  # Flip the label in the zigzag order
 
-        return list(res)
+        return result[::-1]
