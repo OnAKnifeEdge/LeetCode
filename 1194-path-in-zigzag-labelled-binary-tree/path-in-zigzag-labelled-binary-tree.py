33a2453c292f1f8401a468e02718e81a34803fb7
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-02-12 17:08:03 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (40.87%) - LeetSync

---
+++ 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pathInZigZagTree(self, label: int) -> List[int]:
+        # https://leetcode.com/problems/path-in-zigzag-labelled-binary-tree/solutions/2683563/python3-xor-solution/
+        path = []
+        level = 0
+
+        while 1 << level <= label:
+            level += 1
+
+        while label > 0:
+            path.append(label)
+            # min_val = 1 << (level - 1)  # 2 ** (n - 1)
+            # max_val = 1 << level - 1  # 2 ** n - 1
+            min_val = 1 << (level - 1)  # # 2 ** (n - 1)
+            max_val = (1 << level) - 1  #  # 2 ** n - 1
+            label = min_val + max_val - label
+            label //= 2
+            level -= 1
+
+        return path[::-1]
