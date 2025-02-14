ed425b0cfde94ce0dd9ccc0df836edbfde747d86
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-02-14 13:48:56 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (92.14%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -5,28 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def constructFromPrePost(
-        self, preorder: List[int], postorder: List[int]
-    ) -> Optional[TreeNode]:
-        if not preorder or not postorder:
-            return None
-        d = {val: idx for idx, val in enumerate(postorder)}  # {val: idx} for preorder
-
-        def build(start, end):
-            if start > end:
-                return None
-
-            root_val = preorder.pop(0)
-            root = TreeNode(root_val)
-
-            if start == end:
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        postorder_map = {v: idx for idx, v in enumerate(postorder)}
+        n = len(preorder)
+        def build(pre_start, pre_end, post_start, post_end):
+            if pre_start > pre_end:
+                return
+            root = TreeNode(preorder[pre_start])
+            if pre_start == pre_end:
                 return root
-
-            left_root_val = preorder[0]
-            left_root_idx = d[left_root_val]
-
-            root.left = build(start, left_root_idx)
-            root.right = build(left_root_idx + 1, end - 1)
+            left_root_val = preorder[pre_start + 1]
+            post_left_root_idx = postorder_map[left_root_val]
+            left_size = post_left_root_idx - post_start + 1
+            root.left = build(pre_start + 1, pre_start + left_size, post_start, post_left_root_idx)
+            root.right = build(pre_start + 1 + left_size, pre_end, post_left_root_idx  + 1, post_end - 1)
             return root
+        return build(0, n - 1, 0, n - 1)
 
-        return build(0, len(preorder) - 1)
