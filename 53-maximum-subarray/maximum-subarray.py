35024cd410b3c791b1343a18e48a62f965f85ca6
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-02-25 20:09:36 -0800

    Time: 87 ms (36.96%) | Memory: 32.6 MB (51.86%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,12 +1,23 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        prefix_sum = 0
-        min_prefix_sum = 0
-        max_sum = float("-inf")
+        # kadane's algorithms
+        max_sum = nums[0]
+        max_ending_so_far = nums[0]
 
-        for num in nums:
-            prefix_sum += num
-            max_sum = max(max_sum, prefix_sum - min_prefix_sum)
-            min_prefix_sum = min(min_prefix_sum, prefix_sum)
+        for num in nums[1:]:
+            max_ending_so_far = max(num, max_ending_so_far + num)
+            max_sum = max(max_sum, max_ending_so_far)
 
         return max_sum
+
+        # prefix_sum solution
+        # prefix_sum = 0
+        # min_prefix_sum = 0
+        # max_sum = float("-inf")
+
+        # for num in nums:
+        #     prefix_sum += num
+        #     max_sum = max(max_sum, prefix_sum - min_prefix_sum)
+        #     min_prefix_sum = min(min_prefix_sum, prefix_sum)
+
+        # return max_sum
