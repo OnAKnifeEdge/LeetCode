0f52ae6c3f771e880341988d09939c543b802968
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-02-25 21:04:19 -0800

    Time: 51 ms (80.21%) | Memory: 30.7 MB (35.63%) - LeetSync

---
+++ 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getMaxLen(self, nums: List[int]) -> int:
+        positive_cnt = 0
+        negative_cnt = 0
+
+        max_length = 0
+
+        for num in nums:
+            if num > 0:
+                positive_cnt += 1
+                negative_cnt += 1 if negative_cnt > 0 else 0
+            elif num < 0:
+                positive_cnt_copy, negative_cnt_copy = positive_cnt, negative_cnt
+                positive_cnt = negative_cnt_copy + 1 if negative_cnt > 0 else 0
+                negative_cnt = positive_cnt_copy + 1
+
+            else:
+                positive_cnt = negative_cnt = 0
+
+            max_length = max(max_length, positive_cnt)
+        return max_length
