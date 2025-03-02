2e78736106f54e9048d5c6407e589b7922a08e4f
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-03-01 16:28:22 -0800

    Time: 1 ms (60.78%) | Memory: 18 MB (94.51%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.py
+++ 1008-binary-tree-cameras/binary-tree-cameras.py
@@ -1,40 +1,35 @@
-from enum import Enum
-
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from enum import Enum
 
 
-class Camera(Enum):
-    NEEDS_CAMERA = 0
+class CameraStatus(Enum):
+    EMPTY = 0
     COVERED = 1
     HAS_CAMERA = 2
 
 
 class Solution:
-
-    def __init__(self):
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
         self.camera = 0
 
-    def minCameraCover(self, root: Optional[TreeNode]) -> int:
-        if self.cover(root) == Camera.NEEDS_CAMERA:
+        def dfs(node):
+            if not node:
+                return CameraStatus.COVERED
+            left = dfs(node.left)
+            right = dfs(node.right)
+            if left == CameraStatus.EMPTY or right == CameraStatus.EMPTY:
+                self.camera += 1
+                return CameraStatus.HAS_CAMERA
+            if left == CameraStatus.HAS_CAMERA or right == CameraStatus.HAS_CAMERA:
+                return CameraStatus.COVERED
+            return CameraStatus.EMPTY
+            # this node is not monitored and signals to the parent, “I need coverage.”
+
+        if dfs(root) == CameraStatus.EMPTY:
             self.camera += 1
         return self.camera
-
-    def cover(self, node) -> int:
-        if not node:
-            return Camera.COVERED
-        left = self.cover(node.left)
-        right = self.cover(node.right)
-        if left == Camera.NEEDS_CAMERA or right == Camera.NEEDS_CAMERA:
-            self.camera += 1
-            return Camera.HAS_CAMERA
-
-        if left == Camera.HAS_CAMERA or right == Camera.HAS_CAMERA:
-            return Camera.COVERED
-
-        return Camera.NEEDS_CAMERA
