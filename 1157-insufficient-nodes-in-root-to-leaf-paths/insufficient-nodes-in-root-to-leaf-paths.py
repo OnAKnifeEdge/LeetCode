346c9299dab54b41b764fdfbea28d3f8d7acace6
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-03-02 19:51:45 -0800

    Time: 8 ms (33.49%) | Memory: 19.5 MB (9.09%) - LeetSync

--- 1157-insufficient-nodes-in-root-to-leaf-paths/insufficient-nodes-in-root-to-leaf-paths.py
+++ 1157-insufficient-nodes-in-root-to-leaf-paths/insufficient-nodes-in-root-to-leaf-paths.py
@@ -8,17 +8,24 @@ class Solution:
     def sufficientSubset(
         self, root: Optional[TreeNode], limit: int
     ) -> Optional[TreeNode]:
-        if not root:
-            return None
-        if not root.left and not root.right:
-            if root.val < limit:
-                return None
-            return root
-        left = self.sufficientSubset(root.left, limit - root.val)
-        right = self.sufficientSubset(root.right, limit - root.val)
 
-        if not left and not right:
-            return None
-        root.left = left
-        root.right = right
-        return root
+        def dfs(node, parent_sum=0):
+            if not node:
+                return None, float("-inf")
+            curr_sum = parent_sum + node.val
+            if not node.left and not node.right:
+                if curr_sum >= limit:
+                    return node, curr_sum
+                else:
+                    return None, curr_sum
+            left, left_max = dfs(node.left, curr_sum)
+            right, right_max = dfs(node.right, curr_sum)
+            node.left = left
+            node.right = right
+            if not left and not right:
+                return None, curr_sum
+
+            return node, max(left_max, right_max)
+
+        node, _ = dfs(root)
+        return node
