9b076b8d05d1138cc79cee7a9e08721bbd4f36ca
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-03-04 10:18:21 -0800

    Time: 88 ms (32.67%) | Memory: 21.9 MB (19.76%) - LeetSync

---
+++ 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minSwaps(self, data: List[int]) -> int:
+        ones = sum(data)
+        n = len(data)
+        left = 0
+        right = 0
+        ones_in_window = 0
+        max_ones_in_window = 0
+        while right < n:
+            ones_in_window += data[right]
+            if right - left + 1 > ones:
+                ones_in_window -= data[left]
+                left += 1
+            max_ones_in_window = max(max_ones_in_window, ones_in_window)
+            right += 1
+        return ones - max_ones_in_window
