9e77b21f63ddedb84163545718895a0e989469a5
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-03-04 10:34:25 -0800

    Time: 0 ms (100.00%) | Memory: 19.9 MB (81.82%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
@@ -6,20 +6,20 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        r = []  #  the average value of the nodes on each level in the form of an array
         if not root:
-            return r
+            return []
         q = deque([root])
+        averages = []
         while q:
             n = len(q)
-            s = 0
+            total = 0
             for _ in range(n):
                 node = q.popleft()
-                s += node.val
+                total += node.val
                 if node.left:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
-            avg = s / n
-            r.append(avg)
-        return r
+            avg = total / n
+            averages.append(avg)
+        return averages
