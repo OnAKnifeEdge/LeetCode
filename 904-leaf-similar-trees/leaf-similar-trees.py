f92bb2137375db1cd6bc9b1e050d6b0c5482afd8
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-03-04 20:35:29 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (44.47%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -1,26 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        if root1 is None and root2 is None:
-            return True
-        if root1 is None or root2 is None:
-            return False
-
-        def dfs(node):
-            leaves = []
-            if not node:
-                return []
-            if not node.left and not node.right:
-                leaves.append(node.val)
-            left = dfs(node.left)
-            leaves.extend(left)
-            right = dfs(node.right)
-            leaves.extend(right)
-            return leaves
-
-        return dfs(root1) == dfs(root2)
+        def leaf_generator(root):
+            if not root:
+                return
+            if not root.left and not root.right:
+                yield root.val
+            else:
+                yield from leaf_generator(root.left)
+                yield from leaf_generator(root.right)
+        
+        return list(leaf_generator(root1)) == list(leaf_generator(root2))
