42a84be2a6884980206df89420dffb9bebd5ddb0
OnAKnifeEdge <56560144+OnAKnifeEdge@users.noreply.github.com>
2025-03-12 12:37:02 -0700

    Time: 0 ms (100.00%) | Memory: 18.3 MB (22.73%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -14,7 +14,7 @@ class Solution:
                 hi = mid - 1
         return lo
 
-    def right_bound(self, nums):  # find last positive value
+    def right_bound(self, nums):  # find first positive value
         lo, hi = 0, len(nums) - 1
         while lo <= hi:
             mid = (hi + lo) // 2
